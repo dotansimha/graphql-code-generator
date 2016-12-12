cbfe04260b55450c5175e98702799e42130dfef1
dotansimha <dotansimha@gmail.com>
2016-12-12 23:43:22 +0200

    test(base): added basic tests framework support with typescript

---
+++ jest.ts-compiler.js
@@ -0,0 +1,16 @@
+const tsc = require('typescript');
+const tsConfig = require('./tsconfig.json');
+
+module.exports = {
+  process(src, path) {
+    if (path.endsWith('.ts') || path.endsWith('.tsx')) {
+      return tsc.transpile(
+        src,
+        tsConfig.compilerOptions,
+        path,
+        []
+      );
+    }
+    return src;
+  },
+};
