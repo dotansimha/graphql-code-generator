28ae2b0917d7691bff2c20cc0946a224f4ccb872
dotansimha <dotansimha@gmail.com>
2016-12-13 00:54:54 +0200

    test(scheme-loader): added tests

---
+++ test/__tests__/scheme-loader.test.ts
@@ -0,0 +1,41 @@
+import {validateSchema, loadSchema} from "../../src/scheme-loader";
+import {IntrospectionQuery} from "graphql/utilities/introspectionQuery";
+import {GraphQLSchema} from "graphql/type/schema";
+
+describe('document-loader', () => {
+  let schema;
+
+  beforeAll(() => {
+    schema = require('../../dev-test/githunt/schema.json');
+  });
+
+  describe('loadSchema', () => {
+    test('should return GraphQLSchema object', () => {
+      const value = loadSchema(schema);
+
+      expect(value instanceof GraphQLSchema).toBeTruthy();
+    });
+
+    test('should return valid GraphQLSchema object', () => {
+      const value = loadSchema(schema);
+
+      expect(value.getQueryType()).toBeDefined();
+      expect(value.getMutationType()).toBeDefined();
+      expect(value.getSubscriptionType()).toBeDefined();
+    });
+  });
+
+  describe('validateSchema', () => {
+    test('should not throw an exception when using a valid schema', () => {
+      expect(() => {
+        validateSchema(schema);
+      }).not.toThrow();
+    });
+
+    test('should throw an exception when using an invalid schema', () => {
+      expect(() => {
+        validateSchema(<IntrospectionQuery>{});
+      }).toThrow();
+    });
+  });
+});
