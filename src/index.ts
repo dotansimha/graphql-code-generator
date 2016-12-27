7d692e9d0aeaec6d190d698420f7c3a942d96e2c
Hagai Cohen <hagai.co@gmail.com>
2016-12-27 13:06:30 +0200

    chore(libify): moved cli to gql-gen, and exported functionality thru index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,5 @@
-#!/usr/bin/env node
-
-import {initCLI, validateCliOptions, transformOptions, cliError} from './cli';
-import {FileResult, Transform, TransformedOptions} from './transform-engine';
-import {loadSchema} from './scheme-loader';
-import {prepareCodegen} from './codegen';
-import {loadDocumentsSources} from './document-loader';
-import {compileTemplate, loadFromPath} from './template-loader';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as mkdirp from 'mkdirp';
-import {Model, CodegenDocument} from './interfaces';
-import {
-  initHelpers, initPartials, PartialDefinition, HelperDefinition,
-  initTemplateHelpers
-} from './handlebars-helpers';
-
-const options = initCLI(process.argv);
-validateCliOptions(options);
-
-transformOptions(options)
-  .then<FileResult[]>(Transform)
-  .then((generationResult: FileResult[]) => {
-    generationResult.forEach((file: FileResult) => {
-      if (file.isDev) {
-        console.log(`================== ${file.path} ==================`);
-        console.log(file.content);
-      }
-      else {
-        fs.writeFileSync(file.path, file.content);
-        console.log(`Generated file written to ${file.path}`);
-      }
-    });
-  })
-  .catch(cliError);
+export {Transform, TransformedOptions} from './transform-engine';
+export {getTemplateGenerator} from './template-loader';
+export {documentsFromGlobs} from './documents-glob';
+export {introspectionFromUrl} from './introspection-from-url';
+export {introspectionFromFile} from './introspection-from-file';
