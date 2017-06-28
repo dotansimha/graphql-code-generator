afd0ebbcce41345ae8bf19f0e9e1c6832e8863fa
dotansimha <dotansimha@gmail.com>
2017-06-28 20:58:06 +0300

    exports fixes

--- packages/graphql-codegen-core/src/index.ts
+++ packages/graphql-codegen-core/src/index.ts
@@ -1,3 +1,13 @@
 export { schemaToTemplateContext } from './schema/schema-to-template-context';
-export { validateIntrospection } from './utils/introspection-to-schema';
-export * from './types';
+export { validateIntrospection, introspectionToGraphQLSchema } from './utils/introspection-to-schema';
+export {
+  Argument,
+  Field,
+  Type,
+  Scalar,
+  Enum,
+  EnumValue,
+  Union,
+  Interface,
+  SchemaTemplateContext
+} from './types';
