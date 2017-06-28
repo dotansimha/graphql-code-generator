23597791e59ef64a9d9987cba918918f1db23d77
dotansimha <dotansimha@gmail.com>
2017-06-28 16:15:59 +0300

    added more unit tests

--- src/schema/transform-enum.ts
+++ src/schema/transform-enum.ts
@@ -5,14 +5,14 @@ export function transformGraphQLEnum(graphqlEnum: GraphQLEnumType): Enum {
   const enumValues = graphqlEnum.getValues().map<EnumValue>((enumItem: GraphQLEnumValue) => {
     return <EnumValue>{
       name: enumItem.name,
-      description: enumItem.description,
+      description: enumItem.description || '',
       value: enumItem.value
     };
   });
 
   return {
     name: graphqlEnum.name,
-    description: graphqlEnum.description,
+    description: graphqlEnum.description || '',
     values: enumValues,
   };
 }
