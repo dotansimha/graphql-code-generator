a6fea3306d348899d094892be767e0f6fee52f39
dotansimha <dotansimha@gmail.com>
2017-07-12 18:01:59 +0300

    Fixes to support cli package

--- packages/graphql-codegen-generators/src/index.ts
+++ packages/graphql-codegen-generators/src/index.ts
@@ -4,6 +4,8 @@ export { GeneratorConfig, FileOutput } from './types';
 import TypescriptSingleFile from './typescript-single-file/config';
 import TypescriptMultiFile from './typescript-multi-file/config';
 
+export { getGeneratorConfig } from './get-generator';
+
 export {
   TypescriptSingleFile,
   TypescriptMultiFile,
