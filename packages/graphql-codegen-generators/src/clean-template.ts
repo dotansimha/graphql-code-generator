383c3d3576be1250a0cef82dc24a171dd8db6383
dotansimha <dotansimha@gmail.com>
2017-07-15 09:37:38 +0300

    update dependencies and some minor fixes to magic comment parser

--- packages/graphql-codegen-generators/src/clean-template.ts
+++ packages/graphql-codegen-generators/src/clean-template.ts
@@ -1,3 +1,19 @@
+import { debugLog } from 'graphql-codegen-core';
+
 export function cleanTemplateComments(template: string): string {
-  return template.replace(/\/\*\s*gqlgen\s*(.*?)\s*\*\//g, (all, group) => group ? group : all);
+  if (template.match(/\/\*\s*gqlgen/ig)) {
+    debugLog(`Found magic comment 'gqlgen' in template...`, template);
+
+    return template
+      .replace(/.*({{.*}})/ig, (all, group) => {
+        if (all.toLowerCase().includes('gqlgen')) {
+          return all;
+        }
+
+        return all.replace(/{{/g, '\\{{');
+      })
+      .replace(/\/\*\s*gqlgen\s*(.*?)\s*\*\//gi, (all, group) => group ? group : all);
+  }
+
+  return template;
 }
--- packages/graphql-codegen-generators/tests/clean-template.spec.ts
+++ packages/graphql-codegen-generators/tests/clean-template.spec.ts
@@ -7,6 +7,50 @@ describe('cleanTemplateComments', () => {
     expect(result).toBe('{{#each test }}');
   });
 
+  it('should clean comments and add comments for a complex file with {{ ', () => {
+    const str = `
+import * as React from 'react';
+import { Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { Home } from '../../home/components/home';
+import { DataTable } from '../../data-table/components/data-table';
+
+export const routerHistory = createBrowserHistory();
+
+export const routesDefintion = (
+  <div style={{ top: 1 }}>
+    <Route exact={true} path="/" component={Home} />
+    <Route path="/datatable/">
+      /*gqlgen {{#each operations }} */
+      /*gqlgen <Route path="/{{ toLowerCase name }}" component={DataTable} /> */
+      /*gqlgen {{/each }} */
+    </Route>
+  </div>
+);
+`;
+    const result = cleanTemplateComments(str);
+    expect(result).toBe(`
+import * as React from 'react';
+import { Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { Home } from '../../home/components/home';
+import { DataTable } from '../../data-table/components/data-table';
+
+export const routerHistory = createBrowserHistory();
+
+export const routesDefintion = (
+  <div style=\\\{{ top: 1 }}>
+    <Route exact={true} path="/" component={Home} />
+    <Route path="/datatable/">
+      {{#each operations }}
+      <Route path="/{{ toLowerCase name }}" component={DataTable} />
+      {{/each }}
+    </Route>
+  </div>
+);
+`);
+  });
+
   it('should clean comments from simple multiple line string with multiple uses of magic comments', () => {
     const str = `
     class MyComp extends React.Component {
