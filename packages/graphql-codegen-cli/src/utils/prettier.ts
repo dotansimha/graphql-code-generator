b59b9ef6cbc476ea91768b1a8dcb44fd44e0c23d
dotansimha <dotansimha@gmail.com>
2018-03-13 20:15:07 +0200

    added prettier support and respect local config file

---
+++ packages/graphql-codegen-cli/src/utils/prettier.ts
@@ -0,0 +1,31 @@
+import * as prettier from 'prettier';
+import * as path from 'path';
+
+const EXTENSION_TO_PARSER = {
+  'ts': 'typescript',
+  'tsx': 'typescript',
+  'js': 'babylon',
+  'jsx': 'babylon',
+  'js.flow': 'flow',
+  'flow': 'flow',
+  'gql': 'graphql',
+  'graphql': 'graphql',
+  'css': 'postcss',
+  'scss': 'postcss',
+  'less': 'postcss',
+  'stylus': 'postcss',
+  'markdown': 'markdown',
+  'md': 'markdown',
+  'json': 'json',
+};
+
+export async function prettify(filePath: string, content: string): Promise<string> {
+  const fileExtension = path.extname(filePath).slice(1);
+  const parser = EXTENSION_TO_PARSER[fileExtension];
+  const config = await prettier.resolveConfig(process.cwd(), { useCache: true, editorconfig: true });
+
+  return prettier.format(content, {
+    parser,
+    ...(config || {}),
+  });
+}
