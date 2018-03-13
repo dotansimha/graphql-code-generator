aaa33554e762b915b8e159eca4307bdabe343941
dotansimha <dotansimha@gmail.com>
2018-03-13 19:43:20 +0200

    fix for commented code

---
+++ packages/graphql-codegen-cli/tests/test-files/4.ts
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+
+// export const myQuery = gql(`
+//   query myQuery1 {
+//     data {
+//       field1
+//       field2
+//     }
+//   }
+//   `);
+
+export const myQuery = gql(`
+  query myQuery2 {
+    data {
+      field1
+      field2
+    }
+  }
+  `);
+
+/*
+export const myQuery = gql(`
+  query myQuery3 {
+    data {
+      field1
+      field2
+    }
+  }
+  `);
+*/
