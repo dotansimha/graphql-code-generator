c29d8a1e8ce6d5a94296f8152c250bbb2d62686d
dotansimha <dotansimha@gmail.com>
2018-04-29 11:17:35 +0300

    fixes for document finder

--- packages/graphql-codegen-cli/tests/document-finder.spec.ts
+++ packages/graphql-codegen-cli/tests/document-finder.spec.ts
@@ -1,36 +1,59 @@
 import * as fs from 'fs';
 import { extractDocumentStringFromCodeFile } from '../src/utils/document-finder';
+import { parse } from 'graphql';
 
 describe('extractDocumentStringFromCodeFile', () => {
   it('file with gql and template literal', () => {
     const fileContent = fs.readFileSync('./tests/test-files/1.ts').toString();
     const doc = extractDocumentStringFromCodeFile(fileContent);
+    const parsingTest = parse.bind(null, doc);
     expect(doc).toContain('query');
+    expect(parsingTest).not.toThrow();
   });
 
   it('file with gql and string and function use', () => {
     const fileContent = fs.readFileSync('./tests/test-files/2.ts').toString();
     const doc = extractDocumentStringFromCodeFile(fileContent);
+    const parsingTest = parse.bind(null, doc);
     expect(doc).toContain('query');
+    expect(parsingTest).not.toThrow();
   });
 
   it('file with simple gql content', () => {
     const fileContent = fs.readFileSync('./tests/test-files/3.graphql').toString();
     const doc = extractDocumentStringFromCodeFile(fileContent);
+    const parsingTest = parse.bind(null, doc);
+
     expect(doc).toContain('query');
+    expect(parsingTest).not.toThrow();
   });
 
   it('file with commented code', () => {
     const fileContent = fs.readFileSync('./tests/test-files/4.ts').toString();
     const doc = extractDocumentStringFromCodeFile(fileContent);
+    const parsingTest = parse.bind(null, doc);
+
     expect(doc).toContain('myQuery2');
     expect(doc).not.toContain('myQuery1');
     expect(doc).not.toContain('myQuery3');
+    expect(parsingTest).not.toThrow();
   });
 
   it('file with gql and template literal in ts/tsx file', () => {
     const fileContent = fs.readFileSync('./tests/test-files/5.tsx').toString();
     const doc = extractDocumentStringFromCodeFile(fileContent);
+    const parsingTest = parse.bind(null, doc);
+
     expect(doc).toContain('query');
+    expect(parsingTest).not.toThrow();
+  });
+
+  it('file with gql and template literal, with use of string variables', () => {
+    const fileContent = fs.readFileSync('./tests/test-files/6.ts').toString();
+    const doc = extractDocumentStringFromCodeFile(fileContent);
+    const parsingTest = parse.bind(null, doc);
+
+    expect(doc).toContain('query');
+    expect(parsingTest).not.toThrow();
   });
 });
