bb18c7b57e19af14ff0df150ebf458fb7934d181
dotansimha <dotansimha@gmail.com>
2018-04-30 14:13:02 +0300

    fixes for tests

--- packages/scripts/handlebars-templates-scripts/cli/codegen-handlebars-templates-scripts-build.js
+++ packages/scripts/handlebars-templates-scripts/cli/codegen-handlebars-templates-scripts-build.js
@@ -20,7 +20,23 @@ webpackConfig.output.path = path.join(newProjectDir, './dist/');
 rimraf.sync(webpackConfig.output.path);
 
 webpack(webpackConfig, (err, stats) => {
-  if (err || stats.hasErrors()) {
-    console.error(stats);
+  if (err) {
+    console.error(err.stack || err);
+
+    if (err.details) {
+      console.error(err.details);
+    }
+
+    return;
+  }
+
+  const info = stats.toJson();
+
+  if (stats.hasErrors()) {
+    console.error(info.errors);
+  }
+
+  if (stats.hasWarnings()) {
+    console.warn(info.warnings);
   }
 });
