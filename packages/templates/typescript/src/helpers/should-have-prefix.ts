a4f3e7d8646ed25eb3f9ce157d5c6b95e76ff1dc
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-08-08 11:47:53 +0200

    Make it work

--- packages/templates/typescript/src/helpers/should-have-prefix.ts
+++ packages/templates/typescript/src/helpers/should-have-prefix.ts
@@ -2,9 +2,7 @@ import { isPrimitiveType } from './is-primitive-type';
 
 export function shouldHavePrefix(type, options) {
   const config = options.data.root.config || {};
-  const nonPrefixable = ['Enum', 'Scalar', 'Union'];
+  const nonPrefixable = type.isEnum || type.isUnion || type.isScalar;
 
-  return (
-    config.noNamespaces === true && !isPrimitiveType(type, options) && nonPrefixable.indexOf(type.fieldType) === -1
-  );
+  return config.noNamespaces === true && !isPrimitiveType(type, options) && !nonPrefixable;
 }
