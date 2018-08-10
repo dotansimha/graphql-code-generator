4f8b6537d2bc04fe43fbf2c3728c4696da6e6e84
Sebastian Goll <sebastian.goll@gmx.de>
2018-08-10 10:49:43 +0200

    Simplify fix and make it work properly when pascal-casing names

--- packages/templates/typescript/src/helpers/get-scalar-type.ts
+++ packages/templates/typescript/src/helpers/get-scalar-type.ts
@@ -1,10 +1,7 @@
 export function getScalarType(type, options) {
   const config = options.data.root.config || {};
   if (config.scalars && type in config.scalars) {
-    // Avoid alias when identical (#485).
-    if (config.scalars[type] !== type) {
-      return config.scalars[type];
-    }
+    return config.scalars[type];
   } else {
     return 'any';
   }
