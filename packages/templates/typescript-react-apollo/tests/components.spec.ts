31638809ea776bc0bd9171f119d922dc666deafd
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-08-12 00:51:56 -0400

    add tests for react-apollo

--- packages/templates/typescript-react-apollo/tests/components.spec.ts
+++ packages/templates/typescript-react-apollo/tests/components.spec.ts
@@ -41,8 +41,77 @@ describe('Components', () => {
     expect(content).toBeSimilarStringTo(`
           import * as ReactApollo from 'react-apollo';
         `);
+
     expect(content).toBeSimilarStringTo(`
-          export class Component extends ReactApollo.Query<Query, Variables> { }
+      import gql from 'graphql-tag';
+    `);
+
+    expect(content).toBeSimilarStringTo(`
+                  export class Component extends ReactApollo.Query<Query, Variables> {
+                      constructor(props, context){
+                          props.query = props.query ||  gql\` {
+                              feed {
+                                id
+                                commentCount
+                                repository {
+                                  full_name
+                                  html_url
+                                  owner {
+                                    avatar_url
+                                  }
+                                }
+                              }
+                              } \`;
+                          super(props, context);
+                      }
+                  }
+        `);
+  });
+  it('should generate Component w/ noGraphqlTag option', async () => {
+    const schema = introspectionToGraphQLSchema(JSON.parse(fs.readFileSync('./tests/files/schema.json').toString()));
+    const context = schemaToTemplateContext(schema);
+
+    const documents = gql`
+      query {
+        feed {
+          id
+          commentCount
+          repository {
+            full_name
+            html_url
+            owner {
+              avatar_url
+            }
+          }
+        }
+      }
+    `;
+
+    const transformedDocument = transformDocument(schema, documents);
+    const compiled = await compileTemplate(
+      {
+        ...config,
+        config: {
+          noGraphqlTag: true
+        }
+      },
+      context,
+      [transformedDocument],
+      { generateSchema: false }
+    );
+    const content = compiled[0].content;
+
+    expect(content).toBeSimilarStringTo(`
+          import * as ReactApollo from 'react-apollo';
+        `);
+
+    expect(content).toBeSimilarStringTo(`
+                  export class Component extends ReactApollo.Query<Query, Variables> {
+                      constructor(props, context){
+                          props.query = props.query ||  ${JSON.stringify(documents)};
+                          super(props, context);
+                      }
+                  }
         `);
   });
 });
