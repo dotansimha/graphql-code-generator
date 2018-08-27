0b055705a4dacc6cba92f8985d9ee541dfbd5df5
Kamil Kisiela <mys.sterowiec@gmail.com>
2018-08-27 10:45:05 +0200

    Fix missing error message in logger (#542)
    Fixes #535

---
+++ packages/graphql-codegen-cli/tests/codegen.spec.ts
@@ -0,0 +1,48 @@
+import { logger } from 'graphql-codegen-core';
+import { cliError } from '../src/codegen';
+
+describe('cliError', () => {
+  let spyLogger: jest.SpyInstance;
+  let spyProcessExit: jest.SpyInstance;
+
+  beforeEach(() => {
+    spyLogger = jest.spyOn(logger, 'error');
+    spyLogger.mockImplementation();
+    spyProcessExit = jest.spyOn(process, 'exit');
+    spyProcessExit.mockImplementation();
+  });
+
+  afterEach(() => {
+    spyLogger.mockRestore();
+    spyProcessExit.mockRestore();
+  });
+
+  it('should handle an Error', () => {
+    const msg = 'used as error';
+
+    cliError(new Error(msg));
+
+    expect(spyLogger).toBeCalledWith(msg);
+    expect(spyProcessExit).toBeCalledWith(1);
+  });
+
+  it('should handle string', () => {
+    const msg = 'used as string';
+
+    cliError(msg);
+
+    expect(spyLogger).toBeCalledWith(msg);
+    expect(spyProcessExit).toBeCalledWith(1);
+  });
+
+  it('should handle an object', () => {
+    const obj = {
+      foo: 1
+    };
+
+    cliError(obj);
+
+    expect(spyLogger).toBeCalledWith(JSON.stringify(obj));
+    expect(spyProcessExit).toBeCalledWith(1);
+  });
+});
