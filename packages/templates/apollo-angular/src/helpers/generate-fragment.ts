576bcbfbae2931867a6af950a5ceb7a4dac56b61
Kamil Kisiela <mys.sterowiec@gmail.com>
2018-09-01 15:52:36 +0200

    Make apollo-angular template handle fragments (#559)

---
+++ packages/templates/apollo-angular/src/helpers/generate-fragment.ts
@@ -0,0 +1,20 @@
+import { toFragmentName } from './to-fragment-name';
+import { gql } from './gql';
+
+const cachedFragments: Record<string, any> = {};
+
+export function generateFragment(fragment: any, options: any): string {
+  const cached = cachedFragments[fragment.name];
+
+  if (!cached) {
+    cachedFragments[fragment.name] = fragment;
+
+    return `
+        const ${toFragmentName(fragment.name)} = ${gql(fragment, options)};
+    `;
+  } else {
+    if (fragment.document !== cached.document) {
+      throw new Error(`Duplicated fragment called '${fragment.name}'`);
+    }
+  }
+}
