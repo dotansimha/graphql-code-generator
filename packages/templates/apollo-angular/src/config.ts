eba702c67af4166e553b1ee36577e036e100a6df
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-03 10:13:12 +0200

    feature: named clients and custom providedIn

--- packages/templates/apollo-angular/src/config.ts
+++ packages/templates/apollo-angular/src/config.ts
@@ -3,9 +3,11 @@ import typescriptConfig from 'graphql-codegen-typescript-template';
 import * as components from './components.handlebars';
 import { gql } from './helpers/gql';
 import { generateFragment } from './helpers/generate-fragment';
+import { providedIn } from './helpers/provided-in';
 
 typescriptConfig.templates['documents'] += components;
 typescriptConfig.customHelpers.gql = gql;
 typescriptConfig.customHelpers.generateFragment = generateFragment;
+typescriptConfig.customHelpers.providedIn = providedIn;
 
 export { typescriptConfig as config };
