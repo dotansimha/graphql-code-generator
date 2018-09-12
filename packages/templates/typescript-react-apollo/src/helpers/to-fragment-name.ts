80c291582001c8b51091343c69deffa1ef1032c0
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-09-11 20:50:29 -0400

    fix mutation component props, and use getter function to get document

--- packages/templates/typescript-react-apollo/src/helpers/to-fragment-name.ts
+++ packages/templates/typescript-react-apollo/src/helpers/to-fragment-name.ts
@@ -3,8 +3,8 @@ import { pascalCase } from 'change-case';
 export function toFragmentName(fragmentName: string, options: any): string {
   const config = options.data.root.config || {};
   if (config.noNamespaces) {
-    return pascalCase(`${fragmentName}Document`);
+    return pascalCase(`get${fragmentName}Document()`);
   } else {
-    return pascalCase(`${fragmentName}`) + '.Document';
+    return pascalCase(`${fragmentName}`) + '.getDocument()';
   }
 }
