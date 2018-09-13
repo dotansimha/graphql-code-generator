f20e7e86a77051942931e4a8352595828ea5d19b
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-13 15:48:10 +0200

    revert: feature: named clients and custom providedIn

--- packages/templates/apollo-angular/src/helpers/provided-in.ts
+++
@@ -1,5 +0,0 @@
-export function providedIn(options: any): string {
-  const config = options.data.root.config || {};
-
-  return config.providedIn ? config.providedIn.ref : `'root'`; // Important to return it with in quotes
-}
