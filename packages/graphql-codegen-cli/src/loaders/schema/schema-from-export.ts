ad36ba1e06bff3da511a3d45c879fb01db965e2c
Matt Kantor <the.matt.kantor@gmail.com>
2018-09-12 17:36:58 -0700

    Fix typo in a debug log.

--- packages/graphql-codegen-cli/src/loaders/schema/schema-from-export.ts
+++ packages/graphql-codegen-cli/src/loaders/schema/schema-from-export.ts
@@ -42,7 +42,7 @@ export class SchemaFromExport implements SchemaLoader {
 
                 resolve(schemaResult);
               } catch (e) {
-                debugLog('Unexpected exceptiom while trying to figure out the schema: ', e);
+                debugLog('Unexpected exception while trying to figure out the schema: ', e);
 
                 reject(new Error('Exported schema must be of type GraphQLSchema, text, AST, or introspection JSON.'));
 
