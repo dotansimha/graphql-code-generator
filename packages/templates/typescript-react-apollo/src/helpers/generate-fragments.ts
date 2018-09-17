f023f0ca331bad5aee46c8e7c73dbacaf35d44ea
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-09-17 15:00:02 -0400

    [react-apollo] Fix duplicate fragments and order of them

---
+++ packages/templates/typescript-react-apollo/src/helpers/generate-fragments.ts
@@ -0,0 +1,50 @@
+import { DepGraph } from 'dependency-graph';
+
+import { gql, extractFragments } from './gql';
+
+export function generateFragments(fragments: any[], options: any): string {
+  const cachedFragments: Record<string, any> = {};
+  if (!fragments) {
+    return '';
+  }
+  const graph = new DepGraph({ circular: true });
+  fragments.forEach(fragment => {
+    graph.addNode(fragment.name, fragment);
+  });
+  fragments.forEach(fragment => {
+    const depends = extractFragments(fragment.document);
+    if (depends) {
+      depends.forEach(name => {
+        graph.addDependency(fragment.name, name);
+      });
+    }
+  });
+  return graph
+    .overallOrder()
+    .map(name => generateFragment(graph.getNodeData(name), options))
+    .join('\n');
+
+  function generateFragment(fragment: any, options: any): string {
+    const cached = cachedFragments[fragment.name];
+    if (!cached) {
+      cachedFragments[fragment.name] = fragment;
+      const config = options.data.root.config || {};
+
+      if (config.noNamespaces) {
+        return `
+        export const ${fragment.name}Document = ${gql(fragment, options)};
+        `;
+      } else {
+        return `
+          export namespace ${fragment.name} {
+            export const Document = ${gql(fragment, options)};
+          }
+        `;
+      }
+    } else {
+      if (fragment.document !== cached.document) {
+        throw new Error(`Duplicated fragment called '${fragment.name}'`);
+      }
+    }
+  }
+}
