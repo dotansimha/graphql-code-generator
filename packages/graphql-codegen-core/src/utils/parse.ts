0d8b8995d8c55e44ce3c7f4730d4129fd61795fe
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-18 13:19:14 +0200

    Catch parsing errors

---
+++ packages/graphql-codegen-core/src/utils/parse.ts
@@ -0,0 +1,17 @@
+import { parse as coreParse, Source } from 'graphql';
+
+export function parse(source: Source | string) {
+  try {
+    return coreParse(source);
+  } catch (e) {
+    let error: Error;
+
+    if (typeof source === 'string') {
+      error = new Error(`Parsing '${source.trim().substr(0, 50)}' failed with: ${e.message}`);
+    } else {
+      error = new Error(`Parsing a document in '${source.name}' failed with: ${e.message}`);
+    }
+
+    throw error;
+  }
+}
