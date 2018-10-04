292c69e792615458420e31ecd6e66d7c11eb08b5
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-10-04 08:23:35 -0400

    Pascalcase fragment name

--- packages/templates/typescript-react-apollo/src/helpers/generate-fragments.ts
+++ packages/templates/typescript-react-apollo/src/helpers/generate-fragments.ts
@@ -2,6 +2,7 @@ import { DepGraph } from 'dependency-graph';
 
 import { gql, extractFragments } from './gql';
 import { Fragment } from 'graphql-codegen-core';
+import { pascalCase } from 'change-case';
 
 export function generateFragments(fragments: Fragment[], options: Handlebars.HelperOptions): string {
   const cachedFragments: Record<string, any> = {};
@@ -30,14 +31,16 @@ export function generateFragments(fragments: Fragment[], options: Handlebars.Hel
     if (!cached) {
       cachedFragments[fragment.name] = fragment;
       const config = options.data.root.config || {};
-
+      const pascalCasedFragmentName = pascalCase(fragment.name);
+      // fooBar, FooBar and foo_bar may cause conflict due to the pascalCase.
+      // Because all of them will have same namespace FooBar
       if (config.noNamespaces) {
         return `
-        export const ${fragment.name}Document = ${gql(fragment, options)};
+          export const ${pascalCasedFragmentName}Document = ${gql(fragment, options)};
         `;
       } else {
         return `
-          export namespace ${fragment.name} {
+          export namespace ${pascalCasedFragmentName} {
             export const Document = ${gql(fragment, options)};
           }
         `;
