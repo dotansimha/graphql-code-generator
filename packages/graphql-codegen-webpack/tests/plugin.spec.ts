4ae6dd640eca39c6b1fe071ede126b79481b46e4
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-10-25 17:42:22 +0200

    Empty test

---
+++ packages/graphql-codegen-webpack/tests/plugin.spec.ts
@@ -0,0 +1,7 @@
+import { GraphQLCodegenPlugin } from '../src';
+
+describe('GraphQL Codegen Webpack Plugin', () => {
+  test('should be defined', () => {
+    expect(GraphQLCodegenPlugin).toBeDefined();
+  });
+});
