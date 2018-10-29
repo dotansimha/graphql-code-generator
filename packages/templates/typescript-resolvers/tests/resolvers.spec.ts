d1ebd1b8416d1fef444e915dc0f8bd20b50422b5
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-10-29 14:57:33 +0100

    test: allow to define custom parent type

--- packages/templates/typescript-resolvers/tests/resolvers.spec.ts
+++ packages/templates/typescript-resolvers/tests/resolvers.spec.ts
@@ -424,4 +424,93 @@ describe('Resolvers', () => {
       export type UserNameResolver<R = string | null, Parent = User, Context = any> = Resolver<R, Parent, Context>;
     `);
   });
+
+  it('should accept a map of parent types', async () => {
+    const { context } = compileAndBuildContext(`
+        type Query {
+          post: Post
+        }
+
+        type Post {
+          id: String
+          author: User
+        }
+
+        type User {
+          id: String
+          name: String
+          post: Post
+        }
+        
+        schema {
+          query: Query
+        }
+      `);
+
+    // type UserParent = string;
+    // interface PostParent {
+    //   id: string;
+    //   author: string;
+    // }
+    const compiled = await compileTemplate(
+      {
+        ...config,
+        parentTypes: {
+          // it means that User type expects UserParent to be a parent
+          User: './interfaces#UserParent',
+          // it means that Post type expects UserParent to be a parent
+          Post: './interfaces#PostParent'
+        }
+      } as any,
+      context
+    );
+
+    const content = compiled[0].content;
+
+    // TODO: import parents
+    // TODO: merge duplicates into single module
+    expect(content).toBeSimilarStringTo(`
+      import { UserParent, PostParent } from './interfaces';
+    `);
+
+    // TODO: should check field's result and match it with provided parents
+    expect(content).toBeSimilarStringTo(`
+      export namespace QueryResolvers {
+        export interface Resolvers<Context = any, TypeParent = never> {
+          post?: PostResolver<UserParent, TypeParent, Context>;
+        }
+  
+        export type PostResolver<R = UserParent, Parent = never, Context = any> = Resolver<R, Parent, Context>;
+      }
+    `);
+
+    // TODO: should check if type has a defined parent and use it as TypeParent
+    expect(content).toBeSimilarStringTo(`
+      export namespace PostResolvers {
+        export interface Resolvers<Context = any, TypeParent = UserParent> {
+          id?: IdResolver<string | null, TypeParent, Context>;
+          author?: AuthorResolver<User | null, TypeParent, Context>;
+        }
+
+        export type IdResolver<R = string | null, Parent = UserParent, Context = any> = Resolver<R, Parent, Context>;
+        export type AuthorResolver<R = User | null, Parent = UserParent, Context = any> = Resolver<R, Parent, Context>;
+      }
+    `);
+
+    // TODO: should check if type has a defined parent and use it as TypeParent
+    // TODO: should match field's result with provided parent type
+    expect(content).toBeSimilarStringTo(`
+      export namespace UserResolvers {
+        export interface Resolvers<Context = any, TypeParent = User> {
+          id?: IdResolver<string | null, TypeParent, Context>;
+          name?: NameResolver<string | null, TypeParent, Context>;
+          post?: PostResolver<PostParent, TypeParent, Context>;
+        }
+
+        export type IdResolver<R = string | null, Parent = User, Context = any> = Resolver<R, Parent, Context>;
+        export type NameResolver<R = string | null, Parent = User, Context = any> = Resolver<R, Parent, Context>;
+        export type NameResolver<R = PostParent, Parent = User, Context = any> = Resolver<R, Parent, Context>;
+      }
+    `);
+  });
 });
