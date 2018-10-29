ad16407e32623bf95c1205a549228e85acba1185
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-10-29 16:32:08 +0100

    Import mappers

--- packages/templates/typescript-resolvers/src/config.ts
+++ packages/templates/typescript-resolvers/src/config.ts
@@ -5,11 +5,13 @@ import * as beforeSchema from './before-schema.handlebars';
 import * as afterSchema from './after-schema.handlebars';
 import { getParentType } from './helpers/parent-type';
 import { getFieldType } from './helpers/field-type';
+import { importMappers } from './helpers/import-mappers';
 
 typescriptConfig.templates['resolver'] = resolver;
 typescriptConfig.templates['schema'] = `${beforeSchema}${typescriptConfig.templates['schema']}${afterSchema}`;
 typescriptConfig.customHelpers.getParentType = getParentType;
 typescriptConfig.customHelpers.getFieldType = getFieldType;
+typescriptConfig.customHelpers.importMappers = importMappers;
 typescriptConfig.outFile = 'resolvers-types.ts';
 
 export { typescriptConfig as config };
