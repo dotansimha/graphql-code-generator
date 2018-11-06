a5b572ed69141d338a9b12b9bb52bf843a2de0f1
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-06 15:02:50 +0100

    Use codegen.yml to generate all examples

--- dev-test/generate-all.sh
+++ dev-test/generate-all.sh
@@ -1,27 +1,3 @@
 #/bin/sh
 
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-resolvers-template --schema ./dev-test/test-schema/schema.json --out ./dev-test/test-schema/resolvers-types.ts
-
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/test-schema/schema.json --out ./dev-test/test-schema/typings.ts
-CODEGEN_AVOID_OPTIONALS=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/test-schema/schema.json --out ./dev-test/test-schema/typings.avoidOptionals.ts
-CODEGEN_IMMUTABLE_TYPES=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/test-schema/schema.json --out ./dev-test/test-schema/typings.immutableTypes.ts
-
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-graphql-files-typescript-modules --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/graphql-declared-modules.d.ts "./dev-test/githunt/**/*.graphql"
-
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/types.ts "./dev-test/githunt/**/*.graphql"
-CODEGEN_ENUMS_AS_TYPES=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/types.d.ts "./dev-test/githunt/**/*.graphql"
-CODEGEN_AVOID_OPTIONALS=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/types.avoidOptionals.ts "./dev-test/githunt/**/*.graphql"
-CODEGEN_IMMUTABLE_TYPES=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/types.immutableTypes.ts "./dev-test/githunt/**/*.graphql"
-CODEGEN_NO_NAMESPACES=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/types.noNamespaces.ts "./dev-test/githunt/**/*.graphql"
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-react-apollo-template --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/types.reactApollo.tsx "./dev-test/githunt/**/*.graphql"
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template-multiple --schema ./dev-test/githunt/schema.json --out ./dev-test/githunt/ts-multiple/ "./dev-test/githunt/**/*.graphql"
-
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/star-wars/schema.json --out ./dev-test/star-wars/types.ts "./dev-test/star-wars/**/*.graphql"
-CODEGEN_ENUMS_AS_TYPES=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/star-wars/schema.json --out ./dev-test/star-wars/types.d.ts "./dev-test/star-wars/**/*.graphql"
-CODEGEN_AVOID_OPTIONALS=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/star-wars/schema.json --out ./dev-test/star-wars/types.avoidOptionals.ts "./dev-test/star-wars/**/*.graphql"
-CODEGEN_IMMUTABLE_TYPES=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/star-wars/schema.json --out ./dev-test/star-wars/types.immutableTypes.ts "./dev-test/star-wars/**/*.graphql"
-CODEGEN_NO_NAMESPACES=true node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --schema ./dev-test/star-wars/schema.json --out ./dev-test/star-wars/types.noNamespaces.ts "./dev-test/star-wars/**/*.graphql"
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template --skip-schema --schema ./dev-test/star-wars/schema.json --out ./dev-test/star-wars/types.skipSchema.ts "./dev-test/star-wars/**/*.graphql"
-node packages/graphql-codegen-cli/dist/cli.js --template graphql-codegen-typescript-template-multiple --schema ./dev-test/star-wars/schema.json --out ./dev-test/star-wars/ts-multiple/ "./dev-test/star-wars/**/*.graphql"
-
-
+node packages/graphql-codegen-cli/dist/cli.js
