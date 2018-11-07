4201556e84ded409626060f0951f25b5a163ecf9
DAB0mB <emanor6@gmail.com>
2018-11-07 10:08:21 +0800

    Update help page colors

--- website/pages/en/help.js
+++ website/pages/en/help.js
@@ -21,12 +21,11 @@ class Help extends React.Component {
       <div className="docMainWrapper wrapper">
         <Container className="mainContainer documentContainer postContainer">
           <div className="post">
-            <header className="postHeader">
+            <header className="postHeader helpPostHeader">
               <h2>Need help?</h2>
-              <h1 style={{ marginTop: 0 }}>We've got you covered!</h1>
+              <h1 style={{ marginTop: 0 }}>WE'VE GOT YOU COVERED!</h1>
             </header>
           </div>
-          <div className="helpTitle">Expert Support</div>
           <div className="helpSubtitle">Get our team's help with Apollo, GraphQL and GraphQL Code Generator. Whether you’re just getting started or rolling out GraphQL across your whole organization, we can help with architectural design, implementation and education.</div>
           <ContactForm />
           <script src={`${siteConfig.baseUrl}lib/sweetalert2.all.min.js`} />
