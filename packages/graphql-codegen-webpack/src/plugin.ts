a4fbd654f5d101b9edcaf8a6a66e8d81c7978a50
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-07 14:54:54 +0100

    Use new API in webpack plugin

--- packages/graphql-codegen-webpack/src/plugin.ts
+++ packages/graphql-codegen-webpack/src/plugin.ts
@@ -20,7 +20,7 @@ export class GraphQLCodegenPlugin {
   pluginName = 'GraphQLCodeGeneratorPlugin';
   schemaChecksum = '';
   documentsChecksum = '';
-  documents: string[] = [];
+  documentLocations: string[] = [];
   schemaLocations: Types.Schema[] = [];
   outputFiles: string[] = [];
   config: Types.Config;
@@ -29,20 +29,13 @@ export class GraphQLCodegenPlugin {
   constructor(options: CLIOptions);
   constructor(configOrOptions?: string | CLIOptions) {
     if (typeof configOrOptions === 'object') {
-      this.config = createConfigFromOldCli(configOrOptions);
-      this.outputFiles = [resolve(process.cwd(), configOrOptions.out)];
-      this.schemaLocations = [resolve(process.cwd(), configOrOptions.schema)];
+      this.initLegacy(configOrOptions);
     } else {
-      this.config = createConfig(configOrOptions);
-      this.schemaLocations = normalizeInstanceOrArray<Types.Schema>(this.config.schema);
+      this.init(configOrOptions);
     }
   }
 
   public apply(compiler: compiler.Compiler) {
-    // compiler.hooks.watchRun.tap(this.pluginName, () => {
-    //   this.options.exitOnError = false;
-    // });
-
     compiler.hooks.afterEnvironment.tap(this.pluginName, () => {
       (compiler as any).watchFileSystem = new WatchFileSystem(
         compiler,
@@ -52,7 +45,33 @@ export class GraphQLCodegenPlugin {
     });
 
     compiler.hooks.beforeCompile.tapPromise(this.pluginName, () => this.generate());
-    // compiler.hooks.afterCompile.tapPromise(this.pluginName, compilation => this.includeDocuments(compilation));
+    compiler.hooks.afterCompile.tapPromise(this.pluginName, compilation => this.includeDocuments(compilation));
+  }
+
+  private initLegacy(options: CLIOptions) {
+    this.config = createConfigFromOldCli(options);
+    this.outputFiles = [resolve(process.cwd(), options.out)];
+    this.schemaLocations = [resolve(process.cwd(), options.schema)];
+    this.documentLocations = options.args;
+  }
+
+  private init(configPath?: string) {
+    this.config = createConfig(configPath);
+    this.outputFiles = Object.keys(this.config.generates);
+    this.schemaLocations = normalizeInstanceOrArray<Types.Schema>(this.config.schema);
+    this.documentLocations = normalizeInstanceOrArray(this.config.documents);
+
+    for (const filename of Object.keys(this.config.generates)) {
+      const config = normalizeOutputParam(this.config.generates[filename]);
+
+      if (config.schema) {
+        this.schemaLocations.push(...normalizeInstanceOrArray<Types.Schema>(config.schema));
+      }
+
+      if (config.documents) {
+        this.schemaLocations.push(...normalizeInstanceOrArray(config.documents));
+      }
+    }
   }
 
   private async generate() {
@@ -62,12 +81,14 @@ export class GraphQLCodegenPlugin {
   }
 
   private async shouldGenerate(): Promise<boolean> {
-    return true;
-    // return !(await this.didDocumentsChanged()) || !(await this.didSchemaChanged());
+    return !(await this.didDocumentsChanged()) || !(await this.didSchemaChanged());
   }
 
   private async didSchemaChanged(): Promise<boolean> {
-    const schemaChecksum = checksum(printSchema(await loadSchema(this.schemaLocations, this.config)));
+    const schemas = await Promise.all(
+      this.schemaLocations.map(async location => checksum(printSchema(await loadSchema(location, this.config))))
+    );
+    const schemaChecksum = schemas.join('');
 
     const changed = schemaChecksum === this.schemaChecksum;
     this.schemaChecksum = schemaChecksum;
@@ -75,17 +96,30 @@ export class GraphQLCodegenPlugin {
     return changed;
   }
 
-  // private async includeDocuments(/*compilation: compilation.Compilation*/) {
-  //   // const documents = this.options.args;
+  private async didDocumentsChanged(): Promise<boolean> {
+    if (this.documentLocations) {
+      const documents = await loadDocuments(this.documentLocations);
 
-  //   // if (!documents) {
-  //   //   return;
-  //   // }
+      const documentsChecksum = checksum(documents.map(doc => print(doc.content)).join('\n'));
 
-  //   // const found = await documentsFromGlobs(documents);
+      const changed = documentsChecksum === this.documentsChecksum;
+      this.documentsChecksum = documentsChecksum;
 
-  //   // found
-  //   //   .filter((file: any) => !compilation.fileDependencies.has(file))
-  //   //   .map(file => compilation.fileDependencies.add(file));
-  // }
+      return changed;
+    }
+
+    return false;
+  }
+
+  private async includeDocuments(compilation: compilation.Compilation) {
+    if (!this.documentLocations || !this.documentLocations.length) {
+      return;
+    }
+
+    const found = await documentsFromGlobs(this.documentLocations);
+
+    found
+      .filter((file: any) => !compilation.fileDependencies.has(file))
+      .map(file => compilation.fileDependencies.add(file));
+  }
 }
