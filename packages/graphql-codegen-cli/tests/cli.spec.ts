d39554c489a88948fcecf682d0bf886994082f28
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-08 16:32:07 +0100

    Schema root object is skipped (#816)

--- packages/graphql-codegen-cli/tests/cli.spec.ts
+++ packages/graphql-codegen-cli/tests/cli.spec.ts
@@ -85,6 +85,15 @@ describe('executeWithOptions', () => {
     expect(result.length).toBe(1);
   });
 
+  it('execute the correct results when using schema with graphql file and schema object in it', async () => {
+    const result = await executeWithOptions({
+      schema: '../../dev-test/test-schema/schema-with-root.graphql',
+      template: 'ts'
+    });
+
+    expect(result.length).toBe(1);
+  });
+
   it('execute the correct results when using custom config file', async () => {
     const result = await executeWithOptions({
       schema: '../../dev-test/githunt/schema.json',
