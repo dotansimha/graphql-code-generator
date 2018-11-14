0c5e69d5b546deeeb081830f03fc615adac71978
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-14 09:33:15 +0100

    pickMapper is no longer a handlebar hepler

--- packages/plugins/typescript-resolvers/src/index.ts
+++ packages/plugins/typescript-resolvers/src/index.ts
@@ -5,7 +5,6 @@ import * as Handlebars from 'handlebars';
 import * as rootTemplate from './root.handlebars';
 import * as resolver from './resolver.handlebars';
 import { getFieldResolverName, getFieldResolver, getFieldType } from './helpers';
-import { pickMapper } from './mappers';
 import { importMappers } from './import-mappers';
 import { importContext, getContext } from './context';
 import { getParentType } from './parent-type';
@@ -26,7 +25,6 @@ export const plugin: PluginFunction<TypeScriptServerResolversConfig> = async (
   Handlebars.registerPartial('resolver', resolver);
   Handlebars.registerHelper('getFieldResolverName', getFieldResolverName(convert));
   Handlebars.registerHelper('getFieldResolver', getFieldResolver(convert));
-  Handlebars.registerHelper('pickMapper', pickMapper);
   Handlebars.registerHelper('getParentType', getParentType(convert));
   Handlebars.registerHelper('getFieldType', getFieldType(convert));
   Handlebars.registerHelper('importMappers', importMappers);
