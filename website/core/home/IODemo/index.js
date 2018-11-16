59125fc8d5a382d66ae9ddf4e9cb3917ea3cbfa7
dotansimha <dotansimha@gmail.com>
2018-11-16 09:54:18 +0900

    fix live-demo url

--- website/core/home/IODemo/index.js
+++ website/core/home/IODemo/index.js
@@ -3,7 +3,7 @@ const React = require('react');
 const IODemo = () => (
   <div className="IODemo">
     <iframe
-      src={'http://localhost:3000'}
+      src={'/live-demo/'}
       style={{
         width: '100vw',
         height: '500px'
