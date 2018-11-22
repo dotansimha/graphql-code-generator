3407d2126eb785f702258a365f042a6f28ff2bda
dotansimha <dotansimha@gmail.com>
2018-11-22 15:26:49 +0900

    added tests for url schema loader fixed issue with headers

--- packages/graphql-codegen-cli/tests/__mocks__/fs.js
+++ packages/graphql-codegen-cli/tests/__mocks__/fs.js
@@ -1,9 +1,5 @@
-const path = require('path');
 const fs = jest.genMockFromModule('fs');
 
-// This is a custom function that our tests can use during setup to specify
-// what the files on the "mock" filesystem should look like when any of the
-// `fs` APIs are used.
 let mockFiles = {};
 
 function __setMockFiles(file, content) {
