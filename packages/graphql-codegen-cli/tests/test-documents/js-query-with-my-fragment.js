a048a245e549a4659a8aec29530cb5c74f0a8f2a
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-28 08:57:16 +0100

    With args

--- packages/graphql-codegen-cli/tests/test-documents/js-query-with-my-fragment.js
+++ packages/graphql-codegen-cli/tests/test-documents/js-query-with-my-fragment.js
@@ -2,8 +2,8 @@ import gql from 'graphql-tag';
 import { myFragment } from './js-my-fragment';
 
 export const query = gql`
-  query myQuery {
-    fieldA
+  query myQuery($a: String) {
+    fieldA(a: $a)
     fieldB {
       ...MyFragment
     }
