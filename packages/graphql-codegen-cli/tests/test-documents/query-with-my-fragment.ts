34f9279b2612cee886bc6e59f2343948a348ccce
Ben Awad <benawad97@gmail.com>
2018-12-03 06:39:17 -0800

    Fix/ben (#972)

--- packages/graphql-codegen-cli/tests/test-documents/query-with-my-fragment.ts
+++ packages/graphql-codegen-cli/tests/test-documents/query-with-my-fragment.ts
@@ -1,12 +1,25 @@
 import gql from 'graphql-tag';
-import { myFragment } from './my-fragment';
+import { myFragment, myFragment2, myFragment3 } from './my-fragment';
 
 export const query = gql`
-  query myQuery {
-    fieldA
+  query myQuery($a: String) {
+    fieldA(a: $a)
     fieldB {
       ...MyFragment
     }
   }
   ${myFragment}
+`;
+
+export const query2 = gql`
+  query myQuery2 {
+    fieldC {
+      item {
+        ...MyFragment2
+        ...MyFragment3
+      }
+    }
+  }
+  ${myFragment2}
+  ${myFragment3}
 `;
