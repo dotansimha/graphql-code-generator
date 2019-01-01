e3ca4099cea102bb4a31b08c2d574f4c648f21e1
Illya Klymov <xanf@xanf.me>
2019-01-01 23:20:51 +0200

    Fix optional sign for args with default values
    Fixes #1097

--- packages/plugins/flow/src/variables-to-object.ts
+++ packages/plugins/flow/src/variables-to-object.ts
@@ -1,10 +1,15 @@
-import { Kind, TypeNode, VariableNode, NameNode } from 'graphql';
+import { Kind, TypeNode, VariableNode, NameNode, ValueNode } from 'graphql';
 import { indent, getBaseTypeNode, wrapAstTypeWithModifiers } from './utils';
 import { BasicFlowVisitor } from './visitor';
 
 export class OperationVariablesToObject<
   Visitor extends BasicFlowVisitor,
-  DefinitionType extends { name?: NameNode; variable?: VariableNode; type: TypeNode }
+  DefinitionType extends {
+    name?: NameNode;
+    variable?: VariableNode;
+    type: TypeNode;
+    defaultValue?: ValueNode;
+  }
 > {
   constructor(private _visitorInstance: Visitor, private _variablesNode: ReadonlyArray<DefinitionType>) {}
 
@@ -35,12 +40,16 @@ export class OperationVariablesToObject<
           ? this._visitorInstance.scalars[typeName]
           : this._visitorInstance.convertName(typeName, true);
 
-        return indent(
-          `${this.getName(variable)}${variable.type.kind === Kind.NON_NULL_TYPE ? '' : '?'}: ${wrapAstTypeWithModifiers(
-            typeValue,
-            variable.type
-          )}`
-        );
+        const fieldName = this.getName(variable);
+        const fieldType = wrapAstTypeWithModifiers(typeValue, variable.type);
+
+        const hasDefaultValue = !!variable.defaultValue;
+        const isNonNullType = variable.type.kind === Kind.NON_NULL_TYPE;
+
+        const formattedFieldString = hasDefaultValue || isNonNullType ? fieldName : `${fieldName}?`;
+        const formattedTypeString = hasDefaultValue && !isNonNullType ? fieldType.substring(1) : fieldType;
+
+        return indent(`${formattedFieldString}: ${formattedTypeString}`);
       })
       .join(',\n');
 
