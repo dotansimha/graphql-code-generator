d1f1c3f3c48c007ddbe0b9d170292f824933d010
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-02-24 06:22:48 -0500

    Document stencil-apollo

--- packages/plugins/typescript-stencil-apollo/example/stencil.config.ts
+++ packages/plugins/typescript-stencil-apollo/example/stencil.config.ts
@@ -6,26 +6,5 @@ export const config: Config = {
       type: 'www'
     }
   ],
-  globalScript: 'src/global/app.ts',
-  commonjs: {
-    include: ['../node_modules/**'],
-    namedExports: {
-      '../node_modules/graphql-anywhere/lib/async.js': ['graphql'],
-      '../node_modules/graphql/language/parser.mjs': ['default'],
-      '../node_modules/apollo-utilities/lib/index.js': ['default']
-    },
-    extensions: ['.js', '.mjs']
-  } as any,
-  plugins: [
-    {
-      transform(source, id) {
-        return {
-          code: source
-            .replace(`var wrap = require('optimism').wrap;`, '')
-            .replace('export { wrap }', 'export { wrap } from "optimism";'),
-          id
-        };
-      }
-    }
-  ]
+  globalScript: 'src/global/app.ts'
 };
