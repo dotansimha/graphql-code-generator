fa1ed3dd21e7617383272c4e16c59797b9194ac1
Dotan Simha <dotansimha@gmail.com>
2019-02-25 16:54:22 +0200

    added validation for ts files

--- packages/plugins/typescript/tests/typescript.spec.ts
+++ packages/plugins/typescript/tests/typescript.spec.ts
@@ -1,29 +1,70 @@
 import 'graphql-codegen-core/dist/testing';
-import { parse, visit } from 'graphql';
+import { parse, visit, buildSchema } from 'graphql';
 import { TsVisitor } from '../src/visitor';
+import { validateTs } from './validate';
+import { plugin } from '../src/index';
 
 describe('TypeScript', () => {
-  describe('Object (type)', () => {
-    it('Should build type correctly', () => {
-      const ast = parse(`
+  describe('Config', () => {
+    it('Should build type correctly when specified with avoidOptionals config', async () => {
+      const schema = buildSchema(`
         type MyType {
           foo: String
           bar: String!
         }`);
-      const result = visit(ast, {
-        leave: new TsVisitor({
-          namingConvention: null,
-          scalars: { String: 'string' },
-          enumValues: {}
-        }) as any
-      });
+      const result = await plugin(schema, [], { avoidOptionals: true }, { outputFile: '' });
 
-      expect(result.definitions[0]).toBeSimilarStringTo(`
+      expect(result).toBeSimilarStringTo(`
         export type MyType = {
-          foo?: string,
+          foo: Maybe<string>,
           bar: string,
         };
       `);
+      validateTs(result);
+    });
+  });
+
+  describe('Object (type)', () => {
+    it('Should build type correctly', async () => {
+      const schema = buildSchema(`
+        type MyType {
+          foo: String
+          bar: String!
+        }`);
+      const result = await plugin(schema, [], {}, { outputFile: '' });
+
+      expect(result).toBeSimilarStringTo(`
+        export type MyType = {
+          foo?: Maybe<string>,
+          bar: string,
+        };
+      `);
+      validateTs(result);
+    });
+
+    it('Should build type correctly when implementing interface', async () => {
+      const schema = buildSchema(`
+        interface MyInterface {
+          foo: String!
+        }
+
+        type MyType implements MyInterface {
+          foo: String!
+        }
+        `);
+      const result = await plugin(schema, [], { avoidOptionals: true }, { outputFile: '' });
+
+      expect(result).toBeSimilarStringTo(`
+        export type MyInterface = {
+          foo: string,
+        };
+      `);
+      expect(result).toBeSimilarStringTo(`
+        export type MyType = MyInterface & {
+          foo: string,
+        };
+      `);
+      validateTs(result);
     });
   });
 });
