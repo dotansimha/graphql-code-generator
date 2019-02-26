0d7f40b2dd5bf11c5be555d7682b6682ca18f7e6
Dotan Simha <dotansimha@gmail.com>
2019-02-26 13:12:35 +0200

    WIP: move all common code from flow-documents. WIP: typescript-documents

--- packages/plugins/flow-documents/src/visitor.ts
+++ packages/plugins/flow-documents/src/visitor.ts
@@ -1,111 +1,27 @@
-import { GraphQLSchema, GraphQLObjectType, FragmentDefinitionNode, VariableDefinitionNode } from 'graphql';
-import {
-  wrapAstTypeWithModifiers,
-  DeclarationBlock,
-  toPascalCase,
-  OperationVariablesToObject,
-  DEFAULT_SCALARS
-} from 'graphql-codegen-visitor-plugin-common';
+import { GraphQLSchema } from 'graphql';
 import { ScalarsMap, FlowDocumentsPluginConfig } from './index';
-import { OperationDefinitionNode } from 'graphql';
-import { pascalCase } from 'change-case';
-import { SelectionSetToObject } from './selection-set-to-object';
-import { resolveExternalModuleAndFn } from 'graphql-codegen-plugin-helpers';
-import * as authBind from 'auto-bind';
+import { ParsedDocumentsConfig, BaseDocumentsVisitor } from 'graphql-codegen-visitor-plugin-common';
+import { FlowSelectionSetToObject } from './flow-selection-set-to-object';
+import { FlowOperationVariablesToObject } from '../../flow/src/flow-variables-to-object';
 
-export interface ParsedDocumentsConfig {
-  scalars: ScalarsMap;
-  addTypename: boolean;
-  convert: (str: string) => string;
-  typesPrefix: string;
+export interface FlowDocumentsParsedConfig extends ParsedDocumentsConfig {
   useFlowExactObjects: boolean;
   useFlowReadOnlyTypes: boolean;
 }
 
-export class FlowDocumentsVisitor {
-  private _parsedConfig: ParsedDocumentsConfig;
-  private _unnamedCounter = 1;
-
-  constructor(private _schema: GraphQLSchema, pluginConfig: FlowDocumentsPluginConfig) {
-    this._parsedConfig = {
-      addTypename: !pluginConfig.skipTypename,
-      scalars: { ...DEFAULT_SCALARS, ...(pluginConfig.scalars || {}) },
-      convert: pluginConfig.namingConvention ? resolveExternalModuleAndFn(pluginConfig.namingConvention) : toPascalCase,
-      typesPrefix: pluginConfig.typesPrefix || '',
-      useFlowExactObjects: pluginConfig.useFlowExactObjects || false,
-      useFlowReadOnlyTypes: pluginConfig.useFlowReadOnlyTypes || false
-    };
-    authBind(this);
-  }
-
-  public convertName(name: any, addPrefix = true): string {
-    return (addPrefix ? this._parsedConfig.typesPrefix : '') + this._parsedConfig.convert(name);
-  }
-
-  public getFragmentName(nodeName: string): string {
-    return this.convertName(nodeName + 'Fragment');
-  }
-
-  public get parsedConfig(): ParsedDocumentsConfig {
-    return this._parsedConfig;
-  }
-
-  public get schema(): GraphQLSchema {
-    return this._schema;
-  }
-
-  public get scalars(): ScalarsMap {
-    return this._parsedConfig.scalars;
-  }
-
-  public get addTypename(): boolean {
-    return this._parsedConfig.addTypename;
-  }
-
-  private handleAnonymouseOperation(name: string | null): string {
-    if (name) {
-      return this.convertName(name);
-    }
-
-    return this.convertName(`Unnamed_${this._unnamedCounter++}_`);
-  }
-
-  FragmentDefinition = (node: FragmentDefinitionNode): string => {
-    const fragmentRootType = this._schema.getType(node.typeCondition.name.value) as GraphQLObjectType;
-    const selectionSet = new SelectionSetToObject(this, fragmentRootType, node.selectionSet);
-
-    return new DeclarationBlock(this._parsedConfig)
-      .export()
-      .asKind('type')
-      .withName(this.getFragmentName(node.name.value))
-      .withContent(selectionSet.string).string;
-  };
-
-  OperationDefinition = (node: OperationDefinitionNode): string => {
-    const name = this.handleAnonymouseOperation(node.name && node.name.value ? node.name.value : null);
-    const operationRootType = this._schema.getType(pascalCase(node.operation)) as GraphQLObjectType;
-    const selectionSet = new SelectionSetToObject(this, operationRootType, node.selectionSet);
-    const visitedOperationVariables = new OperationVariablesToObject<VariableDefinitionNode>(
-      this._parsedConfig.scalars,
-      this.convertName,
-      node.variableDefinitions,
-      wrapAstTypeWithModifiers('?')
+export class FlowDocumentsVisitor extends BaseDocumentsVisitor<FlowDocumentsPluginConfig, FlowDocumentsParsedConfig> {
+  constructor(schema: GraphQLSchema, config: FlowDocumentsPluginConfig) {
+    super(
+      config,
+      {
+        useFlowExactObjects: config.useFlowExactObjects || false,
+        useFlowReadOnlyTypes: config.useFlowReadOnlyTypes || false
+      } as FlowDocumentsParsedConfig,
+      schema,
+      null,
+      FlowSelectionSetToObject
     );
 
-    const operationResult = new DeclarationBlock(this._parsedConfig)
-      .export()
-      .asKind('type')
-      .withName(this.convertName(name + pascalCase(node.operation)))
-      .withContent(selectionSet.string).string;
-
-    const operationVariables = !visitedOperationVariables
-      ? null
-      : new DeclarationBlock(this._parsedConfig)
-          .export()
-          .asKind('type')
-          .withName(this.convertName(name + pascalCase(node.operation) + 'Variables'))
-          .withBlock(visitedOperationVariables.string).string;
-
-    return [operationVariables, operationResult].filter(r => r).join('\n\n');
-  };
+    this.setVariablesTransformer(new FlowOperationVariablesToObject(this.scalars, this.convertName));
+  }
 }
