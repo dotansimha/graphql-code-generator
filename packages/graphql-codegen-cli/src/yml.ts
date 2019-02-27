4e3049ec85ef9e88779346753b9666b6bc00b457
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-02-27 05:48:39 -0500

    Support env variables in yml

--- packages/graphql-codegen-cli/src/yml.ts
+++ packages/graphql-codegen-cli/src/yml.ts
@@ -1,7 +1,19 @@
 import { Types, debugLog } from 'graphql-codegen-core';
 import { safeLoad } from 'js-yaml';
 
+export function interpolate<T extends { [key: string]: string }>(str: string, context: T) {
+  // tslint:disable-next-line:forin
+  for (let key in context) {
+    str = str.replace(new RegExp('\\$\\{' + key + '\\}', 'g'), context[key]);
+  }
+  return str;
+}
+
 export function parseConfigFile(ymlString: string): Types.Config {
+  if (typeof process !== 'undefined' && 'env' in process) {
+    debugLog(`[CLI] Interpolation of Environmental Variables`);
+    ymlString = interpolate(ymlString, process.env);
+  }
   debugLog(`[CLI] Parsing YAML file`);
   return safeLoad(ymlString) as Types.Config;
 }
