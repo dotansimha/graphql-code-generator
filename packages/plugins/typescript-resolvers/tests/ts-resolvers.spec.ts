af844182da96bf2d466febd3b866f24fb0230c20
Dotan Simha <dotansimha@gmail.com>
2019-02-28 11:32:31 +0200

    fixed tests for typescript packages fixes issues with scalars in common package

--- packages/plugins/typescript-resolvers/tests/ts-resolvers.spec.ts
+++ packages/plugins/typescript-resolvers/tests/ts-resolvers.spec.ts
@@ -1,6 +1,8 @@
 import 'graphql-codegen-core/dist/testing';
 import { makeExecutableSchema } from 'graphql-tools';
 import { plugin } from '../src';
+import { plugin as tsPlugin } from '../../typescript/src/index';
+import { validateTs } from '../../typescript/tests/validate';
 
 describe('TypeScript Resolvers Plugin', () => {
   const schema = makeExecutableSchema({
@@ -39,8 +41,14 @@ describe('TypeScript Resolvers Plugin', () => {
   `
   });
 
-  it('Should generate basic type resolvers', () => {
-    const result = plugin(schema, [], {}, { outputFile: '' });
+  const validate = async (content: string, config: any = {}, pluginSchema = schema) => {
+    const mergedContent = (await tsPlugin(pluginSchema, [], config, { outputFile: '' })) + '\n' + content;
+
+    validateTs(mergedContent);
+  };
+
+  it('Should generate basic type resolvers', async () => {
+    const result = await plugin(schema, [], {}, { outputFile: '' });
 
     expect(result).toBeSimilarStringTo(`
     export type MyDirectiveDirectiveResolver<Result> = DirectiveResolverFn<Result, {   arg?: Maybe<number>,
@@ -80,26 +88,34 @@ describe('TypeScript Resolvers Plugin', () => {
       somethingChanged?: SubscriptionResolver<Maybe<MyOtherType>, ParentType, Context>,
     }
     `);
+
+    await validate(result);
   });
 
-  it('Should generate the correct imports when schema has scalars', () => {
-    const result = plugin(makeExecutableSchema({ typeDefs: `scalar MyScalar` }), [], {}, { outputFile: '' });
+  it('Should generate the correct imports when schema has scalars', async () => {
+    const testSchema = makeExecutableSchema({ typeDefs: `scalar MyScalar` });
+    const result = await plugin(testSchema, [], {}, { outputFile: '' });
 
-    expect(result).toBeSimilarStringTo(`import { GraphQLResolveInfo, GraphQLScalarTypeConfig } from 'graphql';`);
+    expect(result).toBeSimilarStringTo(
+      `import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';`
+    );
+    await validate(result, {}, schema);
   });
 
-  it('Should generate the correct imports when schema has no scalars', () => {
-    const result = plugin(makeExecutableSchema({ typeDefs: `type MyType { f: String }` }), [], {}, { outputFile: '' });
+  it('Should generate the correct imports when schema has no scalars', async () => {
+    const testSchema = makeExecutableSchema({ typeDefs: `type MyType { f: String }` });
+    const result = await plugin(testSchema, [], {}, { outputFile: '' });
 
     expect(result).not.toBeSimilarStringTo(`import { GraphQLResolveInfo, GraphQLScalarTypeConfig } from 'graphql';`);
+    await validate(result, {}, testSchema);
   });
 
-  it('Should generate basic type resolvers with mapping', () => {
-    const result = plugin(
+  it('Should generate basic type resolvers with mapping', async () => {
+    const result = await plugin(
       schema,
       [],
       {
-        mapping: {
+        mappers: {
           MyOtherType: 'MyCustomOtherType'
         }
       },
@@ -144,16 +160,69 @@ describe('TypeScript Resolvers Plugin', () => {
       somethingChanged?: SubscriptionResolver<Maybe<MyCustomOtherType>, ParentType, Context>,
     }
     `);
+    await validate(`type MyCustomOtherType = {}\n${result}`);
   });
 
-  it('Should generate the correct resolver args type names when typesPrefix is specified', () => {
-    const result = plugin(
-      makeExecutableSchema({ typeDefs: `type MyType { f(a: String): String }` }),
+  it('Should generate basic type resolvers with external mappers', async () => {
+    const result = await plugin(
+      schema,
       [],
-      { typesPrefix: 'T' },
+      {
+        mappers: {
+          MyOtherType: './my-file#MyCustomOtherType'
+        }
+      },
       { outputFile: '' }
     );
 
+    expect(result).toBeSimilarStringTo(`import { MyCustomOtherType } from './my-file';`);
+    expect(result).toBeSimilarStringTo(`
+    export type MyDirectiveDirectiveResolver<Result> = DirectiveResolverFn<Result, {   arg?: Maybe<number>,
+      arg2?: Maybe<string>, arg3?: Maybe<boolean> }, any>;
+
+    export interface MyOtherTypeResolvers<Context = any, ParentType = MyCustomOtherType> {
+      bar?: Resolver<string, ParentType, Context>,
+    }
+
+    export interface MyScalarScalarConfig extends GraphQLScalarTypeConfig<MyScalar, any> {
+      name: 'MyScalar'
+    }
+
+    export interface MyTypeResolvers<Context = any, ParentType = MyType> {
+      foo?: Resolver<string, ParentType, Context>,
+      otherType?: Resolver<Maybe<MyCustomOtherType>, ParentType, Context>,
+      withArgs?: Resolver<Maybe<string>, ParentType, Context, MyTypeWithArgsArgs>,
+    }
+
+    export interface MyUnionResolvers<Context = any, ParentType = MyUnion> {
+      __resolveType: TypeResolveFn<'MyType' | 'MyOtherType'>
+    }
+
+    export interface NodeResolvers<Context = any, ParentType = Node> {
+      __resolveType: TypeResolveFn<'SomeNode'>
+    }
+
+    export interface QueryResolvers<Context = any, ParentType = Query> {
+      something?: Resolver<MyType, ParentType, Context>,
+    }
+
+    export interface SomeNodeResolvers<Context = any, ParentType = SomeNode> {
+      id?: Resolver<string, ParentType, Context>,
+    }
+
+    export interface SubscriptionResolvers<Context = any, ParentType = Subscription> {
+      somethingChanged?: SubscriptionResolver<Maybe<MyCustomOtherType>, ParentType, Context>,
+    }
+    `);
+    await validate(result);
+  });
+
+  it('Should generate the correct resolver args type names when typesPrefix is specified', async () => {
+    const testSchema = makeExecutableSchema({ typeDefs: `type MyType { f(a: String): String }` });
+    const config = { typesPrefix: 'T' };
+    const result = await plugin(testSchema, [], config, { outputFile: '' });
+
     expect(result).toBeSimilarStringTo(`f?: Resolver<Maybe<string>, ParentType, Context, TMyTypeFArgs>,`);
+    await validate(result, config, testSchema);
   });
 });
