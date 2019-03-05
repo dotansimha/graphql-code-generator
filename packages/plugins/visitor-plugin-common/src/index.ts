927c47b2cd9500eeb87c5cdecf33331b3ff839af
Dotan Simha <dotansimha@gmail.com>
2019-03-05 17:01:16 +0200

    done with apollo angular

--- packages/plugins/visitor-plugin-common/src/index.ts
+++ packages/plugins/visitor-plugin-common/src/index.ts
@@ -6,6 +6,7 @@ export * from './base-visitor';
 export * from './base-types-visitor';
 export * from './base-documents-visitor';
 export * from './base-resolvers-visitor';
+export * from './client-side-base-visitor';
 
 export * from './variables-to-object';
 export * from './selection-set-to-object';
