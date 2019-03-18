1871b6a4dcfe9afbba5f566a41cee2497ac71f45
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-03-18 18:54:45 +0100

    No more optional arguments (#1487)
    If someone wants to have easier testing then use a wrapper type that turn all arguments to optional as we do it in our projects

--- packages/plugins/typescript-resolvers/src/index.ts
+++ packages/plugins/typescript-resolvers/src/index.ts
@@ -32,10 +32,10 @@ export type ArrayOrIterable<T> = Array<T> | Iterable<T>;
 export type WithIndex<TObject> = TObject & {[key: string]: void};
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
-  parent?: TParent,
-  args?: TArgs,
-  context?: TContext,
-  info?: GraphQLResolveInfo
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => Promise<TResult> | TResult;
 
 export type StitchingResolver<TResult, TParent, TContext, TArgs> = {
@@ -48,17 +48,17 @@ export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
   | StitchingResolver<TResult, TParent, TContext, TArgs>;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
-  parent?: TParent,
-  args?: TArgs,
-  context?: TContext,
-  info?: GraphQLResolveInfo
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
-  parent?: TParent,
-  args?: TArgs,
-  context?: TContext,
-  info?: GraphQLResolveInfo
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
 export interface ISubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
@@ -71,19 +71,19 @@ export type SubscriptionResolver<TResult, TParent = {}, TContext = {}, TArgs = {
   | ISubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
-  parent?: TParent,
-  context?: TContext,
-  info?: GraphQLResolveInfo
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => Maybe<TTypes>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
 export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
-  next?: NextResolverFn<TResult>,
-  parent?: TParent,
-  args?: TArgs,
-  context?: TContext,
-  info?: GraphQLResolveInfo
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
 export type ResolversObject<TObject> = WithIndex<TObject>;
