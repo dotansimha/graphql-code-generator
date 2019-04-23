27fea35cc38c1178ed1a238d87d8e59fe13f627d
Dotan Simha <dotansimha@gmail.com>
2019-04-23 10:41:17 +0300

    fixes for inline fragments (#1761)

--- packages/plugins/typescript/compatibility/src/selection-set-to-types.ts
+++ packages/plugins/typescript/compatibility/src/selection-set-to-types.ts
@@ -33,14 +33,14 @@ export function selectionSetToTypes(
   const parentType = schema.getType(parentTypeName);
   const typeName = baseVisitor.convertName(fieldName);
 
-  if (isObjectType(parentType) || isInterfaceType(parentType)) {
-    if (selectionSet && selectionSet.selections && selectionSet.selections.length) {
-      const typeToUse = handleTypeNameDuplicates(result, typeName, typesPrefix);
-      result[typeToUse] = { export: 'type', name: stack };
+  if (selectionSet && selectionSet.selections && selectionSet.selections.length) {
+    const typeToUse = handleTypeNameDuplicates(result, typeName, typesPrefix);
+    result[typeToUse] = { export: 'type', name: stack };
 
-      for (const selection of selectionSet.selections) {
-        switch (selection.kind) {
-          case Kind.FIELD: {
+    for (const selection of selectionSet.selections) {
+      switch (selection.kind) {
+        case Kind.FIELD: {
+          if (isObjectType(parentType) || isInterfaceType(parentType)) {
             const selectionName = selection.alias && selection.alias.value ? selection.alias.value : selection.name.value;
             const field = parentType.getFields()[selection.name.value];
             const baseType = getBaseType(field.type);
@@ -52,29 +52,29 @@ export function selectionSetToTypes(
             const wrapArrayWithNonNull = baseVisitor.config.strict;
             const newStack = isArray && wrapArrayWithNonNull ? `(NonNullable<${arrayInnerType}>)` : arrayInnerType;
             selectionSetToTypes(typesPrefix, baseVisitor, schema, baseType.name, newStack, selectionName, selection.selectionSet, result);
-
-            break;
           }
 
-          case Kind.FRAGMENT_SPREAD: {
-            const fragmentName = baseVisitor.convertName(selection.name.value, { suffix: 'Fragment' });
-            result[typeToUse] = { export: 'type', name: fragmentName };
+          break;
+        }
 
-            break;
-          }
+        case Kind.FRAGMENT_SPREAD: {
+          const fragmentName = baseVisitor.convertName(selection.name.value, { suffix: 'Fragment' });
+          result[typeToUse] = { export: 'type', name: fragmentName };
 
-          case Kind.INLINE_FRAGMENT: {
-            const typeCondition = selection.typeCondition.name.value;
-            const fragmentName = baseVisitor.convertName(typeCondition, { suffix: 'InlineFragment' });
-            let inlineFragmentValue = `{ __typename: '${typeCondition}' } & Pick<${stack}, ${selection.selectionSet.selections
-              .map(subSelection => (subSelection.kind === Kind.FIELD ? `'${subSelection.name.value}'` : null))
-              .filter(a => a)
-              .join(' | ')}>`;
+          break;
+        }
 
-            selectionSetToTypes(typesPrefix, baseVisitor, schema, typeCondition, `(${inlineFragmentValue})`, fragmentName, selection.selectionSet, result);
+        case Kind.INLINE_FRAGMENT: {
+          const typeCondition = selection.typeCondition.name.value;
+          const fragmentName = baseVisitor.convertName(typeCondition, { suffix: 'InlineFragment' });
+          let inlineFragmentValue = `{ __typename: '${typeCondition}' } & Pick<${stack}, ${selection.selectionSet.selections
+            .map(subSelection => (subSelection.kind === Kind.FIELD ? `'${subSelection.name.value}'` : null))
+            .filter(a => a)
+            .join(' | ')}>`;
 
-            break;
-          }
+          selectionSetToTypes(typesPrefix, baseVisitor, schema, typeCondition, `(${inlineFragmentValue})`, fragmentName, selection.selectionSet, result);
+
+          break;
         }
       }
     }
