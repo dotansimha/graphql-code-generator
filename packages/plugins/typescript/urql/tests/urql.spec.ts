9ecea76237755a4ea3f6e18b15e37d2aed71d35c
Max Stoiber <mxstbr@github.com>
2019-05-13 10:26:08 +0200

    Fix urql typescript types (#1855)
    These would throw an error saying "Required field `query` not provided".
    Need to omit that.

--- packages/plugins/typescript/urql/tests/urql.spec.ts
+++ packages/plugins/typescript/urql/tests/urql.spec.ts
@@ -503,7 +503,7 @@ query MyFeed {
       );
 
       expect(content).toBeSimilarStringTo(`
-export function useFeedQuery(options: Urql.UseQueryArgs<FeedQueryVariables> = {}) {
+export function useFeedQuery(options: Omit<Urql.UseQueryArgs<FeedQueryVariables>, 'query'> = {}) {
   return Urql.useQuery<FeedQuery>({ query: FeedDocument, ...options });
 };`);
 
@@ -553,7 +553,7 @@ export function useSubmitRepositoryMutation() {
       );
 
       expect(content).toBeSimilarStringTo(`
-export function useListenToCommentsSubscription(options: Urql.UseSubscriptionArgs<ListenToCommentsSubscriptionVariables> = {}) {
+export function useListenToCommentsSubscription(options: Omit<Urql.UseSubscriptionArgs<ListenToCommentsSubscriptionVariables>, 'query'> = {}) {
   return Urql.useSubscription<ListenToCommentsSubscription>({ query: ListenToCommentsDocument, ...options });
 };`);
       await validateTypeScript(content, schema, docs, {});
