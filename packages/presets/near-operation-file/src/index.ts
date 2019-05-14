5f6d8dfc98145216bf5cee77b07c4a33a1ff2328
Dotan Simha <dotansimha@gmail.com>
2019-05-14 15:31:09 +0300

    added docs remove temp files

--- packages/presets/near-operation-file/src/index.ts
+++ packages/presets/near-operation-file/src/index.ts
@@ -6,11 +6,82 @@ import { Kind, FragmentDefinitionNode } from 'graphql';
 import { appendExtensionToFilePath, extractExternalFragmentsInUse, resolveRelativeImport } from './utils';
 
 export type NearOperationFileConfig = {
+  /**
+   * @name baseTypesPath
+   * @type string
+   * @description Required, should point to the base schema types file.
+   * The key of the output is used a the base path for this file.
+   *
+   * @example
+   * ```yml
+   * generates:
+   * src/:
+   *  preset: near-operation-file
+   *  presetConfig:
+   *    baseTypesPath: types.ts
+   *  plugins:
+   *    - typescript-operations
+   * ```
+   */
   baseTypesPath: string;
+  /**
+   * @name extension
+   * @type string
+   * @description Optional, sets the extension for the generated files. Use this to override the extension if you are using plugins that requires a different type of extensions (such as `typescript-react-apollo`)
+   * @default .generates.ts
+   *
+   * @example
+   * ```yml
+   * generates:
+   * src/:
+   *  preset: near-operation-file
+   *  presetConfig:
+   *    baseTypesPath: types.ts
+   *    extension: .generated.tsx
+   *  plugins:
+   *    - typescript-operations
+   *    - typescript-react-apollo
+   * ```
+   */
   extension?: string;
+  /**
+   * @name cwd
+   * @type string
+   * @description Optional, override the `cwd` of the execution. We are using `cwd` to figure out the imports between files. Use this if your execuion path is not your project root directory.
+   * @default process.cwd()
+   *
+   * @example
+   * ```yml
+   * generates:
+   * src/:
+   *  preset: near-operation-file
+   *  presetConfig:
+   *    baseTypesPath: types.ts
+   *    cwd: /some/path
+   *  plugins:
+   *    - typescript-operations
+   * ```
+   */
   cwd?: string;
+  /**
+   * @name importTypesNamespace
+   * @type string
+   * @description Optional, override the name of the import namespace used to import from the `baseTypesPath` file.
+   * @default Types
+   *
+   * @example
+   * ```yml
+   * generates:
+   * src/:
+   *  preset: near-operation-file
+   *  presetConfig:
+   *    baseTypesPath: types.ts
+   *    importTypesNamespace: SchemaTypes
+   *  plugins:
+   *    - typescript-operations
+   * ```
+   */
   importTypesNamespace?: string;
-  addFragmentsImport?: boolean;
 };
 
 export type FragmentNameToFile = { [fragmentName: string]: { filePath: string; importName: string; onType: string; node: FragmentDefinitionNode } };
