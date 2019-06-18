8b80a89d88f634a00a1ff3d190753e0ed35db006
n1ru4l <laurinquast@googlemail.com>
2019-06-18 13:15:56 +0200

    fix test by altering implementation

--- packages/plugins/typescript/operations/src/ts-operation-variables-to-object.ts
+++ packages/plugins/typescript/operations/src/ts-operation-variables-to-object.ts
@@ -1,6 +1,13 @@
 import { TypeScriptOperationVariablesToObject as TSOperationVariablesToObject } from '@graphql-codegen/typescript';
 
 export class TypeScriptOperationVariablesToObject extends TSOperationVariablesToObject {
+  protected formatFieldString(fieldName: string, isNonNullType: boolean, hasDefaultValue: boolean): string {
+    if (hasDefaultValue) {
+      return `${fieldName}?`;
+    }
+
+    return super.formatFieldString(fieldName, isNonNullType, hasDefaultValue);
+  }
   protected formatTypeString(fieldType: string, isNonNullType: boolean, hasDefaultValue: boolean): string {
     return fieldType;
   }
