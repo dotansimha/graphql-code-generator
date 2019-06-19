d6b07f60a24b7f493d62d9b8138630554cbc8051
Laurin Quast <laurinquast@googlemail.com>
2019-06-19 09:46:38 +0200

    fix(typescript-operations): wrong type for variable with default value ( #1934) (#2018)
    * modify test to be failing (#1934)
    
    * fix test by altering implementation
    
    * change implementation according to feedback by @dotansimha
    
    * add failing test for avoidOptionals setting
    
    * fix failing test by altering the implementation

--- packages/plugins/typescript/typescript/tests/typescript.spec.ts
+++ packages/plugins/typescript/typescript/tests/typescript.spec.ts
@@ -1361,15 +1361,32 @@ describe('TypeScript', () => {
     });
 
     it('Should generate correctly types for field arguments - with default value', async () => {
-      const schema = buildSchema(`type MyType { foo(a: String = "default", b: String! = "default", c: String): String }`);
+      const schema = buildSchema(`type MyType { foo(a: String = "default", b: String! = "default", c: String, d: String!): String }`);
       const result = (await plugin(schema, [], {}, { outputFile: '' })) as Types.ComplexPluginOutput;
 
       expect(result.content).toBeSimilarStringTo(`
         export type MyTypeFooArgs = {
-          a: Scalars['String'],
-          b: Scalars['String'],
-          c?: Maybe<Scalars['String']>
+          a?: Maybe<Scalars['String']>,
+          b?: Scalars['String'],
+          c?: Maybe<Scalars['String']>,
+          d: Scalars['String']
         };
+    `);
+
+      validateTs(result);
+    });
+
+    it('Should generate correctly types for field arguments - with default value and avoidOptionals option set to true', async () => {
+      const schema = buildSchema(`type MyType { foo(a: String = "default", b: String! = "default", c: String, d: String!): String }`);
+      const result = (await plugin(schema, [], { avoidOptionals: true }, { outputFile: '' })) as Types.ComplexPluginOutput;
+
+      expect(result.content).toBeSimilarStringTo(`
+        export type MyTypeFooArgs = {
+          a?: Maybe<Scalars['String']>,
+          b?: Scalars['String'],
+          c: Maybe<Scalars['String']>,
+          d: Scalars['String']
+      };
     `);
 
       validateTs(result);
