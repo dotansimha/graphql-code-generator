5c095655e7f99d35e00240f981884717d59eb1e0
Dotan Simha <dotansimha@gmail.com>
2019-08-04 13:02:04 +0300

    Fixes for issues with js objects and scalars (#2286)
    * fixes issues with objects and scalars
    
    * fix

--- packages/plugins/other/visitor-plugin-common/src/scalars.ts
+++ packages/plugins/other/visitor-plugin-common/src/scalars.ts
@@ -1,7 +1,21 @@
-export const DEFAULT_SCALARS = {
+import { ScalarsMap } from './types';
+
+export const DEFAULT_SCALARS: ScalarsMap = {
   ID: 'string',
   String: 'string',
   Boolean: 'boolean',
   Int: 'number',
   Float: 'number',
 };
+
+export function normalizeScalars(scalars: ScalarsMap): ScalarsMap {
+  return Object.keys(scalars).reduce(
+    (prev, key) => {
+      return {
+        ...prev,
+        [key]: typeof scalars[key] === 'object' ? JSON.stringify(scalars[key]) : scalars[key],
+      };
+    },
+    {} as ScalarsMap
+  );
+}
