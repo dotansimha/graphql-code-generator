7206bd0c902c67de3cbb31506b672d8248056e0a
Dotan Simha <dotansimha@gmail.com>
2019-08-07 09:23:50 +0300

    fix for flow and dedupe issues

--- packages/plugins/flow/operations/src/flow-selection-set-to-object.ts
+++ packages/plugins/flow/operations/src/flow-selection-set-to-object.ts
@@ -15,7 +15,7 @@ export class FlowSelectionSetToObject extends SelectionSetToObject {
     _parentSchemaType?: GraphQLNamedType,
     _selectionSet?: SelectionSetNode
   ) {
-    super(_scalars, _schema, _convertName, _addTypename, _preResolveTypes, _nonOptionalTypename, _loadedFragments, _visitorConfig.namespacedImportName, _parentSchemaType, _selectionSet);
+    super(_scalars, _schema, _convertName, _addTypename, _preResolveTypes, _nonOptionalTypename, _loadedFragments, _visitorConfig.namespacedImportName, _visitorConfig.dedupeOperationSuffix, _parentSchemaType, _selectionSet);
   }
 
   public createNext(parentSchemaType: GraphQLNamedType, selectionSet: SelectionSetNode): SelectionSetToObject {
