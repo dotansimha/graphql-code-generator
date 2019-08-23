c38e7c33c5f183382b109d547e7b3a374df92ce8
n1ru4l <laurinquast@googlemail.com>
2019-08-24 01:21:00 +0200

    fix(visitor-plugin-common): remove code that caused #2436

--- packages/plugins/other/visitor-plugin-common/src/selection-set-to-object.ts
+++ packages/plugins/other/visitor-plugin-common/src/selection-set-to-object.ts
@@ -349,31 +349,8 @@ export class SelectionSetToObject {
         } else {
           let selectedField: GraphQLField<any, any, any> = null;
 
-          if (isObjectType(this._parentSchemaType)) {
-            const fields = this._parentSchemaType.getFields();
-            selectedField = fields[selectionNode.name.value];
-          } else if (isInterfaceType(this._parentSchemaType)) {
-            const fields = this._parentSchemaType.getFields();
-            selectedField = fields[selectionNode.name.value];
-            if (!selectedField) {
-              const possibleTypes = this._getPossibleTypes(this._parentSchemaType);
-              for (const possibleType of possibleTypes) {
-                selectedField = possibleType.getFields()[selectionNode.name.value];
-                if (selectedField) {
-                  break;
-                }
-              }
-            }
-          } else if (isUnionType(this._parentSchemaType)) {
-            const types = this._parentSchemaType.getTypes();
-            for (const type of types) {
-              const fields = type.getFields();
-              selectedField = fields[selectionNode.name.value];
-              if (selectedField) {
-                break;
-              }
-            }
-          }
+          const fields = parentSchemaType.getFields();
+          selectedField = fields[selectionNode.name.value];
 
           if (isMetadataFieldName(selectionNode.name.value)) {
             selectedField = metadataFieldMap[selectionNode.name.value];
