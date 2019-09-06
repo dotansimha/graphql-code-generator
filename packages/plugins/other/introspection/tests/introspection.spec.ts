033315df3fa83af82f6373070df949cf8e056714
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-09-06 11:24:50 +0200

    Introspection

--- packages/plugins/other/introspection/tests/introspection.spec.ts
+++ packages/plugins/other/introspection/tests/introspection.spec.ts
@@ -1,4 +1,5 @@
-import { buildSchema, introspectionFromSchema } from 'graphql';
+import { buildSchema, introspectionFromSchema, parse, IntrospectionQuery, IntrospectionObjectType } from 'graphql';
+import { codegen } from '@graphql-codegen/core';
 import { plugin } from '../src';
 
 describe('Introspection template', () => {
@@ -16,5 +17,63 @@ describe('Introspection template', () => {
     const content = await plugin(schema, [], {}, { outputFile: '' });
     const introspection = JSON.stringify(introspectionFromSchema(schema, { descriptions: true }));
     expect(introspection).toEqual(content);
+  });
+
+  it('should support Apollo Federation', async () => {
+    const schema = parse(/* GraphQL */ `
+      type Character @key(fields: "id") {
+        id: ID
+        name: String
+      }
+
+      type Jedi @key(fields: "id") {
+        id: ID
+        side: String
+      }
+
+      type Droid @key(fields: "id") {
+        id: ID
+        model: String
+      }
+
+      union People = Character | Jedi | Droid
+
+      type Query {
+        allPeople: [People]
+      }
+    `);
+
+    const content = await codegen({
+      filename: 'foo.json',
+      schema,
+      documents: [],
+      plugins: [
+        {
+          introspection: {},
+        },
+      ],
+      config: {
+        federation: true,
+      },
+      pluginMap: {
+        introspection: {
+          plugin,
+        },
+      },
+    });
+
+    const introspection: IntrospectionQuery = JSON.parse(content);
+    const types = introspection.__schema.types;
+    const queryType = types.find(type => type.name === introspection.__schema.queryType.name) as IntrospectionObjectType;
+
+    // scalar _Any
+    expect(types.some(type => type.name === '_Any')).toBe(false);
+    // union _Entity
+    expect(types.some(type => type.name === '_Entity')).toBe(false);
+    // type _Service
+    expect(types.some(type => type.name === '_Service')).toBe(false);
+    // type Query { _entities, _service }
+    expect(queryType.fields.some(f => f.name === '_entities')).toBe(false);
+    expect(queryType.fields.some(f => f.name === '_service')).toBe(false);
   });
 });
