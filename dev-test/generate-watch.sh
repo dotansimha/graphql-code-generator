97f8a95c9caaa6aeccc118c7b02209cfc71abbd7
Dotan Simha <dotansimha@gmail.com>
2019-10-15 14:46:15 +0300

    Fix issues with dotenv (#2758)

--- dev-test/generate-watch.sh
+++ dev-test/generate-watch.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-DOTENV_CONFIG_PATH=$PWD/dev-test/.env node --require dotenv/config packages/graphql-codegen-cli/dist/commonjs/bin.js dotenv_config_path=./dev-test/.env --config ./dev-test/codegen.yml -w
+DOTENV_CONFIG_PATH=$PWD/dev-test/.env node packages/graphql-codegen-cli/dist/commonjs/bin.js --require dotenv/config --config ./dev-test/codegen.yml -w
