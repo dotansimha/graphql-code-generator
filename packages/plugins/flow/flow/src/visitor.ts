f11f217d0e3b8a789eb1e418538f737e0859867d
Dotan Simha <dotansimha@gmail.com>
2019-11-24 17:10:04 +0200

    fix for flow issues with exact

--- packages/plugins/flow/flow/src/visitor.ts
+++ packages/plugins/flow/flow/src/visitor.ts
@@ -100,7 +100,7 @@ export class FlowVisitor extends BaseTypesVisitor<FlowPluginConfig, FlowPluginPa
       return allFields.join('\n');
     }
 
-    return `...{\n${allFields.map(s => indent(s)).join('\n')}\n  }`;
+    return `...{${this.config.useFlowExactObjects ? '|' : ''}\n${allFields.map(s => indent(s)).join('\n')}\n  ${this.config.useFlowExactObjects ? '|' : ''}}`;
   }
 
   EnumTypeDefinition(node: EnumTypeDefinitionNode): string {
