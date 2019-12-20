c905f6a0c9e2c934c6be844c59a417dd952156e3
Iskander Broere <iskanderbroere@gmail.com>
2019-12-20 16:30:24 +0100

    test: add correct variables types

--- packages/plugins/typescript/vue-apollo/tests/vue-apollo.spec.ts
+++ packages/plugins/typescript/vue-apollo/tests/vue-apollo.spec.ts
@@ -369,7 +369,7 @@ query MyFeed {
         }
       )) as Types.ComplexPluginOutput;
       expect(content.content).toBeSimilarStringTo(`
-export function useFeedQuery(variables?: FeedQueryVariables, baseOptions?: VueApolloComposable.UseQueryOptions<FeedQuery, FeedQueryVariables>) {
+export function useFeedQuery(variables?: FeedQueryVariables | VueCompositionApi.Ref<FeedQueryVariables> | VueApolloComposable.ReactiveFunction<FeedQueryVariables>, baseOptions?: VueApolloComposable.UseQueryOptions<FeedQuery, FeedQueryVariables>) {
   return VueApolloComposable.useQuery<FeedQuery, FeedQueryVariables>(FeedDocument, variables, baseOptions);
 }`);
 
@@ -414,7 +414,7 @@ export function useSubmitRepositoryMutation(baseOptions?: VueApolloComposable.Us
       )) as Types.ComplexPluginOutput;
 
       expect(content.content).toBeSimilarStringTo(`
-export function useFeedQuery(variables?: FeedQueryVariables, baseOptions?: VueApolloComposable.UseQueryOptions<FeedQuery, FeedQueryVariables>) {
+export function useFeedQuery(variables?: FeedQueryVariables | VueCompositionApi.Ref<FeedQueryVariables> | VueApolloComposable.ReactiveFunction<FeedQueryVariables>, baseOptions?: VueApolloComposable.UseQueryOptions<FeedQuery, FeedQueryVariables>) {
   return VueApolloComposable.useQuery<FeedQuery, FeedQueryVariables>(FeedQueryDocument, variables, baseOptions);
 }`);
 
