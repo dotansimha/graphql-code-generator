b06aaced310ca8411360395544c6d785c44304a7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-12-31 14:29:09 +0300

    Update graphql-toolkit

--- packages/graphql-codegen-cli/src/load.ts
+++ packages/graphql-codegen-cli/src/load.ts
@@ -1,6 +1,6 @@
-import { loadSchemaUsingLoaders, loadDocumentsUsingLoaders as loadDocumentsToolkit, UnnormalizedTypeDefPointer, loadTypedefsUsingLoaders } from '@graphql-toolkit/core';
+import { loadSchema as loadSchemaToolkit, loadDocuments as loadDocumentsToolkit, UnnormalizedTypeDefPointer } from '@graphql-toolkit/core';
 import { Types } from '@graphql-codegen/plugin-helpers';
-import { GraphQLSchema, DocumentNode } from 'graphql';
+import { GraphQLSchema } from 'graphql';
 import { DetailedError } from '@graphql-codegen/core';
 import { CodeFileLoader } from '@graphql-toolkit/code-file-loader';
 import { GitLoader } from '@graphql-toolkit/git-loader';
@@ -10,19 +10,19 @@ import { JsonFileLoader } from '@graphql-toolkit/json-file-loader';
 import { UrlLoader } from '@graphql-toolkit/url-loader';
 import { ApolloEngineLoader } from '@graphql-toolkit/apollo-engine-loader';
 import { PrismaLoader } from '@graphql-toolkit/prisma-loader';
-import { mergeTypeDefs } from '@graphql-toolkit/schema-merging';
 import { join } from 'path';
 
-export const loadSchema = async (schemaPointers: UnnormalizedTypeDefPointer, config: Types.Config, out?: 'GraphQLSchema' | 'DocumentNode'): Promise<GraphQLSchema | DocumentNode> => {
+export const loadSchema = async (schemaPointers: UnnormalizedTypeDefPointer, config: Types.Config): Promise<GraphQLSchema> => {
   try {
     const loaders = [new CodeFileLoader(), new GitLoader(), new GithubLoader(), new GraphQLFileLoader(), new JsonFileLoader(), new UrlLoader(), new ApolloEngineLoader(), new PrismaLoader()];
-    if (out === 'DocumentNode') {
-      const documents = await loadTypedefsUsingLoaders(loaders, schemaPointers, config);
-      return mergeTypeDefs(documents.map(doc => doc.document));
-    } else {
-      const schema = await loadSchemaUsingLoaders(loaders, schemaPointers, config);
-      return schema;
-    }
+
+    const schema = await loadSchemaToolkit(schemaPointers, {
+      assumeValidSDL: true,
+      loaders,
+      sort: true,
+      ...config,
+    });
+    return schema;
   } catch (e) {
     throw new DetailedError(
       'Failed to load schema',
@@ -47,20 +47,26 @@ export const loadSchema = async (schemaPointers: UnnormalizedTypeDefPointer, con
 };
 
 export const loadDocuments = async (documentPointers: UnnormalizedTypeDefPointer | UnnormalizedTypeDefPointer[], config: Types.Config): Promise<Types.DocumentFile[]> => {
-  const loadedFromToolkit = await loadDocumentsToolkit([new CodeFileLoader(), new GitLoader(), new GithubLoader(), new GraphQLFileLoader()], documentPointers, {
+  const loaders = [new CodeFileLoader(), new GitLoader(), new GithubLoader(), new GraphQLFileLoader()];
+
+  const loadedFromToolkit = await loadDocumentsToolkit(documentPointers, {
     ignore: Object.keys(config.generates).map(p => join(process.cwd(), p)),
+    loaders,
+    sort: true,
     ...config,
   });
 
-  return loadedFromToolkit.map(({ location, document }) => ({ filePath: location, content: document })).sort((a, b) => {
-    if (a.filePath < b.filePath) {
-      return -1;
-    }
+  return loadedFromToolkit
+    .map(({ location, document }) => ({ filePath: location, content: document }))
+    .sort((a, b) => {
+      if (a.filePath < b.filePath) {
+        return -1;
+      }
 
-    if (a.filePath > b.filePath) {
-      return 1;
-    }
+      if (a.filePath > b.filePath) {
+        return 1;
+      }
 
-    return 0;
-  });
+      return 0;
+    });
 };
