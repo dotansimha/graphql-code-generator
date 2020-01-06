0fb4ac60473e00319bc58fb6b1c78f4e68fb2059
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2020-01-06 07:55:53 +0100

    Added tests for basic operations

--- packages/plugins/other/jsdoc-operations/tests/jsdoc-documents.spec.ts
+++ packages/plugins/other/jsdoc-operations/tests/jsdoc-documents.spec.ts
@@ -2,43 +2,88 @@ import { buildSchema, parse } from 'graphql';
 import { plugin } from '../src/index';
 
 describe('JSDoc Operations Plugin', () => {
-  const schema = buildSchema(/* GraphQL */ `
-    type Bar {
-      amount: Int!
-    }
+  it('should generate a typedef with a property', async () => {
+    const schema = buildSchema(/* Graphql */ `
+        type Foo {
+            foo: Int!
+        }
+    `);
 
-    union FooBar = Foo | Bar
-
-    type Foo {
-      id: String!
-    }
-
-    type Query {
-      foo: Foo
-      bar: Bar
-      foos: [Foo]
-      bars: [Bar!]!
-      foobar: FooBar!
-      fooOrbar: FooBar
-      computedFoo(bar: Bar): Foo
-      computedRequiredFoo(bar: Bar): Foo!
-    }
-
-    schema {
-      query: Query
-    }
-  `);
-
-  it('should ...', async () => {
     const ast = parse(/* GraphQL */ `
-      query foo {
-        myField
+      query {
+        foo
       }
     `);
 
     const config = {};
     const result = await plugin(schema, [{ filePath: 'test-file.ts', content: ast }], config, { outputFile: '' });
 
-    expect(result).toEqual('test');
+    expect(result).toEqual(`/**
+ * @typedef {Object} Foo
+ * @property {number} foo
+ */`);
+  });
+
+  it('should generate a typedef with a nullable property', async () => {
+    const schema = buildSchema(/* Graphql */ `
+        type Foo {
+            foo: Int
+        }
+    `);
+
+    const ast = parse(/* GraphQL */ `
+      query {
+        foo
+      }
+    `);
+
+    const config = {};
+    const result = await plugin(schema, [{ filePath: 'test-file.ts', content: ast }], config, { outputFile: '' });
+
+    expect(result).toEqual(expect.stringContaining('@property {number} [foo]'));
+  });
+
+  it('should generate a typedef for a union', async () => {
+    const schema = buildSchema(/* Graphql */ `
+        union FooBar = Int | Boolean
+    `);
+
+    const ast = parse(/* GraphQL */ `
+      query {
+        foo
+      }
+    `);
+
+    const config = {};
+    const result = await plugin(schema, [{ filePath: 'test-file.ts', content: ast }], config, { outputFile: '' });
+
+    expect(result).toEqual(`/**
+ * @typedef {(Int|Boolean)} FooBar
+ */`);
+  });
+
+  it('should generate a typedef with a List property', async () => {
+    const schema = buildSchema(/* Graphql */ `
+        type Foo {
+            foo: [Int!]!
+            nullableFoo: [Int!]
+            nullableItemsFoo: [Int]!
+            nullableItemsNullableFoo: [Int]
+        }
+    `);
+
+    const ast = parse(/* GraphQL */ `
+      query {
+        foo
+      }
+    `);
+
+    const config = {};
+    const result = await plugin(schema, [{ filePath: 'test-file.ts', content: ast }], config, { outputFile: '' });
+
+    expect(result).toEqual(expect.stringContaining('@property {Array<Int>} foo'));
+    expect(result).toEqual(expect.stringContaining('@property {Array<Int>} [nullableFoo]'));
+    expect(result).toEqual(expect.stringContaining('@property {Array<Int|null|undefined>} nullableItemsFoo'));
+    expect(result).toEqual(expect.stringContaining('@property {Array<Int|null|undefined>} [nullableItemsNullableFoo]'));
   });
 });
