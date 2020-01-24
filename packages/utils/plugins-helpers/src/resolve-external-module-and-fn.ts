0b2bce9e568d28b47f32e7780665773df94a6682
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-01-24 13:45:50 +0300

    Make it browser friendly

--- packages/utils/plugins-helpers/src/resolve-external-module-and-fn.ts
+++ packages/utils/plugins-helpers/src/resolve-external-module-and-fn.ts
@@ -1,9 +1,9 @@
-import { resolve } from 'path';
-import { existsSync } from 'fs';
-import importFrom from 'import-from';
 import { paramCase } from 'param-case';
 
 export function resolveExternalModuleAndFn(pointer: any): any {
+  // tslint:disable-next-line: no-eval
+  const importExternally = (moduleName: string) => eval(`require('${moduleName}')`);
+
   if (typeof pointer === 'function') {
     return pointer;
   }
@@ -13,9 +13,13 @@ export function resolveExternalModuleAndFn(pointer: any): any {
   if (moduleName === 'change-case') {
     moduleName = paramCase(functionName);
   }
+  // tslint:disable-next-line: no-eval
+  const { resolve } = importExternally('path');
   const localFilePath = resolve(process.cwd(), moduleName);
+  const { existsSync } = importExternally('fs');
   const localFileExists = existsSync(localFilePath);
-  const loadedModule = localFileExists ? require(localFilePath) : importFrom(process.cwd(), moduleName);
+  const importFrom = importExternally('import-from');
+  const loadedModule = localFileExists ? importExternally(localFilePath) : importFrom(process.cwd(), moduleName);
 
   if (!(functionName in loadedModule) && typeof loadedModule !== 'function') {
     throw new Error(`${functionName} couldn't be found in module ${moduleName}!`);
