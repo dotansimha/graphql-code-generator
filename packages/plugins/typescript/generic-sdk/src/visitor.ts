8b8eb886314201a1658862415002d591fa2edcb3
Artyom Kozhemiakin <xenus.t@gmail.com>
2020-02-04 13:04:11 +0300

    Introduce typescript-generic-sdk plugin (#2978)
    * Introduce typescript-generic-sdk plugin
    
    * Rename things
    
    * Properly handle DocumentMode
    
    * Opportunity to pass options to requester in request

---
+++ packages/plugins/typescript/generic-sdk/src/visitor.ts
@@ -0,0 +1,48 @@
+import { ClientSideBaseVisitor, ClientSideBasePluginConfig, LoadedFragment, getConfigValue, OMIT_TYPE, indentMultiline, RawClientSideBasePluginConfig, DocumentMode } from '@graphql-codegen/visitor-plugin-common';
+import autoBind from 'auto-bind';
+import { GraphQLSchema, Kind } from 'graphql';
+import { OperationDefinitionNode } from 'graphql';
+
+export class GenericSdkVisitor extends ClientSideBaseVisitor<RawClientSideBasePluginConfig, ClientSideBasePluginConfig> {
+  private _operationsToInclude: { node: OperationDefinitionNode; documentVariableName: string; operationType: string; operationResultType: string; operationVariablesTypes: string }[] = [];
+
+  constructor(schema: GraphQLSchema, fragments: LoadedFragment[], rawConfig: RawClientSideBasePluginConfig) {
+    super(schema, fragments, rawConfig, {});
+
+    autoBind(this);
+
+    if (this.config.documentMode !== DocumentMode.string) {
+      this._additionalImports.push(`import { DocumentNode } from 'graphql';`);
+    }
+  }
+
+  protected buildOperation(node: OperationDefinitionNode, documentVariableName: string, operationType: string, operationResultType: string, operationVariablesTypes: string): string {
+    this._operationsToInclude.push({
+      node,
+      documentVariableName,
+      operationType,
+      operationResultType,
+      operationVariablesTypes,
+    });
+
+    return null;
+  }
+
+  public get sdkContent(): string {
+    const allPossibleActions = this._operationsToInclude
+      .map(o => {
+        const optionalVariables = !o.node.variableDefinitions || o.node.variableDefinitions.length === 0 || o.node.variableDefinitions.every(v => v.type.kind !== Kind.NON_NULL_TYPE || v.defaultValue);
+        return `${o.node.name.value}(variables${optionalVariables ? '?' : ''}: ${o.operationVariablesTypes}, options?: C): Promise<${o.operationResultType}> {
+  return requester<${o.operationResultType}, ${o.operationVariablesTypes}>(${o.documentVariableName}, variables, options);
+}`;
+      })
+      .map(s => indentMultiline(s, 2));
+
+    return `export type Requester<C= {}> = <R, V>(doc: ${this.config.documentMode === DocumentMode.string ? 'string' : 'DocumentNode'}, vars?: V, options?: C) => Promise<R>
+export function getSdk<C>(requester: Requester<C>) {
+  return {
+${allPossibleActions.join(',\n')}
+  };
+}`;
+  }
+}
---
+++ packages/plugins/typescript/generic-sdk/tests/generic-sdk.spec.ts
@@ -0,0 +1,111 @@
+import { DocumentMode } from '@graphql-codegen/visitor-plugin-common';
+import { compileTs } from '@graphql-codegen/testing';
+import { plugin } from '../src/index';
+import { parse, buildClientSchema } from 'graphql';
+import { Types, mergeOutputs } from '@graphql-codegen/plugin-helpers';
+import { plugin as tsPlugin } from '@graphql-codegen/typescript';
+import { plugin as tsDocumentsPlugin } from '@graphql-codegen/typescript-operations';
+import { readFileSync } from 'fs';
+
+describe('generic-sdk', () => {
+  const schema = buildClientSchema(JSON.parse(readFileSync('../../../../dev-test/githunt/schema.json').toString()));
+  const basicDoc = parse(/* GraphQL */ `
+    query feed {
+      feed {
+        id
+        commentCount
+        repository {
+          owner {
+            avatar_url
+          }
+        }
+      }
+    }
+
+    query feed2($v: String!) {
+      feed {
+        id
+      }
+    }
+
+    query feed3($v: String) {
+      feed {
+        id
+      }
+    }
+
+    query feed4($v: String! = "TEST") {
+      feed {
+        id
+      }
+    }
+  `);
+
+  const validateAndCompile = async (content: Types.PluginOutput, config, docs, pluginSchema, usage = '') => {
+    const m = mergeOutputs([await tsPlugin(pluginSchema, docs, config, { outputFile: '' }), await tsDocumentsPlugin(pluginSchema, docs, config, { outputFile: '' }), content, usage]);
+
+    await compileTs(m);
+
+    return m;
+  };
+
+  describe('sdk', () => {
+    it('Should generate a correct wrap method', async () => {
+      const config = {};
+      const docs = [{ filePath: '', content: basicDoc }];
+      const result = (await plugin(schema, docs, config, {
+        outputFile: 'graphql.ts',
+      })) as Types.ComplexPluginOutput;
+
+      const usage = `
+async function test() {
+  const requester = <R, V> (doc: string, vars: V): Promise<R> => Promise.resolve({} as unknown as R);
+  const sdk = getSdk(requester);
+  
+  await sdk.feed();
+  await sdk.feed3();
+  await sdk.feed4();
+
+  const result = await sdk.feed2({ v: "1" });
+
+  if (result.feed) {
+    if (result.feed[0]) {
+      const id = result.feed[0].id
+    }
+  }
+}`;
+      const output = await validateAndCompile(result, config, docs, schema, usage);
+
+      expect(output).toMatchSnapshot();
+    });
+
+    it('Should generate a correct wrap method with documentMode=string', async () => {
+      const config = { documentMode: DocumentMode.string };
+      const docs = [{ filePath: '', content: basicDoc }];
+      const result = (await plugin(schema, docs, config, {
+        outputFile: 'graphql.ts',
+      })) as Types.ComplexPluginOutput;
+
+      const usage = `
+async function test() {
+  const requester = <R, V> (doc: string, vars: V): Promise<R> => Promise.resolve({} as unknown as R);
+  const sdk = getSdk(requester);
+  
+  await sdk.feed();
+  await sdk.feed3();
+  await sdk.feed4();
+
+  const result = await sdk.feed2({ v: "1" });
+
+  if (result.feed) {
+    if (result.feed[0]) {
+      const id = result.feed[0].id
+    }
+  }
+}`;
+      const output = await validateAndCompile(result, config, docs, schema, usage);
+
+      expect(output).toMatchSnapshot();
+    });
+  });
+});
