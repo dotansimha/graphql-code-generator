d8df0148823d2ab6560b7c81664d5d8b7138beb0
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2020-02-12 16:25:38 +0100

    Moved to seperate leave functions

--- packages/plugins/other/jsdoc-operations/src/index.ts
+++ packages/plugins/other/jsdoc-operations/src/index.ts
@@ -2,35 +2,6 @@ import { PluginFunction } from '@graphql-codegen/plugin-helpers';
 import { printSchema, parse, visit } from 'graphql';
 import { DEFAULT_SCALARS } from '@graphql-codegen/visitor-plugin-common';
 
-type NamedType = {
-  kind: 'NamedType';
-  value: string;
-  isRequired?: boolean;
-};
-
-type ListType = {
-  kind: 'ListType';
-  item: NamedType;
-  isRequired?: boolean;
-};
-
-type ScalarType = {
-  kind: 'ScalarTypeDefinition';
-  name: string;
-};
-
-type Property = {
-  name: string;
-  type: ListType | NamedType;
-};
-
-type TypeDef = {
-  name: string;
-  operation: string;
-  properties?: Array<Property>;
-  types?: Array<NamedType>;
-};
-
 const transformScalar = (scalar: string) => {
   if (DEFAULT_SCALARS[scalar] === undefined) {
     return scalar;
@@ -47,11 +18,13 @@ const createTypeDef = (lines: Array<string>) => {
 
 export const plugin: PluginFunction = schema => {
   const visited: Array<string> = visit(parse(printSchema(schema)), {
-    leave: {
-      Document(node) {
+    Document: {
+      leave(node) {
         return node.definitions;
       },
-      ObjectTypeDefinition(node) {
+    },
+    ObjectTypeDefinition: {
+      leave(node) {
         let fields: Array<string> = [];
 
         if (node.fields !== undefined) {
@@ -62,18 +35,26 @@ export const plugin: PluginFunction = schema => {
 
         return createTypeDef([`@typedef {Object} ${node.name}`, ...fields]);
       },
-      UnionTypeDefinition(node) {
+    },
+    UnionTypeDefinition: {
+      leave(node) {
         return `/**
  * @typedef {(${node.types.join('|')})} ${node.name}
  */`;
       },
-      Name(node) {
+    },
+    Name: {
+      leave(node) {
         return node.value;
       },
-      NamedType(node) {
+    },
+    NamedType: {
+      leave(node) {
         return transformScalar(node.name);
       },
-      NonNullType(node, _, parent) {
+    },
+    NonNullType: {
+      leave(node, _, parent) {
         if (parent === undefined) {
           return;
         }
@@ -88,17 +69,23 @@ export const plugin: PluginFunction = schema => {
 
         return node.type;
       },
-      FieldDefinition(node) {
+    },
+    FieldDefinition: {
+      leave(node) {
         const fieldName = node.nonNullable ? node.name : `[${node.name}]`;
 
         return `@property {${node.type}} ${fieldName}`;
       },
-      ListType(node) {
+    },
+    ListType: {
+      leave(node) {
         const type = node.nonNullableItems ? node.type : `(${node.type}|null|undefined)`;
 
         return `Array<${type}>`;
       },
-      ScalarTypeDefinition(node) {
+    },
+    ScalarTypeDefinition: {
+      leave(node) {
         return createTypeDef([`@typedef {*} ${node.name}`]);
       },
     },
