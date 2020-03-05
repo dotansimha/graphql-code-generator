d41dd1ccdca2de6051850e94bce60e828f420781
Auryn Engel <auryn.engel@sap.com>
2020-03-05 14:06:44 +0100

    add support for offix useOfflineMustation

---
+++ packages/plugins/typescript/react-apollo-offix/src/index.ts
@@ -0,0 +1,52 @@
+import { Types, PluginValidateFn, PluginFunction } from '@graphql-codegen/plugin-helpers';
+import { visit, GraphQLSchema, concatAST, Kind, FragmentDefinitionNode } from 'graphql';
+import {
+  LoadedFragment,
+  RawClientSideBasePluginConfig
+} from '@graphql-codegen/visitor-plugin-common';
+import { ReactApolloVisitor } from './visitor';
+import { extname } from 'path';
+
+export const plugin: PluginFunction<RawClientSideBasePluginConfig> = (
+  schema: GraphQLSchema,
+  documents: Types.DocumentFile[],
+  config: RawClientSideBasePluginConfig
+) => {
+  const allAst = concatAST(documents.map(v => v.document));
+
+  const allFragments: LoadedFragment[] = [
+    ...(allAst.definitions.filter(
+      d => d.kind === Kind.FRAGMENT_DEFINITION
+    ) as FragmentDefinitionNode[]).map(fragmentDef => ({
+      node: fragmentDef,
+      name: fragmentDef.name.value,
+      onType: fragmentDef.typeCondition.name.value,
+      isExternal: false
+    })),
+    ...(config.externalFragments || [])
+  ];
+
+  const visitor = new ReactApolloVisitor(schema, allFragments, config, documents);
+  const visitorResult = visit(allAst, { leave: visitor });
+
+  return {
+    prepend: visitor.getImports(),
+    content: [
+      visitor.fragments,
+      ...visitorResult.definitions.filter(t => typeof t === 'string')
+    ].join('\n')
+  };
+};
+
+export const validate: PluginValidateFn<any> = async (
+  _schema: GraphQLSchema,
+  _documents: Types.DocumentFile[],
+  _config: RawClientSideBasePluginConfig,
+  outputFile: string
+) => {
+  if (extname(outputFile) !== '.tsx' && extname(outputFile) !== '.ts') {
+    throw new Error(`Plugin "react-apollo" requires extension to be ".tsx" or ".ts!`);
+  }
+};
+
+export { ReactApolloVisitor };
