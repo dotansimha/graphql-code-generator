e7f075f80d40cdb1e1e30fe171de4be7692f20a4
Sean Kelley <sean.kelley@carbonlighthouse.com>
2020-03-23 18:21:07 -0700

    Start noodling around to figure out how this business works and generate some types.

--- packages/plugins/python/operations/src/ts-operation-variables-to-object.ts
+++ packages/plugins/python/operations/src/ts-operation-variables-to-object.ts
@@ -1,7 +1,67 @@
-import { TypeScriptOperationVariablesToObject as TSOperationVariablesToObject } from '@graphql-codegen/typescript';
+import {
+  ParsedEnumValuesMap,
+  OperationVariablesToObject,
+  NormalizedScalarsMap,
+  ConvertNameFn,
+} from '@graphql-codegen/visitor-plugin-common';
+import { TypeNode, Kind } from 'graphql';
 
-export class TypeScriptOperationVariablesToObject extends TSOperationVariablesToObject {
-  protected formatTypeString(fieldType: string, isNonNullType: boolean, hasDefaultValue: boolean): string {
-    return fieldType;
+export class PythonOperationVariablesToObject extends OperationVariablesToObject {
+  constructor(
+    _scalars: NormalizedScalarsMap,
+    _convertName: ConvertNameFn,
+    private _immutableTypes: boolean,
+    _namespacedImportName: string | null = null,
+    _enumNames: string[] = [],
+    _enumPrefix = true,
+    _enumValues: ParsedEnumValuesMap = {}
+  ) {
+    super(_scalars, _convertName, _namespacedImportName, _enumNames, _enumPrefix, _enumValues);
   }
+
+  // private clearOptional(str: string): string {
+  //   const prefix = this._namespacedImportName ? `${this._namespacedImportName}.` : '';
+  //   const rgx = new RegExp(`^${prefix}Maybe<(.*?)>$`, 'i');
+
+  //   if (str.startsWith(`${this._namespacedImportName ? `${this._namespacedImportName}.` : ''}Maybe`)) {
+  //     return str.replace(rgx, '$1');
+  //   }
+
+  //   return str;
+  // }
+
+  public wrapAstTypeWithModifiers(baseType: string, typeNode: TypeNode): string {
+    const prefix = this._namespacedImportName ? `${this._namespacedImportName}.` : '';
+
+    if (typeNode.kind === Kind.NON_NULL_TYPE) {
+      const type = this.wrapAstTypeWithModifiers(baseType, typeNode.type);
+      return type;
+      // return this.clearOptional(type);
+    } else if (typeNode.kind === Kind.LIST_TYPE) {
+      const innerType = this.wrapAstTypeWithModifiers(baseType, typeNode.type);
+
+      return `Optional[List[${innerType}]]`;
+    } else {
+      return `Optional[${baseType}]`;
+    }
+  }
+
+  // protected formatFieldString(fieldName: string, isNonNullType: boolean, hasDefaultValue: boolean): string {
+  //   if (!hasDefaultValue && (this._avoidOptionals || isNonNullType)) {
+  //     return fieldName;
+  //   }
+  //   return `${fieldName}?`;
+  // }
+
+  // protected formatTypeString(fieldType: string, isNonNullType: boolean, hasDefaultValue: boolean): string {
+  //   if (!hasDefaultValue && isNonNullType) {
+  //     return this.clearOptional(fieldType);
+  //   }
+
+  //   return fieldType;
+  // }
+
+  // protected getPunctuation(): string {
+  //   return ';';
+  // }
 }
--- packages/plugins/python/operations/src/visitor.ts
+++ packages/plugins/python/operations/src/visitor.ts
@@ -1,59 +1,67 @@
-import { GraphQLSchema, GraphQLOutputType, isEnumType, isNonNullType } from 'graphql';
+import {
+  GraphQLSchema,
+  GraphQLOutputType,
+  isEnumType,
+  isNonNullType,
+  isListType,
+  isScalarType,
+  GraphQLScalarType,
+  ObjectTypeDefinitionNode,
+  InputObjectTypeDefinitionNode,
+  OperationDefinitionNode,
+  FragmentDefinitionNode,
+  VariableDefinitionNode,
+  SelectionNode,
+  SelectionSetNode,
+  TypeNode,
+  NamedTypeNode,
+  ListTypeNode,
+} from 'graphql';
 import {
   wrapTypeWithModifiers,
   PreResolveTypesProcessor,
-  ParsedDocumentsConfig,
   BaseDocumentsVisitor,
+  ParsedDocumentsConfig,
+  BaseVisitor,
   LoadedFragment,
   getConfigValue,
   SelectionSetProcessorConfig,
   SelectionSetToObject,
   DeclarationKind,
-  normalizeAvoidOptionals,
-  AvoidOptionalsConfig,
 } from '@graphql-codegen/visitor-plugin-common';
-import { TypeScriptOperationVariablesToObject } from './ts-operation-variables-to-object';
+import { PythonOperationVariablesToObject } from './ts-operation-variables-to-object';
 import { PythonDocumentsPluginConfig } from './config';
 
 import { TypeScriptSelectionSetProcessor } from './ts-selection-set-processor';
 import autoBind from 'auto-bind';
 
-export interface TypeScriptDocumentsParsedConfig extends ParsedDocumentsConfig {
-  avoidOptionals: AvoidOptionalsConfig;
+export interface PythonDocumentsParsedConfig extends ParsedDocumentsConfig {
   immutableTypes: boolean;
-  noExport: boolean;
 }
 
-export class PythonDocumentsVisitor extends BaseDocumentsVisitor<
-  PythonDocumentsPluginConfig,
-  TypeScriptDocumentsParsedConfig
-> {
+export class PythonDocumentsVisitor extends BaseVisitor<PythonDocumentsPluginConfig, PythonDocumentsParsedConfig> {
+  private indent = '    ';
+  private schema: GraphQLSchema;
+
   constructor(schema: GraphQLSchema, config: PythonDocumentsPluginConfig, allFragments: LoadedFragment[]) {
-    super(
-      config,
-      {
-        noExport: getConfigValue(config.noExport, false),
-        avoidOptionals: normalizeAvoidOptionals(getConfigValue(config.avoidOptionals, false)),
-        immutableTypes: getConfigValue(config.immutableTypes, false),
-        nonOptionalTypename: getConfigValue(config.nonOptionalTypename, false),
-      } as TypeScriptDocumentsParsedConfig,
-      schema
-    );
+    super(config, {
+      immutableTypes: getConfigValue(config.immutableTypes, false),
+    } as PythonDocumentsParsedConfig);
+
+    this.schema = schema;
 
     autoBind(this);
 
     const wrapOptional = (type: string) => {
-      const prefix = this.config.namespacedImportName ? `${this.config.namespacedImportName}.` : '';
-      return `${prefix}Maybe<${type}>`;
+      return `Optional[${type}]`;
     };
+
     const wrapArray = (type: string) => {
-      const listModifier = this.config.immutableTypes ? 'ReadonlyArray' : 'Array';
-      return `${listModifier}<${type}>`;
+      return `List[${type}]`;
     };
 
     const formatNamedField = (name: string, type: GraphQLOutputType | null): string => {
-      const optional = !this.config.avoidOptionals.field && !!type && !isNonNullType(type);
-      return (this.config.immutableTypes ? `readonly ${name}` : name) + (optional ? '?' : '');
+      return name;
     };
 
     const processorConfig: SelectionSetProcessorConfig = {
@@ -66,39 +74,130 @@ export class PythonDocumentsVisitor extends BaseDocumentsVisitor<
         return wrapTypeWithModifiers(baseType, type, { wrapOptional, wrapArray });
       },
     };
-    const processor = new (config.preResolveTypes ? PreResolveTypesProcessor : TypeScriptSelectionSetProcessor)(
-      processorConfig
-    );
-    this.setSelectionSetHandler(
-      new SelectionSetToObject(
-        processor,
-        this.scalars,
-        this.schema,
-        this.convertName.bind(this),
-        this.getFragmentSuffix.bind(this),
-        allFragments,
-        this.config
-      )
-    );
-    const enumsNames = Object.keys(schema.getTypeMap()).filter((typeName) => isEnumType(schema.getType(typeName)));
-    this.setVariablesTransformer(
-      new TypeScriptOperationVariablesToObject(
-        this.scalars,
-        this.convertName.bind(this),
-        this.config.avoidOptionals.object,
-        this.config.immutableTypes,
-        this.config.namespacedImportName,
-        enumsNames,
-        this.config.enumPrefix,
-        this.config.enumValues
-      )
-    );
-    this._declarationBlockConfig = {
-      ignoreExport: this.config.noExport,
-    };
+    // const processor = new (config.preResolveTypes ? PreResolveTypesProcessor : TypeScriptSelectionSetProcessor)(
+    //   processorConfig
+    // );
+    // this.setSelectionSetHandler(
+    //   new SelectionSetToObject(
+    //     processor,
+    //     this.scalars,
+    //     this.schema,
+    //     this.convertName.bind(this),
+    //     this.getFragmentSuffix.bind(this),
+    //     allFragments,
+    //     this.config
+    //   )
+    // );
+    // const enumsNames = Object.keys(schema.getTypeMap()).filter(typeName => isEnumType(schema.getType(typeName)));
+    // this.setVariablesTransformer(
+    //   new PythonOperationVariablesToObject(
+    //     this.scalars,
+    //     this.convertName.bind(this),
+    //     this.config.immutableTypes,
+    //     this.config.namespacedImportName,
+    //     enumsNames,
+    //     this.config.enumPrefix,
+    //     this.config.enumValues
+    //   )
+    // );
   }
 
-  protected getPunctuation(declarationKind: DeclarationKind): string {
-    return ';';
+  InputObjectTypeDefinition(node: InputObjectTypeDefinitionNode): string {
+    return node.kind;
+    // return node.fields.map(f => f.name.value).join('\n');
+    // const name = `${this.convertName(node)}Input`;
+  }
+
+  ObjectTypeDefinition(node: ObjectTypeDefinitionNode): string {
+    return node.kind;
+    // return node.fields.map(f => f.name.value).join('\n');
+  }
+
+  OperationDefinition(node: OperationDefinitionNode): string {
+    const result = this.convertSelectionSet(node.name.value, node.selectionSet);
+
+    const variablesDeclaration = `class ${node.name.value}Variables(BaseModel):`;
+    const variablesBody = node.variableDefinitions
+      .map(this.convertVariableDefinition)
+      .filter(Boolean)
+      .join(`\n${this.indent}`);
+
+    return (variablesBody ? `${variablesDeclaration}\n${this.indent}${variablesBody}\n\n` : '') + `${result}`;
+  }
+
+  FragmentDefinition(node: FragmentDefinitionNode): string {
+    return node.kind;
+  }
+
+  convertSelectionSet(name: string, node: SelectionSetNode): string {
+    const resultDeclaration = `class ${name}(BaseModel):`;
+
+    const things = node.selections.map(this.convertSelection).filter(Boolean);
+
+    if (things.length > 0) {
+      return `${resultDeclaration}\n${this.indent + things.join(`\n${this.indent}`)}`;
+    } else {
+      return `${resultDeclaration}\n${this.indent}pass`;
+    }
+  }
+
+  convertSelection(node: SelectionNode): string {
+    if (node.kind === 'Field') {
+      return node.name.value;
+    } else {
+      return node.kind;
+    }
+  }
+
+  convertNamedType(name: string): string {
+    const t = this.schema.getTypeMap()[name];
+
+    if (!t) {
+      throw new Error('dunno');
+    }
+    const PYTHON_SCALAR_CONVERSIONS = {
+      String: 'str',
+      Boolean: 'bool',
+      Int: 'int',
+      Float: 'float',
+    };
+
+    if (t.name in PYTHON_SCALAR_CONVERSIONS) {
+      return PYTHON_SCALAR_CONVERSIONS[t.name];
+    } else {
+      return t.name;
+    }
+  }
+
+  convertType(node: TypeNode): string {
+    if (node.kind === 'NonNullType') {
+      return this.convertNonNullType(node.type);
+    } else if (node.kind === 'ListType') {
+      return `Optional[List[${this.convertType(node.type)}]]`;
+    } else if (node.kind === 'NamedType') {
+      return `Optional[${this.convertNamedType(node.name.value)}]`;
+    } else {
+      // assertNever
+      return '';
+    }
+  }
+
+  convertNonNullType(node: ListTypeNode | NamedTypeNode): string {
+    if (node.kind === 'ListType') {
+      return `List[${this.convertType(node.type)}]`;
+    } else if (node.kind === 'NamedType') {
+      return this.convertNamedType(node.name.value);
+    } else {
+      // assertNever
+      return '';
+    }
+  }
+
+  convertVariableDefinition(node: VariableDefinitionNode): string {
+    let temp = `${node.variable.name.value}: ${this.convertType(node.type)}`;
+    if (isNonNullType(node.type)) {
+      temp += ' = None';
+    }
+    return temp;
   }
 }
