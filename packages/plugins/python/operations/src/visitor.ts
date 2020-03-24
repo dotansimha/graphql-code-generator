2640343f57f3804fb40a907e52200aa6d6a2f523
Sean Kelley <sean.kelley@carbonlighthouse.com>
2020-03-24 16:41:56 -0700

    Get rid of some old implementation crufy.

--- packages/plugins/python/operations/src/visitor.ts
+++ packages/plugins/python/operations/src/visitor.ts
@@ -55,18 +55,6 @@ export interface PythonDocumentsParsedConfig extends ParsedDocumentsConfig {
   immutableTypes: boolean;
 }
 
-type TypePayload =
-  | {
-      kind: 'leaf';
-      name: string;
-      value: string;
-    }
-  | {
-      kind: 'internal';
-      name: string;
-      fields: Record<string, TypePayload>;
-    };
-
 type SelectionThatDoesNotRequireFields =
   | GraphQLLeafType
   | GraphQLList<SelectionThatDoesNotRequireFields>
@@ -328,17 +316,6 @@ export class PythonDocumentsVisitor extends BaseVisitor<PythonDocumentsPluginCon
     // );
   }
 
-  InputObjectTypeDefinition(node: InputObjectTypeDefinitionNode): string {
-    return 'input object ' + node.kind;
-    // return node.fields.map(f => f.name.value).join('\n');
-    // const name = `${this.convertName(node)}Input`;
-  }
-
-  ObjectTypeDefinition(node: ObjectTypeDefinitionNode): string {
-    return 'object ' + node.kind;
-    // return node.fields.map(f => f.name.value).join('\n');
-  }
-
   OperationDefinition(node: OperationDefinitionNode): string {
     let rootType;
     if (node.operation === 'query') {
@@ -351,16 +328,11 @@ export class PythonDocumentsVisitor extends BaseVisitor<PythonDocumentsPluginCon
       // assertNever
     }
 
+    // TODO: Variables types.
+
     const declaration = parse(rootType, node.selectionSet);
     declaration.className = node.name.value;
     return print(declaration);
-
-    // const result = this.convertSelectionSet(rootType, node.name.value, node.selectionSet);
-
-    // const variablesDeclaration = `class ${node.name.value}Variables(BaseModel):`;
-    // const variablesBody = node.variableDefinitions.map(this.convertVariableDefinition).filter(Boolean).join(`\n`);
-
-    // return (variablesBody ? `${variablesDeclaration}\n${indentMultiline(variablesBody)}\n\n` : '') + `${result}`;
   }
 
   FragmentDefinition(node: FragmentDefinitionNode): string {
@@ -368,114 +340,5 @@ export class PythonDocumentsVisitor extends BaseVisitor<PythonDocumentsPluginCon
     const declaration = parse(fragmentRootType, node.selectionSet);
     declaration.className = `${node.name.value}Fragment`;
     return print(declaration);
-  }
-
-  convertSelectionSet(
-    rootType: GraphQLObjectType,
-    name: string,
-    node: SelectionSetNode,
-    parentClass = 'BaseModel'
-  ): string {
-    const resultDeclaration = `class ${name}(${parentClass}):`;
-
-    const things = node.selections.map((s) => this.convertSelection(rootType, name, s)).filter(Boolean);
-
-    if (things.length > 0) {
-      return `${resultDeclaration}\n${indentMultiline(things.join(`\n`))}`;
-    } else {
-      return `${resultDeclaration}\n${indentMultiline('pass')}`;
-    }
-  }
-
-  convertSelection(parentType: GraphQLObjectType, parentName: string, node: SelectionNode): string {
-    if (node.kind === 'Field') {
-      const name = node.alias?.value ?? node.name.value;
-      const fieldType = parentType.getFields()[node.name.value];
-      if (node.selectionSet) {
-        const nestedName = `${parentName}_${name}`;
-        // TODO: This cast is probably illegal.
-        const s = this.convertSelectionSet(
-          getBaseType(fieldType.type) as GraphQLObjectType,
-          nestedName,
-          node.selectionSet
-        );
-        return `${s}\n\n${name}: ${nestedName}`;
-      } else {
-        const t = fieldType.type;
-        let type;
-        if (isNonNullType(t)) {
-        } else if (isScalarType(t)) {
-          type = this.convertNamedType(t.name);
-        } else if (isEnumType(t)) {
-          type = t.name;
-        } else if (isListType(t)) {
-          type = `List[${this.convertNamedType(t.ofType)}]`;
-        } else {
-          throw new Error('what is it?!');
-        }
-        return 'something';
-        // return `${name}: ${fieldType.type} ${}`;
-      }
-    } else if (node.kind === 'FragmentSpread') {
-      const nestedName = `${parentName}_${node.name.value}`;
-      return 'hello ' + this.schema.getType(node.kind)?.name ?? 'dunno';
-      // return 'hello' + this.schema.getTypeMap()[node.kind].name;
-      // node.name
-    } else {
-      return node.kind;
-    }
-  }
-
-  convertNamedType(name: string): string {
-    const t = this.schema.getType(name);
-
-    if (!t) {
-      throw new Error('dunno');
-    }
-
-    const PYTHON_SCALAR_CONVERSIONS = {
-      String: 'str',
-      Boolean: 'bool',
-      Int: 'int',
-      Float: 'float',
-    };
-
-    if (t.name in PYTHON_SCALAR_CONVERSIONS) {
-      return PYTHON_SCALAR_CONVERSIONS[t.name];
-    } else {
-      return t.name;
-    }
-  }
-
-  convertType(node: TypeNode): string {
-    if (node.kind === 'NonNullType') {
-      return this.convertNonNullType(node.type);
-    } else if (node.kind === 'ListType') {
-      return `Optional[List[${this.convertType(node.type)}]]`;
-    } else if (node.kind === 'NamedType') {
-      return `Optional[${this.convertNamedType(node.name.value)}]`;
-    } else {
-      // assertNever
-      return '';
-    }
-  }
-
-  convertNonNullType(node: ListTypeNode | NamedTypeNode): string {
-    if (node.kind === 'ListType') {
-      return `List[${this.convertType(node.type)}]`;
-    } else if (node.kind === 'NamedType') {
-      return this.convertNamedType(node.name.value);
-    } else {
-      // assertNever
-      return '';
-    }
-  }
-
-  convertVariableDefinition(node: VariableDefinitionNode): string {
-    let temp = `${node.variable.name.value}: ${this.convertType(node.type)}`;
-    if (!isNonNullType(node.type)) {
-      temp += ' = None';
-    }
-    return temp;
   }
 }
