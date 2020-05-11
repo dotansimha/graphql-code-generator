1702347f0c7323eccf154007fb84865e7020a572
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-05-11 15:46:19 +0200

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -36,7 +36,7 @@ module.exports = {
           position: 'right'
         },
         {
-          href: 'https://the-guild.dev/connected-build?utm_source=github&utm_medium=release-note&utm_campaign=mesh#section-contact',
+          href: 'https://the-guild.dev/contact',
           label: 'Contact Us',
           position: 'right'
         }
