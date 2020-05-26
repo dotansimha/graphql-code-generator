82dcf35b86658e8a6ea5258095fbddc4aae5becd
Dotan Simha <dotansimha@gmail.com>
2020-05-26 16:56:21 +0300

    improve error when graphql package is not available

--- packages/graphql-codegen-cli/src/cli.ts
+++ packages/graphql-codegen-cli/src/cli.ts
@@ -2,8 +2,11 @@ import { generate } from './generate-and-save';
 import { init } from './init';
 import { createContext } from './config';
 import { lifecycleHooks } from './hooks';
+import { DetailedError } from '@graphql-codegen/plugin-helpers';
 
 export function runCli(cmd: string): Promise<any> {
+  ensureGraphQlPackage();
+
   switch (cmd) {
     case 'init':
       return init();
@@ -16,5 +19,21 @@ export function runCli(cmd: string): Promise<any> {
         });
       });
     }
+  }
+}
+
+export function ensureGraphQlPackage() {
+  try {
+    require('graphql');
+  } catch (e) {
+    throw new DetailedError(
+      `Unable to load "graphql" package. Please make sure to install "graphql" as a dependency!`,
+      `
+  To install "graphql", run:
+    yarn add graphql
+  Or, with NPM:
+    npm install --save graphql
+`
+    );
   }
 }
