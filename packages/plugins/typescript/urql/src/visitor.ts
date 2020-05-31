b260288e6ec37e23f393b5f91d84626bd07a614f
Pytal <24800714+Pytal@users.noreply.github.com>
2020-05-31 14:04:52 -0700

    Fix invalid Urql subscription handler type

--- packages/plugins/typescript/urql/src/visitor.ts
+++ packages/plugins/typescript/urql/src/visitor.ts
@@ -97,7 +97,7 @@ export function use${operationName}() {
 
     if (operationType === 'Subscription') {
       return `
-export function use${operationName}<TData = any>(options: Omit<Urql.Use${operationType}Args<${operationVariablesTypes}>, 'query'> = {}, handler?: Urql.SubscriptionHandler<${operationName}, TData>) {
+export function use${operationName}<TData = any>(options: Omit<Urql.Use${operationType}Args<${operationVariablesTypes}>, 'query'> = {}, handler?: Urql.SubscriptionHandler<${operationResultType}, TData>) {
   return Urql.use${operationType}<${operationResultType}, TData, ${operationVariablesTypes}>({ query: ${documentVariableName}, ...options }, handler);
 };`;
     }
