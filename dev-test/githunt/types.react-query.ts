f25cdf007250c1ef5f120857e3fa77d105bdd2c4
Gil Gardosh <gilgardosh@gmail.com>
2020-11-24 11:51:08 +0200

    dev-tests update

--- dev-test/githunt/types.react-query.ts
+++ dev-test/githunt/types.react-query.ts
@@ -1,8 +1,8 @@
 import { useQuery, QueryConfig, useMutation, MutationConfig } from 'react-query';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in keyof Pick<T, K>]?: Maybe<Pick<T, K>[SubKey]> };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 
 function fetcher<TData, TVariables>(endpoint: string, requestInit: RequestInit, query: string, variables?: TVariables) {
   return async (): Promise<TData> => {
