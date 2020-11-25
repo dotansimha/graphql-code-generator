49242c20cd985addba5f74442d435a527bdbe163
Randolf J <34705014+jun-sheaf@users.noreply.github.com>
2020-11-25 17:58:23 +0100

    feat(typescript): adds a `defaultValue` flag to all `avoidOptionals` configs (#5113)

---
+++ .changeset/short-monkeys-return.md
@@ -0,0 +1,6 @@
+---
+'@graphql-codegen/typescript': minor
+---
+
+Added a "defaultValue" option in the "avoidOptionals" config
+See https://github.com/dotansimha/graphql-code-generator/issues/5112
--- packages/plugins/typescript/typescript/tests/typescript.spec.ts
+++ packages/plugins/typescript/typescript/tests/typescript.spec.ts
@@ -2331,6 +2331,47 @@ describe('TypeScript', () => {
 
       validateTs(result);
     });
+
+    it('Should generate correctly types for inputs with default value and avoidOptionals.defaultValue set to true - #5112', async () => {
+      const schema = buildSchema(
+        `input MyInput { a: String = "default", b: String! = "default", c: String, d: String! }`
+      );
+      const result = await plugin(schema, [], { avoidOptionals: { defaultValue: true } }, { outputFile: '' });
+
+      expect(result.content).toBeSimilarStringTo(`
+        export type MyInput = {
+          a?: Maybe<Scalars['String']>;
+          b: Scalars['String'];
+          c?: Maybe<Scalars['String']>;
+          d: Scalars['String'];
+        };
+    `);
+
+      validateTs(result);
+    });
+
+    it('Should generate correctly types for field arguments with default value and avoidOptionals.defaultValue option set to true - #5112', async () => {
+      const schema = buildSchema(
+        `type MyType { foo(a: String = "default", b: String! = "default", c: String, d: String!): String }`
+      );
+      const result = (await plugin(
+        schema,
+        [],
+        { avoidOptionals: { defaultValue: true } },
+        { outputFile: '' }
+      )) as Types.ComplexPluginOutput;
+
+      expect(result.content).toBeSimilarStringTo(`
+        export type MyTypeFooArgs = {
+          a?: Maybe<Scalars['String']>;
+          b: Scalars['String'];
+          c?: Maybe<Scalars['String']>;
+          d: Scalars['String'];
+        };
+    `);
+
+      validateTs(result);
+    });
   });
 
   describe('Enum', () => {
