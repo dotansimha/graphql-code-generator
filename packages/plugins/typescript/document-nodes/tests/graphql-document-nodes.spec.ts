1b68dd5dc86f1c17b14871605ef7c91f2adc4a8c
Dotan Simha <dotansimha@gmail.com>
2020-11-25 19:04:43 +0200

    fix tests

--- packages/plugins/typescript/document-nodes/tests/graphql-document-nodes.spec.ts
+++ packages/plugins/typescript/document-nodes/tests/graphql-document-nodes.spec.ts
@@ -316,7 +316,7 @@ describe('graphql-codegen typescript-graphql-document-nodes', () => {
     ) as Types.ComplexPluginOutput;
 
     expect(result.content).toBeSimilarStringTo(`
-    export const MyQuery: DocumentNode = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"MyQuery"},"variableDefinitions":[],"directives":[],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"field"},"arguments":[],"directives":[]}]}}]};
+    export const MyQuery: DocumentNode = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"MyQuery"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"field"}}]}}]};
     `);
     validateTs(result.content);
   });
