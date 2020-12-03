b45df6daeebf87649f240ae5fb389d3d9528d923
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-12-03 12:41:21 +0100

    Update bob.config.js

--- bob.config.js
+++ bob.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   ignore: ['@graphql-codegen/website', '@graphql-codegen/live-demo', '@graphql-codegen/config-schema'],
+  base: 'origin/master',
+  track: [
+    'bob.config.js',
+    'package.json',
+    'yarn.lock',
+    'tsconfig.json',
+    '<project>/package.json',
+    '<project>/tsconfig.json',
+    '<project>/src/**',
+  ],
+  commands: {
+    test: {
+      track: [
+        'jest.config.js',
+        'jest.project.js',
+        '<project>/jest.config.js',
+        '<project>/tests/**',
+      ],
+      run(affected) {
+        return [`yarn`, ['test', '--passWithNoTests', ...affected.paths]];
+      },
+    },
+    build: {
+      run() {
+        return [`yarn`, ['build']];
+      },
+    },
+  },
 };
