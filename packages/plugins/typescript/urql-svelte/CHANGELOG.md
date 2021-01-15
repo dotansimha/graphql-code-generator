e5faf9836884ad5588f02c309aa4182f41afaf62
Eden <github@eden2.com>
2021-01-15 15:48:57 +0400

    dupe urql > urql-svelte

---
+++ packages/plugins/typescript/urql-svelte/CHANGELOG.md
@@ -0,0 +1,57 @@
+# @graphql-codegen/typescript-urql
+
+## 2.0.3
+
+### Patch Changes
+
+- 1183d173: Bump all packages to resolve issues with shared dependencies
+- Updated dependencies [1183d173]
+  - @graphql-codegen/visitor-plugin-common@1.17.20
+  - @graphql-codegen/plugin-helpers@1.18.2
+
+## 2.0.2
+
+### Patch Changes
+
+- f603b8f8: Support unnamed queries in operation visitors
+- 65f92288: fix extension validation
+- Updated dependencies [d2cde3d5]
+- Updated dependencies [89a6aa80]
+- Updated dependencies [f603b8f8]
+- Updated dependencies [da8bdd17]
+  - @graphql-codegen/visitor-plugin-common@1.17.15
+  - @graphql-codegen/plugin-helpers@1.17.9
+
+## 2.0.1
+
+### Patch Changes
+
+- 1d7c6432: Bump all packages to allow "^" in deps and fix compatibility issues
+- 1d7c6432: Bump versions of @graphql-tools/ packages to fix issues with loading schemas and SDL comments
+- Updated dependencies [1d7c6432]
+- Updated dependencies [1d7c6432]
+  - @graphql-codegen/visitor-plugin-common@1.17.13
+  - @graphql-codegen/plugin-helpers@1.17.8
+
+## 2.0.0
+
+### Major Changes
+
+- 7f2bf153: Prefer generating React Hooks over React data components by default
+
+  ## Breaking Changes
+
+  The default configuration for this plugins has changed to:
+
+  ```yaml
+  config:
+    withHooks: true
+    withComponent: false
+  ```
+
+  If you are using the generated Component from that plugin, you can turn it on by adding `withComponent: true` to your configuration.
+
+### Patch Changes
+
+- Updated dependencies [4266a15f]
+  - @graphql-codegen/visitor-plugin-common@1.17.12
---
+++ packages/plugins/typescript/urql-svelte/src/visitor.ts
@@ -0,0 +1,130 @@
+import {
+  ClientSideBaseVisitor,
+  ClientSideBasePluginConfig,
+  LoadedFragment,
+  getConfigValue,
+  OMIT_TYPE,
+} from '@graphql-codegen/visitor-plugin-common';
+import { UrqlRawPluginConfig } from './config';
+import autoBind from 'auto-bind';
+import { OperationDefinitionNode, Kind, GraphQLSchema } from 'graphql';
+import { pascalCase } from 'pascal-case';
+
+export interface UrqlPluginConfig extends ClientSideBasePluginConfig {
+  withComponent: boolean;
+  withHooks: boolean;
+  urqlImportFrom: string;
+}
+
+export class UrqlVisitor extends ClientSideBaseVisitor<UrqlRawPluginConfig, UrqlPluginConfig> {
+  constructor(schema: GraphQLSchema, fragments: LoadedFragment[], rawConfig: UrqlRawPluginConfig) {
+    super(schema, fragments, rawConfig, {
+      withComponent: getConfigValue(rawConfig.withComponent, false),
+      withHooks: getConfigValue(rawConfig.withHooks, true),
+      urqlImportFrom: getConfigValue(rawConfig.urqlImportFrom, null),
+    });
+
+    autoBind(this);
+  }
+
+  public getImports(): string[] {
+    const baseImports = super.getImports();
+    const imports = [];
+    const hasOperations = this._collectedOperations.length > 0;
+
+    if (!hasOperations) {
+      return baseImports;
+    }
+
+    if (this.config.withComponent) {
+      imports.push(`import * as React from 'react';`);
+    }
+
+    if (this.config.withComponent || this.config.withHooks) {
+      imports.push(`import * as Urql from '${this.config.urqlImportFrom || 'urql'}';`);
+    }
+
+    imports.push(OMIT_TYPE);
+
+    return [...baseImports, ...imports];
+  }
+
+  private _buildComponent(
+    node: OperationDefinitionNode,
+    documentVariableName: string,
+    operationType: string,
+    operationResultType: string,
+    operationVariablesTypes: string
+  ): string {
+    const componentName: string = this.convertName(node.name?.value ?? '', {
+      suffix: 'Component',
+      useTypesPrefix: false,
+    });
+
+    const isVariablesRequired =
+      operationType === 'Query' &&
+      node.variableDefinitions.some(variableDef => variableDef.type.kind === Kind.NON_NULL_TYPE);
+
+    const generics = [operationResultType, operationVariablesTypes];
+
+    if (operationType === 'Subscription') {
+      generics.unshift(operationResultType);
+    }
+    return `
+export const ${componentName} = (props: Omit<Urql.${operationType}Props<${generics.join(
+      ', '
+    )}>, 'query'> & { variables${isVariablesRequired ? '' : '?'}: ${operationVariablesTypes} }) => (
+  <Urql.${operationType} {...props} query={${documentVariableName}} />
+);
+`;
+  }
+
+  private _buildHooks(
+    node: OperationDefinitionNode,
+    operationType: string,
+    documentVariableName: string,
+    operationResultType: string,
+    operationVariablesTypes: string
+  ): string {
+    const operationName: string = this.convertName(node.name?.value ?? '', {
+      suffix: this.config.omitOperationSuffix ? '' : pascalCase(operationType),
+      useTypesPrefix: false,
+    });
+
+    if (operationType === 'Mutation') {
+      return `
+export function use${operationName}() {
+  return Urql.use${operationType}<${operationResultType}, ${operationVariablesTypes}>(${documentVariableName});
+};`;
+    }
+
+    if (operationType === 'Subscription') {
+      return `
+export function use${operationName}<TData = ${operationResultType}>(options: Omit<Urql.Use${operationType}Args<${operationVariablesTypes}>, 'query'> = {}, handler?: Urql.SubscriptionHandler<${operationResultType}, TData>) {
+  return Urql.use${operationType}<${operationResultType}, TData, ${operationVariablesTypes}>({ query: ${documentVariableName}, ...options }, handler);
+};`;
+    }
+
+    return `
+export function use${operationName}(options: Omit<Urql.Use${operationType}Args<${operationVariablesTypes}>, 'query'> = {}) {
+  return Urql.use${operationType}<${operationResultType}>({ query: ${documentVariableName}, ...options });
+};`;
+  }
+
+  protected buildOperation(
+    node: OperationDefinitionNode,
+    documentVariableName: string,
+    operationType: string,
+    operationResultType: string,
+    operationVariablesTypes: string
+  ): string {
+    const component = this.config.withComponent
+      ? this._buildComponent(node, documentVariableName, operationType, operationResultType, operationVariablesTypes)
+      : null;
+    const hooks = this.config.withHooks
+      ? this._buildHooks(node, operationType, documentVariableName, operationResultType, operationVariablesTypes)
+      : null;
+
+    return [component, hooks].filter(a => a).join('\n');
+  }
+}
