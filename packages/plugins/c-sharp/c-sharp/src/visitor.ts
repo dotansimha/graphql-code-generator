9e0f6395d99c845fbf59739d605e30473ca3199a
Oisin Grehan <oising@gmail.com>
2021-02-09 16:28:41 -0500

    allow csharp plugin to emit c# 9 record types instead of classes (#5509)
    * first stab at it -- four hours without testing a single thing
    
    * oops, forgot to respect flag in ObjectTypeDefinition visitor
    
    * add npm change-case to do pascal casing for fields (both on class and records)
    
    * ensure that pascal case transform happens _before_ convertSafeName call
    
    * enhance record output to support the json serializer hints
    
    * undo my 'fix' for csharp class field casing; broke so many tests and is best done in a separate PR
    
    * add a changeset for this minor release
    
    * update changeset from patch to minor
    
    * add test for record initializer declaration emit
    
    * interfaces will using casing to match records and not define setters when using emitRecords
    
    Co-authored-by: Oisin Grehan <oisin.grehan@hiloenergie.com>

--- packages/plugins/c-sharp/c-sharp/src/visitor.ts
+++ packages/plugins/c-sharp/c-sharp/src/visitor.ts
@@ -38,12 +38,14 @@ import {
   wrapFieldType,
   getListTypeField,
 } from '../../common/common';
+import { pascalCase } from 'change-case';
 
 export interface CSharpResolverParsedConfig extends ParsedConfig {
   namespaceName: string;
   className: string;
   listType: string;
   enumValues: EnumValuesMap;
+  emitRecords: boolean;
 }
 
 export class CSharpResolversVisitor extends BaseVisitor<CSharpResolversPluginRawConfig, CSharpResolverParsedConfig> {
@@ -55,6 +57,7 @@ export class CSharpResolversVisitor extends BaseVisitor<CSharpResolversPluginRaw
       listType: rawConfig.listType || 'List',
       namespaceName: rawConfig.namespaceName || 'GraphQLCodeGen',
       className: rawConfig.className || 'Types',
+      emitRecords: rawConfig.emitRecords || false,
       scalars: buildScalars(_schema, rawConfig.scalars, C_SHARP_SCALARS),
     });
   }
@@ -240,6 +243,42 @@ export class CSharpResolversVisitor extends BaseVisitor<CSharpResolversPluginRaw
     return result;
   }
 
+  protected buildRecord(
+    name: string,
+    description: StringValueNode,
+    inputValueArray: ReadonlyArray<FieldDefinitionNode>,
+    interfaces?: ReadonlyArray<NamedTypeNode>
+  ): string {
+    const classSummary = transformComment(description?.value);
+    const interfaceImpl =
+      interfaces && interfaces.length > 0 ? ` : ${interfaces.map(ntn => ntn.name.value).join(', ')}` : '';
+    const recordMembers = inputValueArray
+      .map(arg => {
+        const fieldType = this.resolveInputFieldType(arg.type);
+        const fieldHeader = this.getFieldHeader(arg, fieldType);
+        const fieldName = this.convertSafeName(pascalCase(this.convertName(arg.name)));
+        const csharpFieldType = wrapFieldType(fieldType, fieldType.listType, this.config.listType);
+        return fieldHeader + indent(`public ${csharpFieldType} ${fieldName} { get; init; } = ${fieldName};`);
+      })
+      .join('\n\n');
+    const recordInitializer = inputValueArray
+      .map(arg => {
+        const fieldType = this.resolveInputFieldType(arg.type);
+        const fieldName = this.convertSafeName(pascalCase(this.convertName(arg.name)));
+        const csharpFieldType = wrapFieldType(fieldType, fieldType.listType, this.config.listType);
+        return `${csharpFieldType} ${fieldName}`;
+      })
+      .join(', ');
+    return `
+#region ${name}
+${classSummary}public record ${this.convertSafeName(name)}(${recordInitializer})${interfaceImpl} {
+  #region members
+${recordMembers}
+  #endregion
+}
+#endregion`;
+  }
+
   protected buildClass(
     name: string,
     description: StringValueNode,
@@ -279,9 +318,23 @@ ${classMembers}
       .map(arg => {
         const fieldType = this.resolveInputFieldType(arg.type);
         const fieldHeader = this.getFieldHeader(arg, fieldType);
-        const fieldName = this.convertSafeName(arg.name);
+
+        let fieldName: string;
+        let getterSetter: string;
+
+        if (this.config.emitRecords) {
+          // record
+          fieldName = this.convertSafeName(pascalCase(this.convertName(arg.name)));
+          getterSetter = '{ get; }';
+        } else {
+          // class
+          fieldName = this.convertSafeName(arg.name);
+          getterSetter = '{ get; set; }';
+        }
+
         const csharpFieldType = wrapFieldType(fieldType, fieldType.listType, this.config.listType);
-        return fieldHeader + indent(`public ${csharpFieldType} ${fieldName} { get; set; }`);
+
+        return fieldHeader + indent(`public ${csharpFieldType} ${fieldName} ${getterSetter}`);
       })
       .join('\n\n');
 
@@ -344,6 +397,10 @@ ${classMembers}
   }
 
   ObjectTypeDefinition(node: ObjectTypeDefinitionNode): string {
+    if (this.config.emitRecords) {
+      return this.buildRecord(node.name.value, node.description, node.fields, node.interfaces);
+    }
+
     return this.buildClass(node.name.value, node.description, node.fields, node.interfaces);
   }
 
