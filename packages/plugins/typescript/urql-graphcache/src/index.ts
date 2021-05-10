78d8cb6c77601cb53e10dcd939c9e6109f8af4d2
Phil Pluckthun <phil@kitten.sh>
2021-05-10 14:08:23 +0100

    Replace RequireFields with WithTypename utility type

--- packages/plugins/typescript/urql-graphcache/src/index.ts
+++ packages/plugins/typescript/urql-graphcache/src/index.ts
@@ -70,13 +70,13 @@ function constructType(typeNode: TypeNode, schema: GraphQLSchema, nullable = tru
         case 'InputObjectTypeDefinition':
         case 'ObjectTypeDefinition':
         case 'ScalarTypeDefinition': {
-          const finalType = `RequireFields<${typeNode.name.value}, '__typename'>${allowString ? ' | string' : ''}`;
+          const finalType = `WithTypename<${typeNode.name.value}>${allowString ? ' | string' : ''}`;
           return nullable ? `Maybe<${finalType}>` : finalType;
         }
 
         case 'InterfaceTypeDefinition': {
           const possibleTypes = schema.getPossibleTypes(type as GraphQLAbstractType).map(function contruct(x) {
-            return `RequireFields<${x}, '__typename'>`;
+            return `WithTypename<${x}>`;
           });
           const finalType = allowString ? possibleTypes.join(' | ') + ' | string' : possibleTypes.join(' | ');
           return nullable ? `Maybe<${finalType}>` : finalType;
@@ -96,7 +96,7 @@ const capitalize = (s: string): string => s.charAt(0).toUpperCase() + s.slice(1)
 
 function getKeysConfig(schema: GraphQLSchema) {
   const keys = getObjectTypes(schema).reduce((keys, type) => {
-    keys.push(`${type.name}?: (data: RequireFields<${type.name}, '__typename'>) => null | string`);
+    keys.push(`${type.name}?: (data: WithTypename<${type.name}>) => null | string`);
     return keys;
   }, []);
 
@@ -113,8 +113,8 @@ function getResolversConfig(schema: GraphQLSchema) {
       const type = unwrapType(field.type);
 
       fields.push(
-        `${field.name.value}?: GraphCacheResolver<RequireFields<` +
-          `${parentType.name}, '__typename'>, ${argsName}, ` +
+        `${field.name.value}?: GraphCacheResolver<WithTypename<` +
+          `${parentType.name}>, ${argsName}, ` +
           `${constructType(type, schema, false, true)}>`
       );
 
@@ -207,7 +207,7 @@ export const plugin: PluginFunction<UrqlGraphCacheConfig, Types.ComplexPluginOut
   return {
     prepend: [imports],
     content: [
-      `type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };`,
+      `type WithTypename<T extends { __typename?: any }> = { [K in Exclude<keyof T, '__typename'>]?: T[K] } & { ['__typename']-?: NonNullable<T['__typename']> };`,
 
       keys,
 
