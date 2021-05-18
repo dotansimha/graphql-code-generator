02a6f86dc15d12aa026205839eecd27d75185601
gilgardosh <gilgardosh@gmail.com>
2021-05-18 18:27:37 +0300

    failing test

--- packages/plugins/typescript/typed-document-node/tests/typed-document-node.spec.ts
+++ packages/plugins/typescript/typed-document-node/tests/typed-document-node.spec.ts
@@ -1,4 +1,5 @@
 import { Types } from '@graphql-codegen/plugin-helpers';
+import { buildSchema, parse } from 'graphql';
 import { plugin } from '../src';
 
 describe('TypeDocumentNode', () => {
@@ -6,5 +7,55 @@ describe('TypeDocumentNode', () => {
     const result = (await plugin(null as any, [], {})) as Types.ComplexPluginOutput;
     expect(result.content).toBe('');
     expect(result.prepend.length).toBe(0);
+  });
+
+  it('Duplicated nested fragments handle', async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      schema {
+        query: Query
+      }
+
+      type Query {
+        jobs: [Job!]!
+      }
+
+      type Job {
+        id: ID!
+        recruiterName: String!
+        title: String!
+      }
+    `);
+
+    const ast = parse(/* GraphQL */ `
+      query GetJobs {
+        jobs {
+          ...DataForPageA
+          ...DataForPageB
+        }
+      }
+
+      fragment DataForPageA on Job {
+        id
+        ...JobSimpleRecruiterData
+      }
+
+      fragment DataForPageB on Job {
+        title
+        ...JobSimpleRecruiterData
+      }
+
+      fragment JobSimpleRecruiterData on Job {
+        recruiterName
+      }
+    `);
+
+    const res = (await plugin(
+      schema,
+      [{ location: '', document: ast }],
+      {},
+      { outputFile: '' }
+    )) as Types.ComplexPluginOutput;
+
+    expect((res.content.match(/"value":"JobSimpleRecruiterData"/g) || []).length).toBeLessThan(3);
   });
 });
