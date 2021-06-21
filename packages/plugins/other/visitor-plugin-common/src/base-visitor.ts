eed15a2ba3c62b7a8f7ec676920963607ae1ba00
Jait Dixit <jait.dixit@gmail.com>
2021-06-21 17:43:44 +0200

    Fix typo in base-visitor.ts (#6164)
    Co-authored-by: Jait Dixit <jait.dixit@joyn.de>

--- packages/plugins/other/visitor-plugin-common/src/base-visitor.ts
+++ packages/plugins/other/visitor-plugin-common/src/base-visitor.ts
@@ -188,9 +188,9 @@ export interface RawConfig {
    */
   globalNamespace?: boolean;
   /**
-   * @description  Removes fragment duplicants for reducing data transfer.
+   * @description  Removes fragment duplicates for reducing data transfer.
    * It is done by removing sub-fragments imports from fragment definition
-   * Instead - import all of them are imported to the Operation node.
+   * Instead - all of them are imported to the Operation node.
    * @type boolean
    * @default false
    */
