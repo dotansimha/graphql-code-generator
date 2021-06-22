dd9b63467985d4913b856103f62b4f09b3dfaa0d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-06-23 00:26:06 +0300

    Fix tests

--- packages/utils/plugins-helpers/src/resolve-external-module-and-fn.ts
+++ packages/utils/plugins-helpers/src/resolve-external-module-and-fn.ts
@@ -1,5 +1,6 @@
 import { createRequire } from 'module';
 import { cwd } from 'process';
+import * as changeCaseAll from 'change-case-all';
 
 export function resolveExternalModuleAndFn(pointer: any): any {
   if (typeof pointer === 'function') {
@@ -12,12 +13,17 @@ export function resolveExternalModuleAndFn(pointer: any): any {
   if (moduleName === 'change-case') {
     moduleName = 'change-case-all';
   }
-  
-  const cwdRequire = createRequire(cwd());
-  const loadedModule = cwdRequire(moduleName);
 
-  if (!(functionName in loadedModule) && typeof loadedModule !== 'function') {
-    throw new Error(`${functionName} couldn't be found in module ${moduleName}!`);
+  let loadedModule: any;
+  if (moduleName === 'change-case-all') {
+    loadedModule = changeCaseAll;
+  } else {
+    const cwdRequire = createRequire(cwd());
+    loadedModule = cwdRequire(moduleName);
+
+    if (!(functionName in loadedModule) && typeof loadedModule !== 'function') {
+      throw new Error(`${functionName} couldn't be found in module ${moduleName}!`);
+    }
   }
 
   return loadedModule[functionName] || loadedModule;
