f6fcf0294153cbabd80c669a2335976cd153d199
marocchino <marocchino@users.noreply.github.com>
2021-07-07 10:39:53 +0900

    Add typescript-enum-array plugin

---
+++ packages/plugins/typescript/enum-array/CHANGELOG.md
@@ -0,0 +1,1 @@
+# @graphql-codegen/typescript-enum-array
---
+++ packages/plugins/typescript/enum-array/src/config.ts
@@ -0,0 +1,7 @@
+export interface EnumArrayPluginConfig {
+  /**
+   * @description import enum types from generated type path
+   * if not given, omit import statement.
+   */
+  importFrom?: string;
+}
