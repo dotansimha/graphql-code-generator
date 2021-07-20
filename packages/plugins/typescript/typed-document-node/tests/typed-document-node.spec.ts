a5b168f67a6bf73a5c9bcd80a42dd5cd01ceab4e
Vladik Tarasov <sinimawath@gmail.com>
2021-07-20 21:22:37 +0300

    Add generation of __typename at typed-document-node plugin

--- packages/plugins/typescript/typed-document-node/tests/typed-document-node.spec.ts
+++ packages/plugins/typescript/typed-document-node/tests/typed-document-node.spec.ts
@@ -167,4 +167,73 @@ describe('TypedDocumentNode', () => {
 
     expect((res.content.match(/JobSimpleRecruiterDataFragmentDoc.definitions/g) || []).length).toBe(2);
   });
+
+  describe('addTypenameToSelectionSets', () => {
+    it('Check is add __typename to typed document', async () => {
+      const schema = buildSchema(/* GraphQL */ `
+        schema {
+          query: Query
+        }
+
+        type Query {
+          job: Job
+        }
+
+        type Job {
+          id: ID!
+        }
+      `);
+
+      const ast = parse(/* GraphQL */ `
+        query {
+          job {
+            id
+          }
+        }
+      `);
+
+      const res = (await plugin(
+        schema,
+        [{ location: '', document: ast }],
+        { addTypenameToSelectionSets: true },
+        { outputFile: '' }
+      )) as Types.ComplexPluginOutput;
+
+      expect((res.content.match(/__typename/g) || []).length).toBe(1);
+    });
+
+    it('Check with __typename in selection set', async () => {
+      const schema = buildSchema(/* GraphQL */ `
+        schema {
+          query: Query
+        }
+
+        type Query {
+          job: Job
+        }
+
+        type Job {
+          id: ID!
+        }
+      `);
+
+      const ast = parse(/* GraphQL */ `
+        query {
+          job {
+            id
+            __typename
+          }
+        }
+      `);
+
+      const res = (await plugin(
+        schema,
+        [{ location: '', document: ast }],
+        { addTypenameToSelectionSets: true },
+        { outputFile: '' }
+      )) as Types.ComplexPluginOutput;
+
+      expect((res.content.match(/__typename/g) || []).length).toBe(1);
+    });
+  });
 });
