984a621e834a0a41014cb3648aad85ceec316294
PabloSzx <pablosaez1995@gmail.com>
2021-08-27 19:37:56 -0400

    default scalars divided in operations and resolver

--- packages/plugins/other/visitor-plugin-common/src/scalars.ts
+++ packages/plugins/other/visitor-plugin-common/src/scalars.ts
@@ -7,3 +7,35 @@ export const DEFAULT_SCALARS: NormalizedScalarsMap = {
   Int: 'number',
   Float: 'number',
 };
+
+export const DEFAULT_RESOLVERS_INTERNAL_SCALARS: NormalizedScalarsMap = {
+  ID: 'string',
+  Int: 'number',
+  Float: 'number',
+  String: 'string',
+  Boolean: 'boolean',
+};
+
+export const DEFAULT_RESOLVERS_EXTERNAL_SCALARS: NormalizedScalarsMap = {
+  ID: 'string | number',
+  Int: 'number',
+  Float: 'number',
+  String: 'string',
+  Boolean: 'boolean',
+};
+
+export const DEFAULT_OPERATIONS_INPUT_SCALARS: NormalizedScalarsMap = {
+  ID: 'string | number',
+  Int: 'number',
+  Float: 'number',
+  String: 'string',
+  Boolean: 'boolean',
+};
+
+export const DEFAULT_OPERATIONS_OUTPUT_SCALARS: NormalizedScalarsMap = {
+  ID: 'string',
+  Int: 'number',
+  Float: 'number',
+  String: 'string',
+  Boolean: 'boolean',
+};
