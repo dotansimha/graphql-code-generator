eaa54e687f43521ff1122542313dbee3f4ab7206
Pablo Sáez <pablosaez1995@gmail.com>
2021-09-11 02:49:29 -0300

    fix: prevent duplicate operations

--- packages/presets/gql-tag-operations/tests/gql-tag-operations.spec.ts
+++ packages/presets/gql-tag-operations/tests/gql-tag-operations.spec.ts
@@ -206,4 +206,85 @@ export const CDocument = {\\"kind\\":\\"Document\\",\\"definitions\\":[{\\"kind\
       "import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'"
     );
   });
+
+  it('prevent duplicate operations', async () => {
+    const result = await executeCodegen({
+      schema: [
+        /* GraphQL */ `
+          type Query {
+            a: String
+          }
+        `,
+      ],
+      documents: path.join(__dirname, 'fixtures/duplicate-operation.ts'),
+      generates: {
+        'out1.ts': {
+          preset,
+          plugins: [],
+        },
+      },
+      config: {
+        useTypeImports: true,
+      },
+    });
+
+    expect(result.length).toBe(2);
+
+    expect(result[0].content).toMatchInlineSnapshot(`
+"/* eslint-disable */
+import * as graphql from './graphql';
+import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+
+const documents = {
+    \\"\\\\n  query a {\\\\n    a\\\\n  }\\\\n\\": graphql.ADocument,
+};
+
+export function gql(source: \\"\\\\n  query a {\\\\n    a\\\\n  }\\\\n\\"): (typeof documents)[\\"\\\\n  query a {\\\\n    a\\\\n  }\\\\n\\"];
+
+export function gql(source: string): unknown;
+export function gql(source: string) {
+  return (documents as any)[source] ?? {};
+}
+
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<
+  infer TType,
+  any
+>
+  ? TType
+  : never;
+"
+`);
+
+    expect(result[1].content).toMatchInlineSnapshot(`
+"/* eslint-disable */
+import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  a?: Maybe<Scalars['String']>;
+};
+
+export type AQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type AQuery = { __typename?: 'Query', a?: Maybe<string> };
+
+
+export const ADocument = {\\"kind\\":\\"Document\\",\\"definitions\\":[{\\"kind\\":\\"OperationDefinition\\",\\"operation\\":\\"query\\",\\"name\\":{\\"kind\\":\\"Name\\",\\"value\\":\\"a\\"},\\"selectionSet\\":{\\"kind\\":\\"SelectionSet\\",\\"selections\\":[{\\"kind\\":\\"Field\\",\\"name\\":{\\"kind\\":\\"Name\\",\\"value\\":\\"a\\"}}]}}]} as unknown as DocumentNode<AQuery, AQueryVariables>;"
+`);
+
+    expect(result[0].content.match(/query a {/g).length).toBe(3);
+  });
 });
