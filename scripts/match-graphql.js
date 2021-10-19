6432a6addcb4e86f3bccf426c9d46fb2a164bfb9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-10-19 20:13:20 +0300

    Add GraphQL v16 to tests

---
+++ scripts/match-graphql.js
@@ -0,0 +1,19 @@
+const { writeFileSync } = require('fs');
+const { resolve } = require('path');
+const { argv, cwd } = require('process');
+
+const pkgPath = resolve(cwd(), './package.json');
+
+const pkg = require(pkgPath);
+
+const version = argv[2];
+
+pkg.resolutions = pkg.resolutions || {};
+if (pkg.resolutions.graphql.startsWith(version)) {
+  console.info(`GraphQL v${version} already installed! Skipping.`);
+}
+
+pkg.dependencies.graphql = `^${version}`;
+pkg.resolutions.graphql = `^${version}`;
+
+writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), 'utf8');
