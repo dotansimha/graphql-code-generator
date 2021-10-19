0416734bc0a1340a50438c29acddcec49f9d9d15
Aleksandr Mischenko <h-alex@bk.ru>
2021-10-19 19:50:47 +0300

    fix: documentation

--- packages/plugins/typescript/react-query/src/config.ts
+++ packages/plugins/typescript/react-query/src/config.ts
@@ -57,18 +57,18 @@ export interface ReactQueryRawPluginConfig
    */
   exposeQueryKeys?: boolean;
 
-   /**
+  /**
    * @default false
-   * @description For each generate mutation hook adds getKey(variables: QueryVariables) function. Useful for call outside of functional component.
+   * @description For each generate mutation hook adds getKey() function. Useful for call outside of functional component.
    * @exampleMarkdown
    * ```ts
    *  const mutation = useUserDetailsMutation(...);
-   *  const key = useUserDetailsMutation.getKey({id: theUsersId});
+   *  const key = useUserDetailsMutation.getKey();
    * ```
    */
-     exposeMutationKeys?: boolean;
+  exposeMutationKeys?: boolean;
 
- /**
+  /**
    * @default false
    * @description For each generate query hook adds `fetcher` field with a corresponding GraphQL query using the fetcher.
    * It is useful for `queryClient.fetchQuery` and `queryClient.prefetchQuery`.
