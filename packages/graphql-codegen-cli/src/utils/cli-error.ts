fb52466866d03513b8509900d08c75d079f83be1
jlmessenger <jlmessenger@users.noreply.github.com>
2021-10-27 15:10:13 -0400

    Display detailed errors from CLI

--- packages/graphql-codegen-cli/src/utils/cli-error.ts
+++ packages/graphql-codegen-cli/src/utils/cli-error.ts
@@ -1,10 +1,29 @@
+import { DetailedError, isDetailedError } from '@graphql-codegen/plugin-helpers';
+
 import { isBrowser, isNode } from './is-browser';
+
+type CompositeError = Error | DetailedError;
+type MultipleError = Error & { errors?: CompositeError[] };
+
+function isError(err: any): err is MultipleError {
+  return err instanceof Error;
+}
 
 export function cliError(err: any, exitOnError = true) {
   let msg: string;
 
-  if (err instanceof Error) {
+  if (isError(err)) {
     msg = err.message || err.toString();
+    if (Array.isArray(err.errors) && err.errors.length) {
+      const allErrs = err.errors.map(errItem => {
+        let subMsg = errItem.message || errItem.toString();
+        if (isDetailedError(errItem)) {
+          subMsg = `${subMsg} for "${errItem.source}"${errItem.details}`;
+        }
+        return subMsg;
+      });
+      msg = `${msg}\n${allErrs.join("\n")}`;
+    }
   } else if (typeof err === 'string') {
     msg = err;
   } else {
