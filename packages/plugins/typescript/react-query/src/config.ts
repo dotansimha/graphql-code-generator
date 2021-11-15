2fc8c2ba1f0c46c789fd231ff6912c433c0baf79
Chris <chris.zirkel@gmail.com>
2021-11-15 10:03:08 +0100

    issue 6794: add stage 2 failing test case

--- packages/plugins/typescript/react-query/src/config.ts
+++ packages/plugins/typescript/react-query/src/config.ts
@@ -1,6 +1,6 @@
 import { RawClientSideBasePluginConfig } from '@graphql-codegen/visitor-plugin-common';
 
-export type HardcodedFetch = { endpoint: string; fetchParams?: string };
+export type HardcodedFetch = { endpoint: string; fetchParams?: string | Record<string, any> };
 export type CustomFetch = { func: string; isReactHook?: boolean } | string;
 
 /**
