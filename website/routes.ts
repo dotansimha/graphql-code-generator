9b16f7e0db0b43cf9c20ce7cdfa6b030479cf9a2
Dotan Simha <dotansimha@gmail.com>
2021-11-24 16:13:00 +0200

    website fixes

--- website/routes.ts
+++ website/routes.ts
@@ -4,62 +4,67 @@ import { GenerateRoutes } from '@guild-docs/server';
 export function getRoutes(): IRoutes {
   const Routes: IRoutes = {
     _: {
-      docs: {
-        $name: 'Docs',
-        _: {
-          'getting-started': {
-            $name: 'Getting Started',
-            $routes: [
-              'index',
-              'installation',
-              'development-workflow',
-              '$config-reference',
-              'programmatic-usage',
-              'further-reading',
-            ],
-            _: {
-              'config-reference': {
-                $name: 'Config Reference',
-                $routes: [
-                  'codegen-config',
-                  'schema-field',
-                  'documents-field',
-                  'config-field',
-                  'require-field',
-                  'naming-convention',
-                  'lifecycle-hooks',
-                ],
-              },
-            },
-          },
-          integrations: {
-            $name: 'Integrations',
-            $routes: ['apollo-local-state', 'create-react-app', 'gatsby', 'prettier', 'federation', 'vscode'],
-          },
-          'custom-codegen': {
-            $name: 'Custom Plugins',
-            $routes: [
-              'index',
-              'write-your-plugin',
-              'validate-configuration',
-              'extend-schema',
-              'using-visitor',
-              'contributing',
-            ],
-          },
-          migration: {
-            $name: 'Migration Guide',
-            $routes: ['from-0-18', 'from-0-13'],
-          },
-        },
+      'docs/getting-started': {
+        $name: 'Getting Started',
+        $routes: [
+          ['index', 'Introduction'],
+          ['installation', 'Installation'],
+          ['development-workflow', 'Development Workflow'],
+          ['further-reading', 'Further Reading'],
+        ],
+      },
+      'docs/config-reference': {
+        $name: 'Config Reference',
+        $routes: [
+          ['codegen-config', 'codegen.yml'],
+          ['schema-field', 'schema field'],
+          ['documents-field', 'documents field'],
+          ['config-field', 'plugin config'],
+          ['require-field', 'require field'],
+          ['naming-convention', 'Naming Convention'],
+        ],
+      },
+      'docs/advanced': {
+        $name: 'Advanced Usage',
+        $routes: [
+          ['programmatic-usage', 'Programmatic Usage'],
+          ['lifecycle-hooks', 'Lifecycle Hooks'],
+        ],
+      },
+      'docs/integrations': {
+        $name: 'Integrations',
+        $routes: [
+          ['vscode', 'VSCode Extension'],
+          ['prettier', 'Prettier'],
+          ['federation', 'Apollo Federation'],
+          'apollo-local-state',
+          'create-react-app',
+          ['gatsby', 'GastbyJS'],
+        ],
+      },
+      'docs/custom-codegen': {
+        $name: 'Custom Plugins',
+        $routes: [
+          ['index', 'What are Plugins?'],
+          ['write-your-plugin', 'Write Your Plugin'],
+          ['validate-configuration', 'Validate Configuration'],
+          ['extend-schema', 'Extend Schema'],
+          ['using-visitor', 'Using Visitor Pattern'],
+          ['contributing', 'Contributing'],
+        ],
+      },
+      'docs/migration': {
+        $name: 'Migration Guides',
+        $routes: [
+          ['from-0-18', 'v0.18 -> v1.0'],
+          ['from-0-13', 'v0.13 -> v0.17'],
+        ],
       },
     },
   };
 
   GenerateRoutes({
     Routes,
-    folderPattern: 'docs',
-    basePath: 'docs',
   });
 
   return Routes;
