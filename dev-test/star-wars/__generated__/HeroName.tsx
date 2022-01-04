c9386e87ad2f32b55e564bcfd4b7ee951c4b4054
Dotan Simha <dotansimha@gmail.com>
2022-01-04 19:40:39 +0200

    typescript-react-apollo: generate "defaultOptions" as const object

--- dev-test/star-wars/__generated__/HeroName.tsx
+++ dev-test/star-wars/__generated__/HeroName.tsx
@@ -2,7 +2,7 @@ import * as Types from '../types.d';
 
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
-const defaultOptions = {};
+const defaultOptions = {} as const;
 export type HeroNameQueryVariables = Types.Exact<{
   episode?: Types.InputMaybe<Types.Episode>;
 }>;
