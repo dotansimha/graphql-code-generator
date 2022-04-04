1f4adf48e833382fef9b8a091c13dc0dcf0db491
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-04-04 15:21:19 +0200

    enable `prefer-arrow-callback` rule

--- packages/plugins/flow/flow/tests/validate-flow.ts
+++ packages/plugins/flow/flow/tests/validate-flow.ts
@@ -1,17 +1,14 @@
 import { Types, mergeOutputs } from '@graphql-codegen/plugin-helpers';
+
 const flow = require('./flow.js');
 
 export function validateFlow(code: Types.PluginOutput) {
   const errors = flow.checkContent('temp.flow.js', mergeOutputs([code]));
 
-  const lint = errors.map(function (err) {
+  const lint = errors.map(err => {
     const messages = err.message;
     const firstLoc = messages[0].loc;
-    const message = messages
-      .map(function (msg) {
-        return msg.descr;
-      })
-      .join('\n');
+    const message = messages.map(msg => msg.descr).join('\n');
 
     return `[l${firstLoc.start.line},c${firstLoc.start.column}][${err.level}]: ${message}`;
   });
