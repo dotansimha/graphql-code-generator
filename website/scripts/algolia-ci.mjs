c8d122c4c04cff875c498b7e93fbb93e1bb43f11
Charly POLY <cpoly55@gmail.com>
2022-05-04 18:24:32 +0200

    feat(website): new algolia indexing

---
+++ website/scripts/algolia-ci.mjs
@@ -0,0 +1,26 @@
+import { createRequire } from 'node:module';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { indexToAlgolia } from '@guild-docs/algolia';
+import { register } from 'esbuild-register/dist/node.js';
+import { getPackagesData } from '@guild-docs/server/npm';
+
+register({ extensions: ['.ts', '.tsx'] });
+
+const require = createRequire(import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const { getRoutes } = require('../routes.ts');
+const { packageList } = require('../src/lib/plugins.ts');
+
+
+getPackagesData({ packageList }).then(plugins => {
+  indexToAlgolia({
+    routes: [getRoutes()],
+    plugins: plugins,
+    source: 'Code Generator',
+    dryMode: process.env.ALGOLIA_DRY_RUN === 'true',
+    domain: process.env.SITE_URL,
+    lockfilePath: resolve(__dirname, '../algolia-lockfile.json'),
+  });
+});
