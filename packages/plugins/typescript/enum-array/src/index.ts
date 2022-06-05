66af58413f6bf0f6484d712e97d2cb955c94eeca
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-06-04 17:53:19 -0700

    Update index.ts

--- packages/plugins/typescript/enum-array/src/index.ts
+++ packages/plugins/typescript/enum-array/src/index.ts
@@ -35,13 +35,11 @@ export const plugin: PluginFunction<EnumArrayPluginConfig> = (
   _documents: Types.DocumentFile[],
   config: EnumArrayPluginConfig
 ): Types.PluginOutput => {
-  const importFrom: EnumArrayPluginConfig['importFrom'] = config.importFrom;
-  const enums = getEnumTypeMap(schema);
-  const content = enums.map(buildArrayDefinition).join('\n');
-
   const result: Types.PluginOutput = { content };
-  if (importFrom) {
-    result['prepend'] = buildImportStatement(enums, importFrom);
+  if (config.importFrom) {
+    const enums = getEnumTypeMap(schema);
+    const content = enums.map(buildArrayDefinition).join('\n');
+    result['prepend'] = buildImportStatement(enums, config.importFrom);
   }
   return result;
 };
