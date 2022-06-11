eb0f8527defaa00fb5fd8f1482ab25c17641891f
Taeho Kim <dittos@gmail.com>
2022-06-11 15:57:16 +0900

    Fix TS type error on strictNullChecks: true
    `Partial<T>` includes `{}`, therefore `NewVisitor[keyof NewVisitor]` includes `undefined`, and indexing `undefined` is error.
    
    Issue: https://github.com/dotansimha/graphql-code-generator/issues/7519

--- packages/utils/plugins-helpers/src/oldVisit.ts
+++ packages/utils/plugins-helpers/src/oldVisit.ts
@@ -3,8 +3,8 @@ import { ASTNode, visit } from 'graphql';
 type VisitFn = typeof visit;
 type NewVisitor = Partial<Parameters<VisitFn>[1]>;
 type OldVisitor = {
-  enter?: Partial<Record<keyof NewVisitor, NewVisitor[keyof NewVisitor]['enter']>>;
-  leave?: Partial<Record<keyof NewVisitor, NewVisitor[keyof NewVisitor]['leave']>>;
+  enter?: Partial<Record<keyof NewVisitor, NonNullable<NewVisitor[keyof NewVisitor]>['enter']>>;
+  leave?: Partial<Record<keyof NewVisitor, NonNullable<NewVisitor[keyof NewVisitor]>['leave']>>;
 } & NewVisitor;
 
 export function oldVisit(
