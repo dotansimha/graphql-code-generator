5c7592b4dde8a8035c33eeeb89a1682cdf2a34fa
marocchino <marocchino@users.noreply.github.com>
2022-07-26 18:13:50 +0900

    feat(react-query): Add legacyMode for support new version (#8124)
    * feat(react-query): Add legacyMode for support new version
    
    * fix(plugins/typescript-react-query): `legacyMode: false` by default, bump major and typing fixes for 4.0
    
    * chore: temp enable canary for testing (revert me)
    
    * Revert "chore: temp enable canary for testing (revert me)"
    
    This reverts commit db728d1c6b1c7f3ea512fd2f5b09064eeccea886.
    
    * doc(plugins/typescript-react-query): proper warning for rq < 4
    
    * Update packages/plugins/typescript/react-query/CHANGELOG.md
    
    * doc: prettier
    
    * fix(plugins/typescript-react-query): Apply new option type to hardcoded
    
    * revert(plugins/typescript-react-query): revert signatures typings changed
    
    * doc(plugins/react-query): better warning for v4
    
    * doc(plugins/react-query): last doc fix
    
    Co-authored-by: Charly POLY <cpoly55@gmail.com>
    Co-authored-by: Charly POLY <1252066+charlypoly@users.noreply.github.com>

--- packages/plugins/typescript/react-query/src/visitor.ts
+++ packages/plugins/typescript/react-query/src/visitor.ts
@@ -29,6 +29,7 @@ export interface ReactQueryPluginConfig extends ClientSideBasePluginConfig {
   exposeMutationKeys: boolean;
   exposeFetcher: boolean;
   addInfiniteQuery: boolean;
+  legacyMode: boolean;
 }
 
 export interface ReactQueryMethodMap {
@@ -81,6 +82,7 @@ export class ReactQueryVisitor extends ClientSideBaseVisitor<ReactQueryRawPlugin
       exposeMutationKeys: getConfigValue(rawConfig.exposeMutationKeys, false),
       exposeFetcher: getConfigValue(rawConfig.exposeFetcher, false),
       addInfiniteQuery: getConfigValue(rawConfig.addInfiniteQuery, false),
+      legacyMode: getConfigValue(rawConfig.legacyMode, false),
     });
     this._externalImportPrefix = this.config.importOperationTypesFrom ? `${this.config.importOperationTypesFrom}.` : '';
     this._documents = documents;
@@ -118,10 +120,6 @@ export class ReactQueryVisitor extends ClientSideBaseVisitor<ReactQueryRawPlugin
       return baseImports;
     }
 
-    if (this.config.addInfiniteQuery) {
-      this.reactQueryOptionsIdentifiersInUse.add('QueryFunctionContext');
-    }
-
     const hookAndTypeImports = [
       ...Array.from(this.reactQueryHookIdentifiersInUse),
       ...Array.from(this.reactQueryOptionsIdentifiersInUse).map(
@@ -129,7 +127,9 @@ export class ReactQueryVisitor extends ClientSideBaseVisitor<ReactQueryRawPlugin
       ),
     ];
 
-    return [...baseImports, `import { ${hookAndTypeImports.join(', ')} } from 'react-query';`];
+    const moduleName = this.config.legacyMode ? 'react-query' : '@tanstack/react-query';
+
+    return [...baseImports, `import { ${hookAndTypeImports.join(', ')} } from '${moduleName}';`];
   }
 
   public getFetcherImplementation(): string {
