d1415cbaa091057e34e7ec703ff89e60b3037eee
Charly POLY <cpoly55@gmail.com>
2022-08-11 17:12:08 +0200

    examples(front-end/vue): `@vue/apollo-composable`

---
+++ examples/front-end/vue/apollo-composable/src/components/FilmItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { gql } from '../gql/gql';
+import { type FragmentType, useFragment } from '../gql/fragment-masking';
+
+const FilmFragment = gql(/* GraphQL */ `
+  fragment FilmItem on Film {
+    id
+    title
+    releaseDate
+    producers
+  }
+`);
+
+const props = defineProps<{
+  film: FragmentType<typeof FilmFragment>;
+}>();
+
+const filmObj = useFragment(FilmFragment, props.film);
+</script>
+
+<template>
+  <div>
+    <h3>{{ filmObj.title }}</h3>
+    <p>{{ filmObj.releaseDate }}</p>
+  </div>
+</template>
