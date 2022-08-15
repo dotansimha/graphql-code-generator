3da35be8ce6fde922f779f23055c4bf684f9c0ee
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-15 11:04:52 +0200

    add basePath for the website

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,13 +2,11 @@ import { withGuildDocs } from 'guild-docs/next.config';
 import { CategoryToPackages } from './src/category-to-packages.mjs';
 
 const PLUGINS_REDIRECTS = Object.entries(CategoryToPackages).flatMap(([category, packageNames]) =>
-  packageNames.map(packageName => ({
-    source: `/plugins/${packageName}`,
-    destination: `/plugins/${category}/${packageName}`,
-  }))
+  packageNames.map(packageName => [`/plugins/${packageName}`, `/plugins/${category}/${packageName}`])
 );
 
 export default withGuildDocs({
+  basePath: '/graphql/codegen',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -24,54 +22,23 @@ export default withGuildDocs({
     return config;
   },
   redirects: () =>
-    [
-      {
-        source: '/docs/presets/:presetName',
-        destination: '/plugins/:presetName-preset',
-      },
-      {
-        source: '/docs/plugins/:pluginName',
-        destination: '/plugins/:pluginName',
-      },
-      {
-        source: '/docs/getting-started/config-reference/codegen-config',
-        destination: '/docs/config-reference/codegen-config',
-      },
-      {
-        source: '/docs/getting-started/codegen-config',
-        destination: '/docs/config-reference/codegen-config',
-      },
-      {
-        source: '/docs/getting-started/documents-field',
-        destination: '/docs/config-reference/documents-field',
-      },
-      {
-        source: '/docs/getting-started/schema-field',
-        destination: '/docs/config-reference/schema-field',
-      },
-      {
-        source: '/docs/getting-started/config-field',
-        destination: '/docs/config-reference/config-field',
-      },
-      {
-        source: '/docs/getting-started/lifecycle-hooks',
-        destination: '/docs/config-reference/lifecycle-hooks',
-      },
-      {
-        source: '/docs/getting-started/require-field',
-        destination: '/docs/config-reference/require-field',
-      },
-      {
-        source: '/docs/getting-started/naming-convention',
-        destination: '/docs/config-reference/naming-convention',
-      },
-      {
-        source: '/docs/getting-started/how-does-it-work',
-        destination: '/docs/advanced/how-does-it-work',
-      },
-      ...PLUGINS_REDIRECTS,
-    ].map(redirect => ({
-      ...redirect,
-      permanent: true,
-    })),
+    Object.entries({
+      '/docs/presets/:presetName': '/plugins/:presetName-preset',
+      '/docs/plugins/:pluginName': '/plugins/:pluginName',
+      '/docs/getting-started/config-reference/codegen-config': '/docs/config-reference/codegen-config',
+      '/docs/getting-started/codegen-config': '/docs/config-reference/codegen-config',
+      '/docs/getting-started/documents-field': '/docs/config-reference/documents-field',
+      '/docs/getting-started/schema-field': '/docs/config-reference/schema-field',
+      '/docs/getting-started/config-field': '/docs/config-reference/config-field',
+      '/docs/getting-started/lifecycle-hooks': '/docs/config-reference/lifecycle-hooks',
+      '/docs/getting-started/require-field': '/docs/config-reference/require-field',
+      '/docs/getting-started/naming-convention': '/docs/config-reference/naming-convention',
+      '/docs/getting-started/how-does-it-work': '/docs/advanced/how-does-it-work',
+    })
+      .concat(PLUGINS_REDIRECTS)
+      .map(([from, to]) => ({
+        source: from,
+        destination: to,
+        permanent: true,
+      })),
 });
