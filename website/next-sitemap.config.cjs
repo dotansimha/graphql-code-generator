38987059b135127a499f4dda9e3b40d598c1f6d8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-10-31 13:58:43 +0100

    try to fix sitemap.xml

--- website/next-sitemap.config.cjs
+++ website/next-sitemap.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.graphql-code-generator.com',
+  siteUrl: process.env.SITE_URL || 'https://graphql-code-generator.com',
   generateIndexSitemap: false,
 };
