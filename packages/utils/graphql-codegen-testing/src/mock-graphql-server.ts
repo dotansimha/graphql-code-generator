238ce94c6b4c0a64165cfaca7fb9d8369d956fce
Charly POLY <cpoly55@gmail.com>
2022-11-02 11:05:25 +0100

    refactor: replace `graphql-helix` by `graphql-yoga`

--- packages/utils/graphql-codegen-testing/src/mock-graphql-server.ts
+++ packages/utils/graphql-codegen-testing/src/mock-graphql-server.ts
@@ -1,6 +1,6 @@
 import { GraphQLSchema } from 'graphql';
 import nock from 'nock';
-import { getGraphQLParameters, processRequest as processGraphQLHelixRequest } from 'graphql-helix';
+import { createYoga } from 'graphql-yoga';
 
 export function mockGraphQLServer({
   schema,
@@ -15,6 +15,8 @@ export function mockGraphQLServer({
   intercept?: (obj: nock.ReplyFnContext) => void;
   method?: string;
 }) {
+  const yoga = createYoga({ schema });
+
   const handler = async function (this: nock.ReplyFnContext, uri: string, body: any) {
     if (intercept) {
       intercept(this);
@@ -22,33 +24,20 @@ export function mockGraphQLServer({
     const uriObj = new URL(host + uri);
     const queryObj: any = {};
     uriObj.searchParams.forEach((val, key) => (queryObj[key] = val));
-    // Create a generic Request object that can be consumed by Graphql Helix's API
     const request = {
-      body,
+      body: JSON.stringify(body),
       headers: this.req.headers,
       method,
       query: queryObj,
     };
-    // Extract the GraphQL parameters from the request
-    const { operationName, query, variables } = getGraphQLParameters(request);
 
-    // Validate and execute the query
-    const result = await processGraphQLHelixRequest({
-      operationName,
-      query,
-      variables,
-      request,
-      schema,
-    });
-    // processRequest returns one of three types of results depending on how the server should respond
-    // 1) RESPONSE: a regular JSON payload
-    // 2) MULTIPART RESPONSE: a multipart response (when @stream or @defer directives are used)
-    // 3) PUSH: a stream of events to push back down the client for a subscription
-    if (result.type === 'RESPONSE') {
+    const response = await yoga.fetch('http://localhost:4000/graphql', request);
+
+    if (response) {
       const headers = {};
       // We set the provided status and headers and just the send the payload back to the client
-      result.headers.forEach(({ name, value }) => (headers[name] = value));
-      return [result.status, result.payload, headers];
+      response.headers.forEach(([value, name]) => (headers[name] = value));
+      return [response.status, await response.json(), headers];
     }
     return [500, 'Not implemented'];
   };
