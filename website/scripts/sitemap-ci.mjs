3de061a47d5f3f4f73a5eebda6c48f26ce7bdb00
Tuval Simha <tuval.simha@Gmail.com>
2022-11-08 17:42:55 +0200

    Sitemap: Fix sitemap generate file (#8589)
    * Sitemap: Fix sitemap generate file
    
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>
    
    * some
    
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>
    
    * delete console log
    
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>
    
    * fix
    
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>
    
    * Dima fix sitemap ci script
    
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>
    
    * Update website/package.json
    
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>
    
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

--- website/scripts/sitemap-ci.mjs
+++ website/scripts/sitemap-ci.mjs
@@ -1,37 +1,25 @@
 import { XMLParser } from 'fast-xml-parser';
-import { fileURLToPath } from 'node:url';
-import { dirname } from 'node:path';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import config from '../next.config.mjs';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const parser = new XMLParser();
 
-const sitemapPath = path.join(__dirname, '..', 'public', 'sitemap.xml');
-const lockfilePath = path.join(__dirname, '..', 'route-lockfile.txt');
+const sitemapPath = path.join(process.cwd(), 'public', 'sitemap.xml');
+const { urlset } = parser.parse(await fs.readFile(sitemapPath, 'utf8'));
 
-async function main() {
-  const parser = new XMLParser();
+const routes = urlset.url.map(url => new URL(url.loc).pathname);
+const redirectsPointingToNonExistingStuff = [];
 
-  const d = parser.parse(fs.readFileSync(sitemapPath, 'utf-8'));
+const redirects = await config.redirects();
 
-  const routes = d.urlset.url.map(url =>
-    url.loc.replace(process.env.SITE_URL || `https://www.graphql-code-generator.com`, ``)
-  );
-
-  const redirectsPointingToNonExistingStuff = [];
-
-  const redirects = await config.redirects();
-
-  for (const redirect of redirects) {
-    if (routes.includes(redirect.destination) === false) {
-      redirectsPointingToNonExistingStuff.push(redirect);
-    } else {
-      routes.push(`${redirect.source} -> ${redirect.destination}`);
-    }
+for (const redirect of redirects) {
+  if (routes.includes(redirect.destination)) {
+    routes.push(`${redirect.source} -> ${redirect.destination}`);
+  } else {
+    redirectsPointingToNonExistingStuff.push(redirect);
   }
-
-  fs.writeFileSync(lockfilePath, routes.sort().join(`\n`) + `\n`);
 }
 
-main();
+const lockfilePath = path.join(process.cwd(), 'route-lockfile.txt');
+await fs.writeFile(lockfilePath, routes.sort().join('\n') + '\n');
