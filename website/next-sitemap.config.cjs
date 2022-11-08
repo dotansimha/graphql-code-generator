4518b436a38624eec6adc8d48694381c18690c5b
Tuval Simha <tuval.simha@gmail.com>
2022-11-08 14:59:50 +0200

    Sitemap: fix siteUrl
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>

--- website/next-sitemap.config.cjs
+++ website/next-sitemap.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.graphql-code-generator.com',
+  siteUrl: process.env.SITE_URL || 'https://the-guild.dev/graphql/codegen',
   generateIndexSitemap: false,
 };
