54db06afa7a636234e64ac704267631dfcd2e658
Jan Nicklas <j.nicklas@me.com>
2022-11-23 09:43:14 +0100

    regenerate gql files

--- dev-test/gql-tag-operations-urql/gql/gql.d.ts
+++ dev-test/gql-tag-operations-urql/gql/gql.d.ts
@@ -2,12 +2,21 @@
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 declare module '@urql/core' {
+  /**
+   * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+   */
   export function gql(
     source: '\n  query Foo {\n    Tweets {\n      id\n    }\n  }\n'
   ): typeof import('./graphql.js').FooDocument;
+  /**
+   * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+   */
   export function gql(
     source: '\n  fragment Lel on Tweet {\n    id\n    body\n  }\n'
   ): typeof import('./graphql.js').LelFragmentDoc;
+  /**
+   * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+   */
   export function gql(
     source: '\n  query Bar {\n    Tweets {\n      ...Lel\n    }\n  }\n'
   ): typeof import('./graphql.js').BarDocument;
