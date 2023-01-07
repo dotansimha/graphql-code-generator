55e2f48ee133b53f0c92f055e185c43b537cb47f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-07 19:11:00 +0100

    fixes

--- packages/plugins/other/time/tests/time.spec.ts
+++ packages/plugins/other/time/tests/time.spec.ts
@@ -1,12 +1,12 @@
 import { plugin } from '../src/index.js';
 
 describe('Time', () => {
-  it('Should use default comment when extenion is unknown', async () => {
+  it('Should use default comment when extension is unknown', async () => {
     const result = await plugin(null as any, [], null, { outputFile: null });
     expect(result).toContain('// Generated on');
   });
 
-  it('Should use # prefix for comment when extenion is graphql', async () => {
+  it('Should use # prefix for comment when extension is graphql', async () => {
     const result = await plugin(null as any, [], null, { outputFile: 'schema.graphql' });
     expect(result).toContain('# Generated on');
   });
