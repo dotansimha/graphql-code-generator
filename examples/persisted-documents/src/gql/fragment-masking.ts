e05d575d91a67d6ca1d77f47aee792366de1db28
Laurin Quast <laurinquast@googlemail.com>
2023-01-31 16:12:44 +0100

    lint gods amoing us

--- examples/persisted-documents/src/gql/fragment-masking.ts
+++ examples/persisted-documents/src/gql/fragment-masking.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { ResultOf, TypedDocumentNode as DocumentNode,  } from '@graphql-typed-document-node/core';
 
 
--- examples/typescript-esm/src/gql/gql.ts
+++ examples/typescript-esm/src/gql/gql.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+ 
 import * as types from './graphql.js';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
