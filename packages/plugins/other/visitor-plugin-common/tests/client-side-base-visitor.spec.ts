d2c4a4c0d700fa20a47eff181754e243a8ab1e87
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2023-02-03 13:19:20 +0100

    adds test for preserving extension
    Signed-off-by: Benjamin Kroeger <benjamin.kroeger@gmail.com>

--- packages/plugins/other/visitor-plugin-common/tests/client-side-base-visitor.spec.ts
+++ packages/plugins/other/visitor-plugin-common/tests/client-side-base-visitor.spec.ts
@@ -82,4 +82,51 @@ describe('getImports', () => {
       });
     });
   });
+
+  describe('when documentMode "external", importDocumentNodeExternallyFrom is relative path', () => {
+    const schema = buildSchema(/* GraphQL */ `
+      type Query {
+        a: A
+      }
+
+      type A {
+        foo: String
+        bar: String
+      }
+    `);
+
+    describe('when emitLegacyCommonJSImports is false', () => {
+      it('preserves `.js` on Operations import path', () => {
+        const fileName = 'fooBarQuery';
+        const importPath = `./src/queries/${fileName}.js`;
+
+        const document = parse(
+          `query fooBarQuery {
+            a {
+              foo
+              bar
+            }
+          }
+        `
+        );
+
+        const visitor = new ClientSideBaseVisitor(
+          schema,
+          [],
+          {
+            emitLegacyCommonJSImports: false,
+            importDocumentNodeExternallyFrom: importPath,
+            documentMode: DocumentMode.external,
+          },
+          {},
+          [{ document, location: importPath }]
+        );
+
+        visitor.OperationDefinition(document.definitions[0] as OperationDefinitionNode);
+
+        const imports = visitor.getImports();
+        expect(imports[0]).toBe(`import * as Operations from '${importPath}';`);
+      });
+    });
+  });
 });
