79a17dd4c47e10aa303ec48d69680dc83cc1354d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-02-14 08:56:35 +0100

    remove plugin code

--- packages/plugins/typescript/nhost/src/config.ts
+++
@@ -1,7 +0,0 @@
-import { RawTypesConfig } from '@graphql-codegen/visitor-plugin-common';
-
-/**
- * @description This plugin generates the Typescript schema that enables queries and mutations to be typed in the Nhost SDK.
- *
- */
-export interface NhostPluginConfig extends Pick<RawTypesConfig, 'scalars'> {}
