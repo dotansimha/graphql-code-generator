350b14f47999e6551d298dfe565af481cdd9454f
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2023-02-23 16:36:57 -0500

    run `yarn examples:codegen`

--- examples/vue/apollo-composable/src/gql/fragment-masking.ts
+++ examples/vue/apollo-composable/src/gql/fragment-masking.ts
@@ -1,13 +1,21 @@
 import type { ResultOf, TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import type { FragmentDefinitionNode } from 'graphql';
+
+export type FragmentName<TDocumentType extends DocumentNode<any, any>> = TDocumentType extends DocumentNode<
+  infer TType,
+  any
+>
+  ? TType extends { ' $fragmentName'?: infer TKey }
+    ? TKey
+    : never
+  : never;
 
 export type FragmentType<TDocumentType extends DocumentNode<any, any>> = TDocumentType extends DocumentNode<
   infer TType,
   any
 >
-  ? TType extends { ' $fragmentName'?: infer TKey }
-    ? TKey extends string
-      ? { ' $fragmentRefs'?: { [key in TKey]: TType } }
-      : never
+  ? FragmentName<TDocumentType> extends string
+    ? { ' $fragmentRefs'?: { [key in FragmentName<TDocumentType>]: TType } }
     : never
   : never;
 
@@ -47,4 +55,16 @@ export function makeFragmentData<F extends DocumentNode, FT extends ResultOf<F>>
   _fragment: F
 ): FragmentType<F> {
   return data as FragmentType<F>;
+}
+
+export function getFragmentName<TType>(doc: DocumentNode<TType>): FragmentName<DocumentNode<TType, any>> {
+  const fragmentName = doc.definitions
+    .filter((definition): definition is FragmentDefinitionNode => definition.kind === 'FragmentDefinition')
+    .map(definition => definition.name.value as FragmentName<DocumentNode<TType, any>>)[0];
+
+  if (!fragmentName) {
+    throw new Error(`Could not find a fragment name for the provided document: ${doc}`);
+  }
+
+  return fragmentName;
 }
