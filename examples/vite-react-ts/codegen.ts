b777bae6b0ad456054d56c8d3ffe5b892748104e
beerose <alexsandra.sikora@gmail.com>
2023-03-01 10:48:46 +0100

    Add failing test

---
+++ examples/vite-react-ts/codegen.ts
@@ -0,0 +1,14 @@
+import { type CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  documents: ['src/**/*.tsx'],
+  generates: {
+    './src/gql/': {
+      preset: 'client',
+    },
+  },
+  hooks: { afterAllFileWrite: ['prettier --write'] },
+};
+
+export default config;
---
+++ examples/vite-react-ts/src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// const AllFilmsWithVariablesQuery = graphql(/* GraphQL */ `
+//   query allFilmsWithVariablesQuery($first: Int!) {
+//     allFilms(first: $first) {
+//       edges {
+//         node {
+//           ...FilmItem
+//         }
+//       }
+//     }
+//   }
+// `);
+
+function App() {
+  return null;
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
