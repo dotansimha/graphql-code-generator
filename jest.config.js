0816b82a01f2fa8652d47a3f72244b16641c1e38
Laurin Quast <laurinquast@googlemail.com>
2023-03-16 11:57:15 +0100

    chore: upgrade to bob v6

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,5 @@ module.exports =
     : {
         rootDir: __dirname,
         projects: ['<rootDir>/packages/**/*/jest.config.js'],
-        resolver: 'bob-the-bundler/jest-resolver.js',
+        resolver: 'bob-the-bundler/jest-resolver.cjs',
       };
