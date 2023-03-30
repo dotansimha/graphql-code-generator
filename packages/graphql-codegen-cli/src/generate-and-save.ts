396b18149fdc7e2faf58e68f7f8f9506a0b744f8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-03-30 17:01:45 +0200

    Update packages/graphql-codegen-cli/src/generate-and-save.ts

--- packages/graphql-codegen-cli/src/generate-and-save.ts
+++ packages/graphql-codegen-cli/src/generate-and-save.ts
@@ -34,7 +34,6 @@ export async function generate(
             debugLog(`Removed stale file: ${prettyFilename}`);
           }
         });
-        continue;
       }
     }
     previouslyGeneratedFilenames = filenames;
