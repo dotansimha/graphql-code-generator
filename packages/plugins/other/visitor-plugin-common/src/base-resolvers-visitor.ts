4d461c8c5da20651f56646fe82d8473d8e202d87
aidant <15520814+aidant@users.noreply.github.com>
2023-08-16 15:09:29 +1000

    replace record with type literal

--- packages/plugins/other/visitor-plugin-common/src/base-resolvers-visitor.ts
+++ packages/plugins/other/visitor-plugin-common/src/base-resolvers-visitor.ts
@@ -1137,7 +1137,7 @@ export class BaseResolversVisitor<
     return new DeclarationBlock(this._declarationBlockConfig)
       .export()
       .asKind(declarationKind)
-      .withName(this.convertName('ResolversUnionTypes'), `<RefType extends Record<string, unknown>>`)
+      .withName(this.convertName('ResolversUnionTypes'), `<RefType extends { [key: string]: unknown }>`)
       .withComment('Mapping of union types')
       .withBlock(
         Object.entries(this._resolversUnionTypes)
@@ -1155,7 +1155,7 @@ export class BaseResolversVisitor<
     return new DeclarationBlock(this._declarationBlockConfig)
       .export()
       .asKind(declarationKind)
-      .withName(this.convertName('ResolversInterfaceTypes'), `<RefType extends Record<string, unknown>>`)
+      .withName(this.convertName('ResolversInterfaceTypes'), `<RefType extends { [key: string]: unknown }>`)
       .withComment('Mapping of interface types')
       .withBlock(
         Object.entries(this._resolversInterfaceTypes)
