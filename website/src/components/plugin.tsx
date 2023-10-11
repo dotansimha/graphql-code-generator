eeef9a29fe121bacf356eb99cec18ae092d489a9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-11 21:59:33 +0200

    aaa

--- website/src/components/plugin.tsx
+++ website/src/components/plugin.tsx
@@ -1,15 +1,15 @@
 import { ReactElement } from 'react';
-import { useSSG, Tab, Tabs, Callout } from '@theguild/components';
+import { useData, Tabs, Callout } from '@theguild/components';
 import { MDXRemote } from 'next-mdx-remote';
 
 export function PluginHeader(): ReactElement {
   // Get the data from SSG, and render it as a component.
-  const { compiledHeader } = useSSG();
-  return <MDXRemote compiledSource={compiledHeader} components={{ $Tab: Tab, $Tabs: Tabs, Callout }} />;
+  const { compiledHeader } = useData();
+  return <MDXRemote compiledSource={compiledHeader} components={{ $Tabs: Tabs, Callout }} />;
 }
 
 export const PluginApiDocs = (): ReactElement => {
   // Get the data from SSG, and render it as a component.
-  const { compiledSource } = useSSG();
+  const { compiledSource } = useData();
   return <MDXRemote compiledSource={compiledSource} />;
 };
