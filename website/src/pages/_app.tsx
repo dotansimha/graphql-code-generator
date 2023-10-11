dfa088799e7f1f13f3647cd6ffa5b9c1dcc50b2d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-11 22:06:15 +0200

    prettify

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@theguild/components/style.css'
+import '@theguild/components/style.css';
 import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
