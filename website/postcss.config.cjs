82dffb0bdb0e6774efc72534954eaedf354db0d0
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-10-31 17:29:44 +0100

    Add postcss-nesting

--- website/postcss.config.cjs
+++ website/postcss.config.cjs
@@ -1,1 +1,5 @@
-module.exports = require('@theguild/tailwind-config/postcss.config');
+const config = require('@theguild/tailwind-config/postcss.config');
+
+config.plugins['postcss-nesting'] = {};
+
+module.exports = config;
