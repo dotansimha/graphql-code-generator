73a985bfdc94ac19fe83351e1c1224335295e06d
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-11-08 17:33:07 +0100

    Tweak footer logo

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -94,9 +94,9 @@ export default defineConfig({
           logo={{
             href: '/',
             children: (
-              <div className="flex gap-3 text-green-1000">
+              <div className="flex items-center gap-3 text-green-1000">
                 <CodegenIcon className="size-8" />
-                <span className="text-2xl font-medium tracking-[-0.16px]">Codegen</span>
+                <span className="text-2xl font-medium leading-[1.2] tracking-[-0.16px]">Codegen</span>
               </div>
             ),
           }}
