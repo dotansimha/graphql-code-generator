05aa6b4cee6214674b25c9d20df27ce5e0e3927c
azu <azu@users.noreply.github.com>
2024-11-21 19:59:09 +0900

    feat(presets/client): support `enumsAsConst` option (#9981)
    * feat(presets/client): support `enumsAsConst` option
    
    * Create shy-rabbits-juggle.md
    
    * docs: add `enumsAsConst` to `client` preset config
    
    * Add enumsAsConst to preset-client.mdx

--- packages/presets/client/src/index.ts
+++ packages/presets/client/src/index.ts
@@ -127,6 +127,7 @@ export const preset: Types.OutputPreset<ClientPresetConfig> = {
       skipTypename: options.config.skipTypename,
       arrayInputCoercion: options.config.arrayInputCoercion,
       enumsAsTypes: options.config.enumsAsTypes,
+      enumsAsConst: options.config.enumsAsConst,
       futureProofEnums: options.config.futureProofEnums,
       dedupeFragments: options.config.dedupeFragments,
       nonOptionalTypename: options.config.nonOptionalTypename,
