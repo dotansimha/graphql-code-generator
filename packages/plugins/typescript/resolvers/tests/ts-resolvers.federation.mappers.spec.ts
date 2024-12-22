a784d3bf06225f47066ff6e99a270fabb201f32f
Eddy Nguyen <github@eddeee888.me>
2024-12-22 23:18:11 +1100

    Clean up tests

--- packages/plugins/typescript/resolvers/tests/ts-resolvers.federation.mappers.spec.ts
+++ packages/plugins/typescript/resolvers/tests/ts-resolvers.federation.mappers.spec.ts
@@ -1,19 +1,5 @@
 import '@graphql-codegen/testing';
-import { codegen } from '@graphql-codegen/core';
-import { parse } from 'graphql';
-import { TypeScriptResolversPluginConfig } from '../src/config.js';
-import { plugin } from '../src/index.js';
-
-function generate({ schema, config }: { schema: string; config: TypeScriptResolversPluginConfig }) {
-  return codegen({
-    filename: 'graphql.ts',
-    schema: parse(schema),
-    documents: [],
-    plugins: [{ 'typescript-resolvers': {} }],
-    config,
-    pluginMap: { 'typescript-resolvers': { plugin } },
-  });
-}
+import { generate } from './utils';
 
 describe('TypeScript Resolvers Plugin + Apollo Federation - mappers', () => {
   it('generates FederationTypes and use it for reference type', async () => {
