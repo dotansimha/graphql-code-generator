f24f3998e3d0926cd9af41e1a750951165423da5
Eddy Nguyen <github@eddeee888.me>
2025-01-17 22:41:30 +1100

    Avoid using graphql-request in examples because it does not work

--- examples/typescript-graphql-request/src/main.ts
+++ examples/typescript-graphql-request/src/main.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-import { GraphQLClient } from 'graphql-request';
 import { graphql } from './gql';
 import { AllPeopleQueryQuery } from './gql/graphql';
 
@@ -35,14 +33,17 @@ const AllPeopleWithVariablesQueryDocument = graphql(/* GraphQL */ `
 
 const apiUrl = 'https://graphql.org/graphql/';
 
-const client = new GraphQLClient(apiUrl);
-
 export const getPeople = async (first?: number) => {
-  let res: AllPeopleQueryQuery;
-  if (first) {
-    res = await client.request(AllPeopleWithVariablesQueryDocument.toString(), { first });
-  } else {
-    res = await client.request(AllPeopleQueryDocument.toString());
-  }
-  return res?.allPeople?.edges;
+  const request = first
+    ? { query: AllPeopleWithVariablesQueryDocument.toString(), variables: { first } }
+    : { query: AllPeopleQueryDocument.toString() };
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(request),
+  });
+
+  const res: AllPeopleQueryQuery = (await response.json()).data;
+  return res.allPeople?.edges;
 };
