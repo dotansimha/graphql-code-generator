e39b555af19f253f2b83ec46247f38c512679616
Eddy Nguyen <github@eddeee888.me>
2025-01-18 19:41:06 +1100

    Revert "Skip failing tests because of upstream"
    This reverts commit 3286febc718d88007af1f83a021f9826219b934e.

--- examples/typescript-graphql-request/src/main.spec.ts
+++ examples/typescript-graphql-request/src/main.spec.ts
@@ -1,12 +1,12 @@
 import { getPeople } from './main';
 
 describe('TypeScript GraphQL Request tests', () => {
-  it.skip('works without variables', async () => {
+  it('works without variables', async () => {
     const result = await getPeople();
     expect(result?.map(o => o?.node?.name)).toContain('Luke Skywalker');
   });
 
-  it.skip('returns first 3 entries', async () => {
+  it('returns first 3 entries', async () => {
     const result = await getPeople(3);
     expect(result).toHaveLength(3);
   });
