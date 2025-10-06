b2ed0b1abb193526a8454efb6abed33349d64da1
Eddy Nguyen <github@eddeee888.me>
2025-10-06 22:21:27 +1100

    Forward immutableTypes to client preset

--- packages/presets/client/src/index.ts
+++ packages/presets/client/src/index.ts
@@ -141,6 +141,7 @@ export const preset: Types.OutputPreset<ClientPresetConfig> = {
       onlyOperationTypes: options.config.onlyOperationTypes,
       onlyEnums: options.config.onlyEnums,
       customDirectives: options.config.customDirectives,
+      immutableTypes: options.config.immutableTypes,
     };
 
     const visitor = new ClientSideBaseVisitor(options.schemaAst, [], options.config, options.config);
