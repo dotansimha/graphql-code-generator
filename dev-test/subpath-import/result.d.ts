9771e174119a91a2f15c2eade8544a439288fe9f
Eddy Nguyen <ch@eddeee888.me>
2025-11-15 23:16:40 +1100

    [typescript-operations] Generate `Exact` into target file (#10504)
    * Create standalone test
    
    * Add Exact and set up test
    
    * Remove Exact from typescript plugin
    
    * Update tests that switch out Exact
    
    * Update dev tests
    
    * Add changeset
    
    * Update test
    
    * Add InputMaybe Scalars tests
    
    * Fix schema in test
    
    * Fix up examples tests
    
    * Add result enum example

--- dev-test/subpath-import/result.d.ts
+++ dev-test/subpath-import/result.d.ts
@@ -4,7 +4,6 @@ import { TestContext } from '#test-null-value/context';
 import { FiedContextType } from '#test/root';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
