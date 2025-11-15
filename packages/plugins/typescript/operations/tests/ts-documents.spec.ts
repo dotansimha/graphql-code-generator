2e9421ccd756aebb4bd41ba17093f68b070268e0
Eddy Nguyen <ch@eddeee888.me>
2025-11-15 23:16:40 +1100

    [typescript-operations] Generate `Exact` into target file (#10504)
    * Create standalone test
    
    * Add Exact and set up test
    
    * Remove Exact from typescript plugin
    
    * Update tests that switch out Exact
    
    * Update dev tests
    
    * Add changeset
    
    * Update test
    
    * Add InputMaybe Scalars tests
    
    * Fix schema in test
    
    * Fix up examples tests
    
    * Add result enum example

--- packages/plugins/typescript/operations/tests/ts-documents.spec.ts
+++ packages/plugins/typescript/operations/tests/ts-documents.spec.ts
@@ -4071,7 +4071,6 @@ export type Q2Query = { search: Array<
       expect(output).toBeSimilarStringTo(`
         export type Maybe<T> = T | null;
         export type InputMaybe<T> = Maybe<T>;
-        export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
         export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
         export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
         export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
@@ -4181,7 +4180,6 @@ export type Q2Query = { search: Array<
       expect(output).toBeSimilarStringTo(`
         export type Maybe<T> = T | null;
         export type InputMaybe<T> = Maybe<T>;
-        export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
         export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
         export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
         export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
@@ -4269,7 +4267,6 @@ export type Q2Query = { search: Array<
       expect(output).toBeSimilarStringTo(`
         export type Maybe<T> = T | null;
         export type InputMaybe<T> = Maybe<T>;
-        export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
         export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
         export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
         export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
