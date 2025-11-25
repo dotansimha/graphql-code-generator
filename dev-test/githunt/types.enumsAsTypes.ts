046102142aca5ec00b92e43ff4b5c43f2ef1b365
Eddy Nguyen <ch@eddeee888.me>
2025-11-26 00:47:46 +1100

    [typescript-operations] Generate enums referenced in operation Variables (#10508)
    * Collect used enums
    
    * Use enum converter for Enum
    
    * Add changeset
    
    * Add doc
    
    * Update dev tests
    
    * Fix return type issue

--- dev-test/githunt/types.enumsAsTypes.ts
+++ dev-test/githunt/types.enumsAsTypes.ts
@@ -163,6 +163,18 @@ export type Vote = {
 /** The type of vote to record, when submitting a vote */
 export type VoteType = 'CANCEL' | 'DOWN' | 'UP';
 
+/** A list of options for the sort order of the feed */
+export type FeedType =
+  /** Sort by a combination of freshness and score, using Reddit's algorithm */
+  | 'HOT'
+  /** Newest entries first */
+  | 'NEW'
+  /** Highest score entries first */
+  | 'TOP';
+
+/** The type of vote to record, when submitting a vote */
+export type VoteType = 'CANCEL' | 'DOWN' | 'UP';
+
 export type OnCommentAddedSubscriptionVariables = Exact<{
   repoFullName: string;
 }>;
--- packages/plugins/typescript/operations/src/index.ts
+++ packages/plugins/typescript/operations/src/index.ts
@@ -1,6 +1,7 @@
 import { oldVisit, PluginFunction, Types } from '@graphql-codegen/plugin-helpers';
-import { LoadedFragment, optimizeOperations } from '@graphql-codegen/visitor-plugin-common';
-import { concatAST, FragmentDefinitionNode, GraphQLSchema, Kind } from 'graphql';
+import { transformSchemaAST } from '@graphql-codegen/schema-ast';
+import { optimizeOperations } from '@graphql-codegen/visitor-plugin-common';
+import { concatAST, GraphQLSchema } from 'graphql';
 import { TypeScriptDocumentsPluginConfig } from './config.js';
 import { TypeScriptDocumentsVisitor } from './visitor.js';
 
@@ -20,25 +21,11 @@ export const plugin: PluginFunction<TypeScriptDocumentsPluginConfig, Types.Compl
     : rawDocuments;
   const allAst = concatAST(documents.map(v => v.document));
 
-  const allFragments: LoadedFragment[] = [
-    ...(allAst.definitions.filter(d => d.kind === Kind.FRAGMENT_DEFINITION) as FragmentDefinitionNode[]).map(
-      fragmentDef => ({
-        node: fragmentDef,
-        name: fragmentDef.name.value,
-        onType: fragmentDef.typeCondition.name.value,
-        isExternal: false,
-      })
-    ),
-    ...(config.externalFragments || []),
-  ];
+  const visitor = new TypeScriptDocumentsVisitor(schema, config, allAst);
 
-  const visitor = new TypeScriptDocumentsVisitor(schema, config, allFragments);
+  const operationsResult = oldVisit(allAst, { leave: visitor });
 
-  const visitorResult = oldVisit(allAst, {
-    leave: visitor,
-  });
-
-  let content = visitorResult.definitions.join('\n');
+  let operationsContent = operationsResult.definitions.join('\n');
 
   if (config.addOperationExport) {
     const exportConsts = [];
@@ -49,15 +36,29 @@ export const plugin: PluginFunction<TypeScriptDocumentsPluginConfig, Types.Compl
       }
     }
 
-    content = visitorResult.definitions.concat(exportConsts).join('\n');
+    operationsContent = operationsResult.definitions.concat(exportConsts).join('\n');
   }
 
   if (config.globalNamespace) {
-    content = `
+    operationsContent = `
     declare global {
-      ${content}
+      ${operationsContent}
     }`;
   }
+
+  const schemaTypes = oldVisit(transformSchemaAST(schema, config).ast, { leave: visitor });
+
+  // IMPORTANT: when a visitor leaves a node with no transformation logic,
+  // It will leave the node as an object.
+  // Here, we filter in nodes that have been turned into strings, i.e. they have been transformed
+  // This way, we do not have to explicitly declare a method for every node type to convert them to null
+  const schemaTypesContent = schemaTypes.definitions.filter(def => typeof def === 'string').join('\n');
+
+  const content: string[] = [];
+  if (schemaTypesContent) {
+    content.push(schemaTypesContent);
+  }
+  content.push(operationsContent);
 
   return {
     prepend: [
@@ -65,7 +66,7 @@ export const plugin: PluginFunction<TypeScriptDocumentsPluginConfig, Types.Compl
       ...visitor.getGlobalDeclarations(visitor.config.noExport),
       'type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };',
     ],
-    content,
+    content: content.join('\n'),
   };
 };
 
