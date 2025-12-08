d8465a990ab172ba88e6fd34cd7fb65367defd7a
Eddy Nguyen <github@eddeee888.me>
2025-12-08 23:30:51 +1100

    Add test for absolute importSchemaTypesFrom

--- packages/plugins/typescript/operations/tests/ts-documents.standalone.import-types.spec.ts
+++ packages/plugins/typescript/operations/tests/ts-documents.standalone.import-types.spec.ts
@@ -4,7 +4,7 @@ import { mergeOutputs } from '@graphql-codegen/plugin-helpers';
 import { plugin } from '../src/index.js';
 
 describe('TypeScript Operations Plugin - Import Types', () => {
-  it('imports user-defined types externally with importSchemaTypesFrom correctly', async () => {
+  it('imports user-defined types externally with relative importSchemaTypesFrom correctly', async () => {
     const schema = buildSchema(/* GraphQL */ `
       type Query {
         user(id: ID!): User
@@ -136,9 +136,140 @@ describe('TypeScript Operations Plugin - Import Types', () => {
          };
       "
     `);
+  });
 
-    // FIXME: enable this to ensure type correctness
-    // validateTs(content, undefined, undefined, undefined, undefined, true);
+  it('imports user-defined types externally with absolute importSchemaTypesFrom correctly', async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      type Query {
+        user(id: ID!): User
+        users(input: UsersInput!): UsersResponse!
+      }
+
+      type ResponseError {
+        error: ResponseErrorType!
+      }
+
+      enum ResponseErrorType {
+        NOT_FOUND
+        INPUT_VALIDATION_ERROR
+        FORBIDDEN_ERROR
+        UNEXPECTED_ERROR
+      }
+
+      type User {
+        id: ID!
+        name: String!
+        role: UserRole!
+        createdAt: DateTime!
+      }
+
+      "UserRole Description"
+      enum UserRole {
+        "UserRole ADMIN"
+        ADMIN
+        "UserRole CUSTOMER"
+        CUSTOMER
+      }
+
+      "UsersInput Description"
+      input UsersInput {
+        "UsersInput from"
+        from: DateTime
+        "UsersInput to"
+        to: DateTime
+        role: UserRole
+      }
+
+      type UsersResponseOk {
+        result: [User!]!
+      }
+      union UsersResponse = UsersResponseOk | ResponseError
+
+      scalar DateTime
+    `);
+    const document = parse(/* GraphQL */ `
+      query User($id: ID!) {
+        user(id: $id) {
+          id
+          name
+          role
+          createdAt
+        }
+      }
+
+      query Users($input: UsersInput!) {
+        users(input: $input) {
+          ... on UsersResponseOk {
+            result {
+              id
+            }
+          }
+          ... on ResponseError {
+            error
+          }
+        }
+      }
+
+      query UsersWithScalarInput($from: DateTime!, $to: DateTime, $role: UserRole) {
+        users(input: { from: $from, to: $to, role: $role }) {
+          ... on UsersResponseOk {
+            result {
+              __typename
+            }
+          }
+          ... on ResponseError {
+            __typename
+          }
+        }
+      }
+    `);
+
+    const result = mergeOutputs([
+      await plugin(
+        schema,
+        [{ document }],
+        {
+          importSchemaTypesFrom: '~@my-company/package/types',
+          namespacedImportName: 'TypeImport',
+        },
+        { outputFile: './base-dir/this-file.ts' }
+      ),
+    ]);
+
+    expect(result).toMatchInlineSnapshot(`
+      "import type * as TypeImport from '@my-company/package/types';
+
+      type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+      export type UserQueryVariables = Exact<{
+        id: string;
+      }>;
+
+
+      export type UserQuery = { __typename?: 'Query', user?: { __typename?: 'User', id: string, name: string, role: TypeImport.UserRole, createdAt: any } | null };
+
+      export type UsersQueryVariables = Exact<{
+        input: TypeImport.UsersInput;
+      }>;
+
+
+      export type UsersQuery = { __typename?: 'Query', users:
+          | { __typename?: 'UsersResponseOk', result: Array<{ __typename?: 'User', id: string }> }
+          | { __typename?: 'ResponseError', error: TypeImport.ResponseErrorType }
+         };
+
+      export type UsersWithScalarInputQueryVariables = Exact<{
+        from: any;
+        to?: any | null;
+        role?: TypeImport.UserRole | null;
+      }>;
+
+
+      export type UsersWithScalarInputQuery = { __typename?: 'Query', users:
+          | { __typename?: 'UsersResponseOk', result: Array<{ __typename: 'User' }> }
+          | { __typename: 'ResponseError' }
+         };
+      "
+    `);
   });
 
   it('does not import external types if only native GraphQL types are used in Variables and Result', async () => {
