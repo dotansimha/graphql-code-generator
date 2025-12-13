af412df121196376cb2b6c67a4fd149114673c15
Eddy Nguyen <github@eddeee888.me>
2025-12-14 00:07:14 +1100

    Update examples

--- examples/react/apollo-client-defer/src/gql/graphql.ts
+++ examples/react/apollo-client-defer/src/gql/graphql.ts
@@ -1,37 +1,6 @@
 /* eslint-disable */
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = T | null | undefined;
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-/** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: { input: string; output: string };
-  String: { input: string; output: string };
-  Boolean: { input: boolean; output: boolean };
-  Int: { input: number; output: number };
-  Float: { input: number; output: number };
-};
-
-export type Query = {
-  __typename?: 'Query';
-  alphabet: Array<Scalars['String']['output']>;
-  /** A field that resolves fast. */
-  fastField: Scalars['String']['output'];
-  /**
-   * A field that resolves slowly.
-   * Maybe you want to @defer this field ;)
-   */
-  slowField: Scalars['String']['output'];
-};
-
-export type QuerySlowFieldArgs = {
-  waitFor?: Scalars['Int']['input'];
-};
-
 export type SlowFieldFragmentFragment = { __typename?: 'Query'; slowField: string } & {
   ' $fragmentName'?: 'SlowFieldFragmentFragment';
 };
