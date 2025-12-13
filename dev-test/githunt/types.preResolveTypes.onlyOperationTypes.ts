758a809786afe320f861db11babe027ca64346f2
Eddy Nguyen <ch@eddeee888.me>
2025-12-14 00:59:47 +1100

    [typescript-operations][client-preset] Integrate new typescript-operations with client preset (#10540)
    * Do not use typescript and its config in Client Preset
    
    * Fix issue where enumValues are imported even if not used
    
    * Split enum tests for client preset
    
    * Update unit tests
    
    * Update examples
    
    * Generate Incremental utilty type
    
    * Add changeset
    
    * Rebase tests (with errors)

--- dev-test/githunt/types.preResolveTypes.onlyOperationTypes.ts
+++ dev-test/githunt/types.preResolveTypes.onlyOperationTypes.ts
@@ -1,36 +1,5 @@
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-/** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: { input: string; output: string };
-  String: { input: string; output: string };
-  Boolean: { input: boolean; output: boolean };
-  Int: { input: number; output: number };
-  Float: { input: number; output: number };
-};
-
-/** A list of options for the sort order of the feed */
-export enum FeedType {
-  /** Sort by a combination of freshness and score, using Reddit's algorithm */
-  Hot = 'HOT',
-  /** Newest entries first */
-  New = 'NEW',
-  /** Highest score entries first */
-  Top = 'TOP',
-}
-
-/** The type of vote to record, when submitting a vote */
-export enum VoteType {
-  Cancel = 'CANCEL',
-  Down = 'DOWN',
-  Up = 'UP',
-}
-
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 export type OnCommentAddedSubscriptionVariables = Exact<{
   repoFullName: string;
 }>;
