297d9695a6f94f6bd9b0acfe8e90cb0ac3752835
Eddy Nguyen <github@eddeee888.me>
2025-12-28 23:31:26 +1100

    Update tests

--- dev-test/test-schema/types.preResolveTypes.onlyOperationTypes.ts
+++ dev-test/test-schema/types.preResolveTypes.onlyOperationTypes.ts
@@ -3,7 +3,6 @@ export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' |
 export type TestQueryVariables = Exact<{ [key: string]: never }>;
 
 export type TestQuery = {
-  __typename?: 'Query';
   testArr1: Array<string | null> | null;
   testArr2: Array<string | null>;
   testArr3: Array<string>;
