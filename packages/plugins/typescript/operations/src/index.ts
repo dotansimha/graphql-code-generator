c2be6b30ecf8f34a72f3671a36cb6d17a461cdc2
Eddy Nguyen <ch@eddeee888.me>
2026-01-06 22:34:09 +1100

    Adjust config inheritance to reflect usage and type-hint better (#10560)
    * Refactor utility function to match practice
    
    * Fix config option inheritance
    
    - addTypename/skipTypename: is only a types-visitor concern. This is moved to types-visitor from base-visitor
    - nonOptionalTypename: is a documents-visitor and types-visitor concern. Moved from base-visitor there
    - extractAllFieldsToTypes: is a documents-visitor concern. Moved from base-visitor there
    - enumPrefix and enumSuffix: need to be in base-visitor as all 3 types of visitors need this to correctly sync the enum type names. This is moved to base visitor
    - ignoreEnumValuesFromSchema: is a documents-visitor and types-visitor concern. Moved from base-visitor there.
    - globalNamespace: is a documents-visitor concern. Moved from base-visitor there
    
    Other changes:
    - documents-visitor no longer extends types-visitor _option types_ as they have two distinct usages now. The types now extend base-visitor types. This is now consistent with documents-visitor extending base-visitor
    - Classes now handle config parsing and types at the same level e.g. if typescript-operations plugin parses configOne, then the types for configOne must be in that class, rather than in base-documents-visitor
    
    * Remove references and tests related to skipTypename in operations plugin
    
    * Add changeset

--- packages/plugins/typescript/operations/src/index.ts
+++ packages/plugins/typescript/operations/src/index.ts
@@ -5,8 +5,6 @@ import { concatAST, GraphQLSchema } from 'graphql';
 import { TypeScriptDocumentsPluginConfig } from './config.js';
 import { TypeScriptDocumentsVisitor } from './visitor.js';
 
-export { TypeScriptDocumentsPluginConfig } from './config.js';
-
 export const plugin: PluginFunction<TypeScriptDocumentsPluginConfig, Types.ComplexPluginOutput> = async (
   inputSchema,
   rawDocuments,
