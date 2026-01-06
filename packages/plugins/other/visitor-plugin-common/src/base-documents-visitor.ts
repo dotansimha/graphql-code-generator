0dee3c5bffb95af038bb29323a1312bf44cc0e76
Eddy Nguyen <ch@eddeee888.me>
2026-01-06 22:34:09 +1100

    Adjust config inheritance to reflect usage and type-hint better (#10560)
    * Refactor utility function to match practice
    
    * Fix config option inheritance
    
    - addTypename/skipTypename: is only a types-visitor concern. This is moved to types-visitor from base-visitor
    - nonOptionalTypename: is a documents-visitor and types-visitor concern. Moved from base-visitor there
    - extractAllFieldsToTypes: is a documents-visitor concern. Moved from base-visitor there
    - enumPrefix and enumSuffix: need to be in base-visitor as all 3 types of visitors need this to correctly sync the enum type names. This is moved to base visitor
    - ignoreEnumValuesFromSchema: is a documents-visitor and types-visitor concern. Moved from base-visitor there.
    - globalNamespace: is a documents-visitor concern. Moved from base-visitor there
    
    Other changes:
    - documents-visitor no longer extends types-visitor _option types_ as they have two distinct usages now. The types now extend base-visitor types. This is now consistent with documents-visitor extending base-visitor
    - Classes now handle config parsing and types at the same level e.g. if typescript-operations plugin parses configOne, then the types for configOne must be in that class, rather than in base-documents-visitor
    
    * Remove references and tests related to skipTypename in operations plugin
    
    * Add changeset

--- packages/plugins/other/visitor-plugin-common/src/base-documents-visitor.ts
+++ packages/plugins/other/visitor-plugin-common/src/base-documents-visitor.ts
@@ -7,8 +7,7 @@ import {
   OperationTypeNode,
   VariableDefinitionNode,
 } from 'graphql';
-import { ParsedTypesConfig, RawTypesConfig } from './base-types-visitor.js';
-import { BaseVisitor } from './base-visitor.js';
+import { BaseVisitor, type RawConfig, type ParsedConfig } from './base-visitor.js';
 import { DEFAULT_SCALARS } from './scalars.js';
 import { SelectionSetToObject } from './selection-set-to-object.js';
 import { CustomDirectivesConfig, NormalizedScalarsMap } from './types.js';
@@ -20,27 +19,16 @@ import {
 } from './utils.js';
 import { OperationVariablesToObject } from './variables-to-object.js';
 
-function getRootType(operation: OperationTypeNode, schema: GraphQLSchema) {
-  switch (operation) {
-    case 'query':
-      return schema.getQueryType();
-    case 'mutation':
-      return schema.getMutationType();
-    case 'subscription':
-      return schema.getSubscriptionType();
-  }
-  throw new Error(`Unknown operation type: ${operation}`);
-}
-
-export interface ParsedDocumentsConfig extends ParsedTypesConfig {
+export interface ParsedDocumentsConfig extends ParsedConfig {
   extractAllFieldsToTypes: boolean;
-  globalNamespace: boolean;
   operationResultSuffix: string;
   dedupeOperationSuffix: boolean;
   omitOperationSuffix: boolean;
   namespacedImportName: string | null;
   exportFragmentSpreadSubTypes: boolean;
   skipTypeNameForRoot: boolean;
+  nonOptionalTypename: boolean;
+  globalNamespace: boolean;
   experimentalFragmentVariables: boolean;
   mergeFragmentTypes: boolean;
   customDirectives: CustomDirectivesConfig;
@@ -48,7 +36,7 @@ export interface ParsedDocumentsConfig extends ParsedTypesConfig {
   importSchemaTypesFrom: string;
 }
 
-export interface RawDocumentsConfig extends RawTypesConfig {
+export interface RawDocumentsConfig extends RawConfig {
   /**
    * @default false
    * @description Avoid adding `__typename` for root types. This is ignored when a selection explicitly specifies `__typename`.
@@ -72,6 +60,30 @@ export interface RawDocumentsConfig extends RawTypesConfig {
    * ```
    */
   skipTypeNameForRoot?: boolean;
+  /**
+   * @default false
+   * @description Automatically adds `__typename` field to the generated types, even when they are not specified
+   * in the selection set, and makes it non-optional
+   *
+   * @exampleMarkdown
+   * ```ts filename="codegen.ts"
+   *  import type { CodegenConfig } from '@graphql-codegen/cli';
+   *
+   *  const config: CodegenConfig = {
+   *    // ...
+   *    generates: {
+   *      'path/to/file': {
+   *        // plugins...
+   *        config: {
+   *          nonOptionalTypename: true
+   *        },
+   *      },
+   *    },
+   *  };
+   *  export default config;
+   * ```
+   */
+  nonOptionalTypename?: boolean;
   /**
    * @default false
    * @description Puts all generated code under `global` namespace. Useful for Stencil integration.
@@ -204,6 +216,14 @@ export interface RawDocumentsConfig extends RawTypesConfig {
    * ```
    */
   importSchemaTypesFrom?: string;
+  /**
+   * @default false
+   * @description Extract all field types to their own types, instead of inlining them.
+   * This helps to reduce type duplication, and makes type errors more readable.
+   * It can also significantly reduce the size of the generated code, the generation time,
+   * and the typechecking time.
+   */
+  extractAllFieldsToTypes?: boolean;
 }
 
 export class BaseDocumentsVisitor<
@@ -223,11 +243,10 @@ export class BaseDocumentsVisitor<
   ) {
     super(rawConfig, {
       exportFragmentSpreadSubTypes: getConfigValue(rawConfig.exportFragmentSpreadSubTypes, false),
-      enumPrefix: getConfigValue(rawConfig.enumPrefix, true),
-      enumSuffix: getConfigValue(rawConfig.enumSuffix, true),
       dedupeOperationSuffix: getConfigValue(rawConfig.dedupeOperationSuffix, false),
       omitOperationSuffix: getConfigValue(rawConfig.omitOperationSuffix, false),
       skipTypeNameForRoot: getConfigValue(rawConfig.skipTypeNameForRoot, false),
+      nonOptionalTypename: getConfigValue(rawConfig.nonOptionalTypename, false),
       namespacedImportName: getConfigValue(rawConfig.namespacedImportName, null),
       experimentalFragmentVariables: getConfigValue(rawConfig.experimentalFragmentVariables, false),
       globalNamespace: !!rawConfig.globalNamespace,
@@ -238,6 +257,7 @@ export class BaseDocumentsVisitor<
       }),
       generatesOperationTypes: getConfigValue(rawConfig.generatesOperationTypes, true),
       importSchemaTypesFrom: getConfigValue(rawConfig.importSchemaTypesFrom, ''),
+      extractAllFieldsToTypes: getConfigValue(rawConfig.extractAllFieldsToTypes, false),
       ...((additionalConfig || {}) as any),
     });
 
@@ -393,4 +413,16 @@ export class BaseDocumentsVisitor<
       .filter(r => r)
       .join('\n\n');
   }
+}
+
+function getRootType(operation: OperationTypeNode, schema: GraphQLSchema) {
+  switch (operation) {
+    case 'query':
+      return schema.getQueryType();
+    case 'mutation':
+      return schema.getMutationType();
+    case 'subscription':
+      return schema.getSubscriptionType();
+  }
+  throw new Error(`Unknown operation type: ${operation}`);
 }
