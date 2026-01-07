4f09ba31b5699f981627d461262ba27189a59689
Eddy Nguyen <ch@eddeee888.me>
2026-01-08 00:09:23 +1100

    Remove unused utility types and functions (#10564)
    * Remove MakeOptional MakeMaybe MakeEmpty and Incremental utitlity types from typescript plugin because they were used for typescript-operations
    
    * Fix unit tests
    
    * Remove deprecated functions
    
    * Add changeset

--- dev-test/subpath-import/result.d.ts
+++ dev-test/subpath-import/result.d.ts
@@ -4,10 +4,6 @@ import { TestContext } from '#test-null-value/context';
 import { FiedContextType } from '#test/root';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 export type EnumResolverSignature<T, AllowedValues = any> = { [key in keyof T]?: AllowedValues };
 export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
