9540a27befb5212c6fbd766bf6406d8ac8289d79
Eddy Nguyen <ch@eddeee888.me>
2026-01-08 00:09:23 +1100

    Remove unused utility types and functions (#10564)
    * Remove MakeOptional MakeMaybe MakeEmpty and Incremental utitlity types from typescript plugin because they were used for typescript-operations
    
    * Fix unit tests
    
    * Remove deprecated functions
    
    * Add changeset

--- dev-test/test-schema/typings.wrapped.ts
+++ dev-test/test-schema/typings.wrapped.ts
@@ -1,10 +1,6 @@
 declare namespace GraphQL {
   export type Maybe<T> = T | null;
   export type InputMaybe<T> = Maybe<T>;
-  export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-  export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-  export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-  export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
   /** All built-in and custom scalars, mapped to their actual values */
   export type Scalars = {
     ID: { input: string; output: string };
