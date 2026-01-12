64b783487359ee022bf307980605d6dbd1cd5aa5
Igor Kusakov <igor@kusakov.com>
2026-01-12 17:27:59 -0500

    Adding back the integration tests + fix them

---
+++ dev-test-apollo-tooling/cli/index.ts
@@ -0,0 +1,108 @@
+#!/usr/bin/env ts-node
+
+import path from 'path';
+import { isEmpty, uniq } from 'lodash-es';
+import { generate } from '@graphql-codegen/cli';
+import type { Types } from '@graphql-codegen/plugin-helpers';
+import deleteAsync from 'del';
+
+export const GENERATED = '__generated__';
+export const GLOBAL_TYPES_FILE = 'globalTypes.ts';
+export const TS_GENERATED_FILE_HEADER = `\
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+`;
+
+/**
+ * The following GraphQL Codegen config matches as closely as possible
+ * to the old apollo-tooling codegen
+ * @see https://github.com/apollographql/apollo-tooling/issues/2053
+ * */
+const GRAPHQL_CODEGEN_CONFIG = {
+  skipTypename: false,
+  useTypeImports: true,
+  preResolveTypes: true, // Simplifies the generated types
+  namingConvention: 'keep', // Keeps naming as-is
+  avoidOptionals: false, // Allow '?' on variables fields
+  nonOptionalTypename: true, // Forces `__typename` on all selection sets
+  skipTypeNameForRoot: true, // Don't generate __typename for root types
+  omitOperationSuffix: true, // Don't add 'Query', 'Mutation' or 'Subscription' suffixes to operation result types
+  fragmentSuffix: '', // Don't add 'Fragment' suffix to fragment result types
+  extractAllFieldsToTypes: true, // Extracts all fields to separate types (similar to apollo-codegen behavior)
+  printFieldsOnNewLines: true, // Prints each field on a new line (similar to apollo-codegen behavior)
+  importTypesNamespace: '', // Disable namespace prefix on imported types
+  enumType: 'native',
+  generatesOperationTypes: true,
+  defaultScalarType: 'any',
+  maybeValue: 'T | null',
+  inputMaybeValue: 'T | null',
+};
+
+export const main = async () => {
+  const cwd = process.cwd();
+
+  await deleteAsync([`src/**/${GENERATED}`], { cwd });
+
+  const localSchemaFilePath = `${cwd}/schema.graphql`;
+
+  const { internalIncludes, externalIncludes } = { internalIncludes: [`${cwd}/src/**/*.ts`], externalIncludes: [] };
+
+  const generateFiles: { [scanPath: string]: Types.ConfiguredOutput } = {};
+
+  const includesForCodegen = uniq(internalIncludes.map((includes: string) => includes.replace(/\/\*\*\/.+$/, '')));
+
+  const globalTypesPath = `${cwd}/src/${GENERATED}/${GLOBAL_TYPES_FILE}`;
+
+  const monorepoRoot = path.resolve(cwd, '..');
+
+  if (!isEmpty(includesForCodegen)) {
+    // Prepare the required structure for GraphQL Codegen
+    includesForCodegen.forEach((includes: string) => {
+      const relativeIncludes = path.relative(monorepoRoot, includes);
+
+      generateFiles[relativeIncludes] = {
+        preset: 'near-operation-file', // This preset tells the codegen to generate multiple files instead of one
+        presetConfig: {
+          extension: '.ts', //  Matches the existing Apollo-Codegen file naming
+          baseTypesPath: `../${path.relative(cwd, globalTypesPath)}`, // Relative (to repo root) path to the global types file to include
+          folder: GENERATED, // Output folder for generated files
+          importTypesNamespace: '', // Disable namespace prefix on imported types
+        },
+        plugins: [
+          'typescript-operations',
+          {
+            add: {
+              content: TS_GENERATED_FILE_HEADER,
+            },
+          },
+        ],
+      };
+    });
+
+    await generate(
+      {
+        schema: localSchemaFilePath,
+        documents: [
+          // matching js extensins as well - there are cases where js files are not converted to typescript yet
+          // (but the package is typescript)
+          ...includesForCodegen.map((include: any) => `${include}/**/*.{js,jsx,ts,tsx}`),
+          ...externalIncludes,
+          `!**/${GENERATED}/**`,
+        ],
+        config: GRAPHQL_CODEGEN_CONFIG,
+        generates: generateFiles,
+        silent: true,
+      },
+      true // overwrite existing files
+    );
+  }
+};
+
+if (import.meta.url === process.argv[1] || import.meta.url === `file://${process.argv[1]}`) {
+  main().catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
+}
---
+++ dev-test-apollo-tooling/src/__generated__/Component.ts
@@ -0,0 +1,58 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import type * as Types from '../../dev-test-apollo-tooling/src/__generated__/globalTypes';
+
+type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export enum UserManagerRoleType {
+  ROLE_TYPE_1 = 'ROLE_TYPE_1',
+  ROLE_TYPE_2 = 'ROLE_TYPE_2',
+  ROLE_TYPE_3 = 'ROLE_TYPE_3',
+}
+
+export type GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager_fooUser_User_profilePhoto_UserProfilePhoto_photoUrl_UserPhotoUrl =
+  {
+    __typename: 'UserPhotoUrl';
+    url: string | null;
+  };
+
+export type GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager_fooUser_User_profilePhoto_UserProfilePhoto =
+  {
+    __typename: 'UserProfilePhoto';
+    photoUrl: GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager_fooUser_User_profilePhoto_UserProfilePhoto_photoUrl_UserPhotoUrl | null;
+  };
+
+export type GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager_fooUser_User = {
+  __typename: 'User';
+  profilePhoto: GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager_fooUser_User_profilePhoto_UserProfilePhoto | null;
+};
+
+export type GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager = {
+  __typename: 'UserManager';
+  roleType: Types.UserManagerRoleType;
+  fooUser: GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager_fooUser_User;
+};
+
+export type GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup = {
+  __typename: 'SuperUserGroup';
+  managers: Array<GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup_managers_UserManager> | null;
+};
+
+export type GetFoo_superUser_SuperUser = {
+  __typename: 'SuperUser';
+  groupFromAlias: GetFoo_superUser_SuperUser_groupFromAlias_SuperUserGroup;
+};
+
+export type GetFoo_Query = {
+  superUser: GetFoo_superUser_SuperUser;
+};
+
+export type GetFooVariables = Exact<{
+  alias: string;
+  collectionId: string;
+}>;
+
+export type GetFoo = GetFoo_Query;
