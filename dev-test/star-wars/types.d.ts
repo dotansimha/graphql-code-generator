3f5db91ae3956f25ff4397416e95d7842849e59a
Eddy Nguyen <ch@eddeee888.me>
2026-01-14 00:52:11 +1100

    Only generate `Exact` utility type if documents have operations (#10571)
    * Remove extraneous test setup
    
    * Do not generate Exact utility type of not used
    
    * Add default test case to check that Exact is used for Mutation and Subscription variables
    
    * Update dev-tests
    
    * Add changeset
    
    * Remove extraneous config for Exact test

--- dev-test/star-wars/types.d.ts
+++ dev-test/star-wars/types.d.ts
@@ -1,2 +1,1 @@
-type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
