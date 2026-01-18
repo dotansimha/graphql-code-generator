78c309223422467e65e714c3b53d015270f3403f
Eddy Nguyen <ch@eddeee888.me>
2026-01-18 23:39:30 +1100

    [typescript-operations] Fix Input generation when using `importSchemaTypesFrom` (#10575)
    * Fix importing issue when using importSchemaTypesFrom
    
    * Add dev-tests for usage with typescript plugin
    
    * Update unit test
    
    * Add changeset

--- dev-test/standalone-operations/import-schema-types/_base.generated.ts
+++ dev-test/standalone-operations/import-schema-types/_base.generated.ts
@@ -4,3 +4,9 @@ export type UserRole =
   | 'ADMIN'
   /** UserRole CUSTOMER */
   | 'CUSTOMER';
+
+export type UsersInput = {
+  name?: string | null | undefined;
+  nestedInput?: UsersInput | null | undefined;
+  role?: UserRole | null | undefined;
+};
