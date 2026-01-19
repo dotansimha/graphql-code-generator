9bed2d69bc3bad564a8a14ccc604abb0dfd17a2d
Igor Kusakov <ikusakov@yelp.com>
2026-01-19 06:21:01 -0500

    Bugfix: enums from external fragments were not generated along with operations (#10565)
    * integration test
    
    * cleanups
    
    * cleanups2
    
    * fixed a bug with external fragments
    
    * cleanup
    
    * keep generated files, refactored visitorwq
    
    * cleanup
    
    * changeset
    
    * adding missing dependency
    
    * delete temporary files
    
    * cleanup
    
    * cleanups
    
    * Adding back the integration tests + fix them
    
    * merged + fixes
    
    * cleanup
    
    * cleanup
    
    * patching near-operation-file preset
    
    * cleanup
    
    * adding duplicates case to integration tests
    
    * Fixing bug with duplicates generated when using extractAllFieldsToTypesCompact
    
    * cleanup
    
    * prevent running prettier on generated code
    
    * cleanups
    
    * cleanup
    
    * adding unit tests
    
    * update changeset
    
    * cleanups
    
    * try resolving graphql 16 error
    
    * remove unused deps
    
    * remove unused deps
    
    * cleanups
    
    * Revert and apply minimal yarn.lock changes
    
    ---------
    
    Co-authored-by: ikusakov <ikusakov@gxnfgqk426.y.yelp>
    Co-authored-by: Igor Kusakov <igor@kusakov.com>
    Co-authored-by: Eddy Nguyen <github@eddeee888.me>
    Co-authored-by: Eddy Nguyen <ch@eddeee888.me>

---
+++ dev-test-apollo-tooling/cli/index.ts
@@ -0,0 +1,91 @@
+#!/usr/bin/env ts-node
+
+import { generate } from '@graphql-codegen/cli';
+import type { Types } from '@graphql-codegen/plugin-helpers';
+
+export const GENERATED = '__generated__';
+export const GLOBAL_TYPES_FILE = 'globalTypes.ts';
+export const TS_GENERATED_FILE_HEADER = `\
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+`;
+
+/**
+ * The following GraphQL Codegen config matches as closely as possible
+ * to the old apollo-tooling codegen
+ * @see https://github.com/apollographql/apollo-tooling/issues/2053
+ * */
+const GRAPHQL_CODEGEN_CONFIG = {
+  useTypeImports: true,
+  namingConvention: 'keep', // Keeps naming as-is
+  avoidOptionals: false, // Allow '?' on variables fields
+  nonOptionalTypename: true, // Forces `__typename` on all selection sets
+  skipTypeNameForRoot: true, // Don't generate __typename for root types
+  omitOperationSuffix: true, // Don't add 'Query', 'Mutation' or 'Subscription' suffixes to operation result types
+  fragmentSuffix: '', // Don't add 'Fragment' suffix to fragment result types
+  extractAllFieldsToTypesCompact: true, // Extracts all fields to separate types (similar to apollo-codegen behavior)
+  printFieldsOnNewLines: true, // Prints each field on a new line (similar to apollo-codegen behavior)
+  importTypesNamespace: '', // Disable namespace prefix on imported types (preset config)
+  enumType: 'native',
+  generatesOperationTypes: true,
+};
+
+export const main = async () => {
+  const cwd = process.cwd();
+
+  const localSchemaFilePath = `${cwd}/schema.graphql`;
+
+  const includes = ['src'];
+
+  const generateFiles: { [scanPath: string]: Types.ConfiguredOutput } = {};
+
+  // Prepare the required structure for GraphQL Codegen
+  includes.forEach((include: string) => {
+    generateFiles[include] = {
+      preset: 'near-operation-file', // This preset tells the codegen to generate multiple files instead of one
+      presetConfig: {
+        extension: '.ts', //  Matches the existing Apollo-Codegen file naming
+        // FIXME: The following config is required, but it is not needed with the recent version of typescript-operations.
+        // So - when the new version of near-operation-file' is available - fix this.
+        baseTypesPath: 'unused',
+        folder: GENERATED, // Output folder for generated files
+        importTypesNamespace: '', // Disable namespace prefix on imported types
+      },
+      plugins: [
+        'typescript-operations',
+        {
+          add: {
+            content: TS_GENERATED_FILE_HEADER,
+          },
+        },
+      ],
+    };
+  });
+
+  await generate(
+    {
+      schema: localSchemaFilePath,
+      documents: [
+        // matching js extensins as well - there are cases where js files are not converted to typescript yet
+        // (but the package is typescript)
+        ...includes.map((include: any) => `${include}/**/*.{js,jsx,ts,tsx}`),
+        `!**/${GENERATED}/**`,
+      ],
+      config: GRAPHQL_CODEGEN_CONFIG,
+      generates: generateFiles,
+      silent: false,
+      debug: true,
+      verbose: true,
+    },
+    true // overwrite existing files
+  );
+};
+
+if (import.meta.url === process.argv[1] || import.meta.url === `file://${process.argv[1]}`) {
+  main().catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
+}
