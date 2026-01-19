edcc6527fbbe7be2bd51fcf9657452c18e8f0ca0
Igor Kusakov <ikusakov@yelp.com>
2026-01-19 06:21:01 -0500

    Bugfix: enums from external fragments were not generated along with operations (#10565)
    * integration test
    
    * cleanups
    
    * cleanups2
    
    * fixed a bug with external fragments
    
    * cleanup
    
    * keep generated files, refactored visitorwq
    
    * cleanup
    
    * changeset
    
    * adding missing dependency
    
    * delete temporary files
    
    * cleanup
    
    * cleanups
    
    * Adding back the integration tests + fix them
    
    * merged + fixes
    
    * cleanup
    
    * cleanup
    
    * patching near-operation-file preset
    
    * cleanup
    
    * adding duplicates case to integration tests
    
    * Fixing bug with duplicates generated when using extractAllFieldsToTypesCompact
    
    * cleanup
    
    * prevent running prettier on generated code
    
    * cleanups
    
    * cleanup
    
    * adding unit tests
    
    * update changeset
    
    * cleanups
    
    * try resolving graphql 16 error
    
    * remove unused deps
    
    * remove unused deps
    
    * cleanups
    
    * Revert and apply minimal yarn.lock changes
    
    ---------
    
    Co-authored-by: ikusakov <ikusakov@gxnfgqk426.y.yelp>
    Co-authored-by: Igor Kusakov <igor@kusakov.com>
    Co-authored-by: Eddy Nguyen <github@eddeee888.me>
    Co-authored-by: Eddy Nguyen <ch@eddeee888.me>

---
+++ dev-test-apollo-tooling/src/Component.ts
@@ -0,0 +1,26 @@
+/**
+ * This file tests generating enum from external fragement from another file.
+ * Check that UserManagerRoleType enum is generated in __generated__/Component.ts
+ */
+
+import { useQuery, gql } from '@apollo/client';
+import Helper from './Helper';
+
+export const getFooQuery = gql`
+  ${Helper.fragments.query}
+  query GetFoo($alias: String!, $collectionId: String!) {
+    superUser {
+      groupFromAlias(alias: $alias) {
+        managers(onlyPublic: true) {
+          ...HelperFields
+        }
+      }
+    }
+  }
+`;
+
+const Component = () => {
+  useQuery(getFooQuery, {});
+};
+
+export default Component;
