030100aa64917818bc14c44a582fc3a19f2d18c8
Eddy Nguyen <ch@eddeee888.me>
2026-02-26 01:02:03 +1100

    Migration guide: typescript-operations and client-preset to v6.0 (#10599)
    * Draft migration guide
    
    * Update next-env
    
    * Add notes
    
    * Add callout
    
    * Update doc meta
    
    * Format
    
    * Minor text updates
    
    * Improve generateOperationTypes and importSchemaTypesFrom comments
    
    * Fix typo
    
    * Add breaking change note about changing of the default ID type
    
    * Clean up

--- website/src/pages/docs/migration/_meta.ts
+++ website/src/pages/docs/migration/_meta.ts
@@ -3,4 +3,5 @@ export default {
   'from-0-18': 'v0.18 -> v1.0',
   'from-0-13': 'v0.13 -> v0.17',
   'from-4-0': 'v4.0 -> v5.0',
+  'operations-and-client-preset-from-5-0': 'typescript-operations and client-preset v5.0 -> v6.0',
 };
