916d0236f6952c8ea7d88294d79be27986a1ae97
Igor Kusakov <ikusakov@yelp.com>
2026-03-11 09:01:30 -0400

    Documentation for migrating from Apollo Tooling to GraphQL Codegen (#10612)
    * docs
    
    * fixed docs
    
    * added more docs
    
    * better comments
    
    * Run prettier
    
    ---------
    
    Co-authored-by: Igor Kusakov <igor@kusakov.com>
    Co-authored-by: Eddy Nguyen <ch@eddeee888.me>
    Co-authored-by: Eddy Nguyen <github@eddeee888.me>

--- website/src/pages/docs/migration/_meta.ts
+++ website/src/pages/docs/migration/_meta.ts
@@ -4,4 +4,5 @@ export default {
   'from-0-13': 'v0.13 -> v0.17',
   'from-4-0': 'v4.0 -> v5.0',
   'operations-and-client-preset-from-5-0': 'typescript-operations and client-preset v5.0 -> v6.0',
+  'apollo-tooling': 'Apollo Tooling -> GraphQL Code Generator',
 };
