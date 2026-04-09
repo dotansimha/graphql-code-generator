f9de28fba6fa228f53b4e963a857ab6ff70801de
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-04-09 19:46:55 +0300

    ..

--- packages/utils/graphql-codegen-testing/src/typescript.ts
+++ packages/utils/graphql-codegen-testing/src/typescript.ts
@@ -13,6 +13,7 @@ import {
   ScriptKind,
   ScriptTarget,
   ScriptTarget as ScriptTargetType,
+  version as tsVersion,
 } from 'typescript';
 import { Types } from '@graphql-codegen/plugin-helpers';
 
@@ -54,6 +55,10 @@ export function validateTs(
     options.strictPropertyInitialization = true;
     options.alwaysStrict = true;
     options.strictFunctionTypes = true;
+  }
+  if (tsVersion.startsWith('6.')) {
+    options.ignoreDeprecations ||= '6.0';
+    options.types ||= ['@types/node'];
   }
 
   const contents: string =
