bd3f236c35ee6df6c8c5d5482af2f030b4a432e6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-04-10 14:57:10 +0300

    Go

--- examples/react/apollo-client-defer/src/main.tsx
+++ examples/react/apollo-client-defer/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { GraphQL17Alpha2Handler } from '@apollo/client/incremental';
 import { ApolloProvider } from '@apollo/client/react';
 import App from './App';
 
@@ -9,6 +10,7 @@ const client = new ApolloClient({
     uri: 'https://graphql.org/graphql/',
   }),
   cache: new InMemoryCache(),
+  incrementalHandler: new GraphQL17Alpha2Handler(),
 });
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
