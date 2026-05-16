832ca5f6aa6ac5322edf4f0bcd6c205d58145344
Eddy Nguyen <github@eddeee888.me>
2026-05-17 00:41:02 +1000

    Fix overrides when matching graphql version, revert new package

--- scripts/match-graphql.js
+++ scripts/match-graphql.js
@@ -1,6 +1,7 @@
-const { writeFileSync } = require('fs');
+const fs = require('fs');
 const { resolve } = require('path');
 const { argv, cwd } = require('process');
+const yaml = require('js-yaml');
 
 const pkgPath = resolve(cwd(), './package.json');
 
@@ -8,15 +9,18 @@ const pkg = require(pkgPath);
 
 const version = argv[2];
 
-pkg.resolutions ||= {};
 if (pkg.devDependencies.graphql?.startsWith(version)) {
   // eslint-disable-next-line no-console
   console.info(`GraphQL v${version} is match! Skipping.`);
   return;
 }
 
-const npmVersion = version.includes('-') ? version : `^${version}`;
-pkg.devDependencies.graphql = npmVersion;
-pkg.resolutions.graphql = npmVersion;
+const pnpmWorkspaceFile = './pnpm-workspace.yaml';
 
-writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), 'utf8');
+const file = fs.readFileSync(pnpmWorkspaceFile, 'utf8');
+const parsedFile = yaml.load(file);
+
+parsedFile.overrides ||= {};
+parsedFile.overrides.graphql = '15';
+
+fs.writeFileSync(pnpmWorkspaceFile, yaml.dump(parsedFile), 'utf8');
