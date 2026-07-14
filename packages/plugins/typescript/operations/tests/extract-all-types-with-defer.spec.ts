6734b6dd95774a7bf6d9d54953598534f1d4a771
Eddy Nguyen <ch@eddeee888.me>
2026-07-15 00:13:43 +1000

    Set up test case

---
+++ packages/plugins/typescript/operations/tests/extract-all-types-with-defer.spec.ts
@@ -0,0 +1,61 @@
+import { buildSchema, parse } from 'graphql';
+import { mergeOutputs } from '@graphql-codegen/plugin-helpers';
+import { validateTs } from '@graphql-codegen/testing';
+import { plugin } from '../src/index.js';
+
+describe('extractAllFieldsToTypes: true with @defer', () => {
+  it('should extract types from queries', async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      type Query {
+        user(id: ID!): User
+      }
+
+      type User {
+        id: ID!
+        username: String!
+        email: String!
+      }
+    `);
+
+    const document = parse(/* GraphQL */ `
+      fragment UserEmail on User {
+        email
+      }
+
+      query user {
+        user(id: 1) {
+          id
+          username
+          ...UserEmail @defer
+        }
+      }
+    `);
+
+    const result = mergeOutputs([
+      await plugin(schema, [{ document }], { extractAllFieldsToTypes: true }, { outputFile: '' }),
+    ]);
+
+    expect(result).toMatchInlineSnapshot(`
+      "/** Internal type. DO NOT USE DIRECTLY. */
+      type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+      /** Internal type. DO NOT USE DIRECTLY. */
+      export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+      export type UserEmailFragment = { email: string };
+
+      export type UserQuery_user_User = { id: string, username: string };
+
+      export type UserQuery_user_User = { email: string } | { email?: never };
+
+      export type UserQuery_Query = { user: UserQuery_user_User & UserQuery_user_User | null };
+
+
+      export type UserQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+      export type UserQuery = UserQuery_Query;
+      "
+    `);
+
+    validateTs(result, undefined, undefined, undefined, undefined, true);
+  });
+});
