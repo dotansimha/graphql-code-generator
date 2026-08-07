7088c618adcf7177d151fdf7dd7c17ab618e58a2
Laurin <laurinquast@googlemail.com>
2026-08-07 15:06:01 +0200

    docs: fix page titles missing? (#10914)

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@theguild/components/style.css';
 import { AppProps } from 'next/app';
 import localFont from 'next/font/local';
+import Head from 'next/head';
+import favicon from '../../public/favicon.svg';
 import '../selection-styles.css';
 
 const neueMontreal = localFont({
@@ -13,6 +15,9 @@ const neueMontreal = localFont({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href={favicon.src} />
+      </Head>
       <style jsx global>{`
         :root {
           --font-sans: ${neueMontreal.style.fontFamily};
