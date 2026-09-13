464b531bd954dc3cd283a281f132e93ab395f291
Eddy Nguyen <ch@eddeee888.me>
2026-09-13 12:20:15 +1000

    [testing] refactor: replace unmaintained graphql-helix with graphql-yoga (#10951)

--- packages/utils/graphql-codegen-testing/src/mock-graphql-server.ts
+++ packages/utils/graphql-codegen-testing/src/mock-graphql-server.ts
@@ -1,5 +1,5 @@
 import { GraphQLSchema } from 'graphql';
-import { getGraphQLParameters, processRequest as processGraphQLHelixRequest } from 'graphql-helix';
+import { createYoga } from 'graphql-yoga';
 import nock from 'nock';
 
 export function mockGraphQLServer({
@@ -15,46 +15,27 @@ export function mockGraphQLServer({
   intercept?: (obj: nock.ReplyFnContext) => void;
   method?: string;
 }) {
+  const yoga = createYoga({ schema, logging: false });
+
   const handler = async function (this: nock.ReplyFnContext, uri: string, body: any) {
     if (intercept) {
       intercept(this);
     }
-    const uriObj = new URL(host + uri);
-    const queryObj: any = {};
-    for (const [key, val] of uriObj.searchParams.entries()) {
-      queryObj[key] = val;
-    }
-    // Create a generic Request object that can be consumed by Graphql Helix's API
-    const request = {
-      body,
-      headers: this.req.headers,
+    // Create a generic Request object that can be consumed by Yoga's fetch API
+    const request = new Request(new URL(host + uri), {
       method,
-      query: queryObj,
-    };
-    // Extract the GraphQL parameters from the request
-    const { operationName, query, variables } = getGraphQLParameters(request);
-
-    // Validate and execute the query
-    const result = await processGraphQLHelixRequest({
-      operationName,
-      query,
-      variables,
-      request,
-      schema,
+      headers: this.req.headers as HeadersInit,
+      body: method === 'GET' ? undefined : JSON.stringify(body),
     });
-    // processRequest returns one of three types of results depending on how the server should respond
-    // 1) RESPONSE: a regular JSON payload
-    // 2) MULTIPART RESPONSE: a multipart response (when @stream or @defer directives are used)
-    // 3) PUSH: a stream of events to push back down the client for a subscription
-    if (result.type === 'RESPONSE') {
-      const headers = {};
-      // We set the provided status and headers and just the send the payload back to the client
-      for (const { name, value } of result.headers) {
-        headers[name] = value;
-      }
-      return [result.status, result.payload, headers];
+
+    const response = await yoga.fetch(request);
+
+    const headers: Record<string, string> = {};
+    for (const [name, value] of response.headers) {
+      headers[name] = value;
     }
-    return [500, 'Not implemented'];
+
+    return [response.status, await response.json(), headers];
   };
   switch (method) {
     case 'GET':
